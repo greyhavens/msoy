fa40482f89441dbd96dd87e9a2ecbe80f2d274a3
Michael Bayne <mdb@threerings.net>
2006-08-03 21:01:13 +0000

    More fiddling regarding item upload.

--- src/gwt/client/inventory/MediaItemEditor.java
+++ src/gwt/client/inventory/MediaItemEditor.java
@@ -3,7 +3,11 @@
 
 package client.inventory;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
 
 import org.gwtwidgets.client.ui.FileUploadField;
 import org.gwtwidgets.client.ui.FormPanel;
@@ -21,16 +25,40 @@ public abstract class MediaItemEditor extends ItemEditor
     protected void createEditorInterface ()
     {
         _panel = new FormPanel(new FlowPanel());
-        _panel.setTarget("/upload");
-        _panel.setTarget("__msoy_uploadFrame");
+        _panel.setAction("http://localhost:8080/upload");
+        _panel.setTarget("upload");
         _panel.setMethodAsPost();
         _panel.setMultipartEncoding();
         _panel.addField(new FileUploadField("media"), "media");
 
+        _panel.add(_submit = new Button("Upload"));
+        _submit.addClickListener(new ClickListener() {
+            public void onClick (Widget button) {
+                _submit.setEnabled(false);
+                _panel.submit();
+            }
+        });
+
         int row = getRowCount();
         setText(row, 0, "Upload");
-        setWidget(row, 1, _panel.getPanel());
+        setWidget(row, 1, _panel);
+
+        setWidget(row+1, 0, _out = new Label());
+        setWidget(row+1, 1, _check = new Button("Check"));
+        _check.addClickListener(new ClickListener() {
+            public void onClick (Widget widget) {
+                _out.setText(checkUpload());
+            }
+        });
     }
 
+    protected static native String checkUpload () /*-{
+        return $doc.getElementById('__msoy_uploadFrame').toString();
+    }-*/;
+
     protected FormPanel _panel;
+    protected Button _submit;
+
+    protected Label _out;
+    protected Button _check;
 }
