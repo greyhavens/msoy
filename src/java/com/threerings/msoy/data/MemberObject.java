c3128d8805f6c0bdec7d9d8342076a93561a300d
Ray Greenwell <ray@threerings.net>
2006-08-09 21:12:00 +0000

    Enable daniel's bunny.

--- src/java/com/threerings/msoy/data/MemberObject.java
+++ src/java/com/threerings/msoy/data/MemberObject.java
@@ -299,6 +299,6 @@ public class MemberObject extends BodyObject
     // TEMP: media ids for our standard avatars
     public static final int[] AVATARS = {
         0, 1, 2, 20, 21, 22, 25, 38, 39,
-        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
+        40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 54
     };
 }
