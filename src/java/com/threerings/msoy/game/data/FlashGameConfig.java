1d5dbbd06615f1e2ac941bd00ca357d51b2feb36
Ray Greenwell <ray@threerings.net>
2006-08-24 00:31:11 +0000

    Fergot this lil' feller...

--- src/java/com/threerings/msoy/game/data/FlashGameConfig.java
+++ src/java/com/threerings/msoy/game/data/FlashGameConfig.java
@@ -5,30 +5,10 @@ package com.threerings.msoy.game.data;
 
 import com.threerings.ezgame.data.EZGameConfig;
 
-import com.threerings.msoy.data.MediaData;
-
 /**
  * A game config for a simple multiplayer flash game.
  */
 public class FlashGameConfig extends EZGameConfig
 {
-    /** The media that is the game we're going to play. */
-    public MediaData game;
-
-    @Override
-    public boolean equals (Object other)
-    {
-        if (!super.equals(other)) {
-            return false;
-        }
-
-        FlashGameConfig that = (FlashGameConfig) other;
-        return this.game.equals(that.game);
-    }
-
-    @Override
-    public int hashCode ()
-    {
-        return super.hashCode() + game.hashCode();
-    }
+    // absolutely nothing, here on the java side
 }
