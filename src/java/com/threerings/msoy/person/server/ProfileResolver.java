c2eac0da647b23c37228722012e8f82dee8431ae
Michael Bayne <mdb@threerings.net>
2006-08-25 19:03:14 +0000

    Get the profile from the MemberManager.

--- src/java/com/threerings/msoy/person/server/ProfileResolver.java
+++ src/java/com/threerings/msoy/person/server/ProfileResolver.java
@@ -3,7 +3,9 @@
 
 package com.threerings.msoy.person.server;
 
-import com.threerings.msoy.item.data.Photo;
+import com.samskivert.util.ResultListener;
+
+import com.threerings.msoy.server.MsoyServer;
 import com.threerings.msoy.web.data.Profile;
 
 /**
@@ -14,17 +16,14 @@ public class ProfileResolver extends BlurbResolver
     @Override // from BlurbResolver
     protected void resolve ()
     {
-        Profile stub = new Profile();
-        stub.memberId = _memberId;
-        stub.displayName = "Captain Cleaver";
-        stub.photo = new Photo();
-        stub.photo.mediaHash = "816cd5aebc2d9d228bf66cff193b81eba1a6ac85";
-        stub.photo.mimeType = Photo.IMAGE_JPEG;
-        stub.headline = "Arr! Mateys, this here be me profile!";
-        stub.homePageURL = "http://www.puzzlepirates.com/";
-        stub.isMale = true;
-        stub.location = "San Francisco, CA";
-        stub.age = 36;
-        resolutionCompleted(stub);
+        MsoyServer.memberMan.loadProfile(
+            _memberId, new ResultListener<Profile>() {
+            public void requestCompleted (Profile profile) {
+                resolutionCompleted(profile);
+            }
+            public void requestFailed (Exception cause) {
+                resolutionFailed(cause);
+            }
+        });
     }
 }
