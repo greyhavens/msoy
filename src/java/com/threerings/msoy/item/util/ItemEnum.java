c2084271b150b7566a17f61d365cb54bbaaf09e8
Ray Greenwell <ray@threerings.net>
2006-09-26 23:23:50 +0000

    Use programmer-assigned bytes to identify an item type rather than arbitrary ugly ints (that would have been harder to duplicate in actionscript land).

--- src/java/com/threerings/msoy/item/util/ItemEnum.java
+++ src/java/com/threerings/msoy/item/util/ItemEnum.java
@@ -3,8 +3,6 @@
 
 package com.threerings.msoy.item.util;
 
-import java.util.zip.CRC32;
-
 import com.samskivert.util.HashIntMap;
 
 import com.threerings.msoy.item.web.Document;
@@ -22,20 +20,20 @@ public enum ItemEnum
 {
     // NOTE: Don't remove or change the name of any of these unless you know
     // what you're doing.
-    PHOTO(Photo.class),
-    DOCUMENT(Document.class),
-    FURNITURE(Furniture.class),
-    GAME(Game.class),
+    PHOTO(Photo.class, (byte)1),
+    DOCUMENT(Document.class,(byte)2),
+    FURNITURE(Furniture.class, (byte)3),
+    GAME(Game.class, (byte)4),
 
     // NOTE: If you add an item to this list, you need to make sure that
     // Item.getType() returns the string value of the enumeration that is added
     // to correspond to the new item.
-    UNUSED(null);
+    UNUSED(null, (byte)-1);
 
     /**
      * Maps an {@link ItemEnum}'s code code back to an instance.
      */
-    public static ItemEnum getItem (int code)
+    public static ItemEnum getItem (byte code)
     {
         return _codeToEnum.get(code);
     }
@@ -52,50 +50,37 @@ public enum ItemEnum
      * Returns the unique code for this item type, which is a function of its
      * name.
      */
-    public int getCode ()
+    public byte getCode ()
     {
         return _code;
     }
 
-    ItemEnum (Class<? extends Item> iclass)
+    ItemEnum (Class<? extends Item> iclass, byte code)
     {
         _itemClass = iclass;
-        _code = register(this);
-    }
-
-    protected static int register (ItemEnum value)
-    {
-        // this method gets called before the class's static initializer
-        // (magic!) so we have to create these here
-        if (_crc == null) {
-            _crc = new CRC32();
-            _codeToEnum = new HashIntMap<ItemEnum>();
-        }
-
-        // compute our unique code
-        _crc.reset();
-        _crc.update(value.toString().getBytes());
-        int code = (int)_crc.getValue();
-
-        if (_codeToEnum.containsKey(code)) {
-            log.warning("Item name collision! " + value + " and " +
-                _codeToEnum.get(code) + " both map to '" + code + "'.");
-        } else {
-            _codeToEnum.put(code, value);
-        }
-
-        return code;
+        _code = code;
     }
 
     /** The unique code for this item. */
-    protected int _code;
+    protected byte _code;
 
     /** The derived class for this item. */
     protected Class<? extends Item> _itemClass;
 
     /** The table mapping codes back to enumerated values. */
-    protected static HashIntMap<ItemEnum> _codeToEnum;
+    protected static HashIntMap<ItemEnum> _codeToEnum =
+        new HashIntMap<ItemEnum>();
 
-    /** Used to compute 32-bit codes for each item type. */
-    protected static CRC32 _crc;
+    // initialize the reverse-mapping
+    static {
+        for (ItemEnum value : ItemEnum.values()) {
+            byte code = value.getCode();
+            if (_codeToEnum.containsKey(code)) {
+                log.warning("Item code collision! " + value + " and " +
+                    _codeToEnum.get(code) + " both use code '" + code + "'.");
+            } else {
+                _codeToEnum.put(code, value);
+            }
+        }
+    }
 }
