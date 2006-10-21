39036caa8312387f5541edf0b84c32e738e56cf6
Par Winzell <zell@threerings.net>
2006-10-21 22:53:15 +0000

    Added placeholder MailManager so things will compile, sorry!

---
+++ src/java/com/threerings/msoy/server/MailManager.java
@@ -0,0 +1,28 @@
+//
+// $Id$
+
+package com.threerings.msoy.server;
+
+import com.threerings.msoy.server.persist.MailRepository;
+import com.threerings.msoy.server.persist.MemberRepository;
+
+/**
+ * Manage msoy members.
+ */
+public class MailManager
+{
+    /**
+     * Prepares our member manager for operation.
+     */
+    public void init (MailRepository mailRepo, MemberRepository memberRepo)
+    {
+        _mailRepo = mailRepo;
+        _memberRepo = memberRepo;
+    }
+
+    /** Provides access to persistent mail data. */
+    protected MailRepository _mailRepo;
+    
+    /** Provides access to persistent member data. */
+    protected MemberRepository _memberRepo;
+}
