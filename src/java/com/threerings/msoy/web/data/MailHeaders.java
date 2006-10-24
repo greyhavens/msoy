c8f77a53a4e2f792dcf445ad11960001f783cca9
Par Winzell <zell@threerings.net>
2006-10-24 01:53:43 +0000

    Forgot this file in my last check-in. Grrr.

---
+++ src/java/com/threerings/msoy/web/data/MailHeaders.java
@@ -0,0 +1,38 @@
+//
+// $Id$
+
+package com.threerings.msoy.web.data;
+
+import java.util.Date;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+import com.threerings.io.Streamable;
+
+/**
+ * Represents all the metadata for a mail message.
+ */
+
+public class MailHeaders
+    implements IsSerializable, Streamable
+{
+    /** The id of this message, unique within its current folder. */
+    public int messageId;
+
+    /** The id of the folder in which this message is currently filed. */
+    public int folderId;
+
+    /** The id of the owner of this message. */
+    public int ownerId;
+
+    /** The message sender. */
+    public MemberGName sender;
+
+    /** The message recipient. */
+    public MemberGName recipient;
+
+    /** The subject of this message. */
+    public String subject;
+    
+    /** The time at which the message was delivered. */
+    public Date sent;
+}
