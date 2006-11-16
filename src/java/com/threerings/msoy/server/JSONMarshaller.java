c93467766e4cdfef4999176d265b4e84ef2ee8af
Par Winzell <zell@threerings.net>
2006-11-16 22:39:46 +0000

    Well, for better or worse, I dun it, the JSON marshaller now recurses through array and object fields. There is no 'marker' interface; it'll follow any link whatsoever. It uses declared class only, no runtime class, which severely limits its utility in storing general data structures. Still, as long as it's judged on its own merit rather than as a general purpose serializer, it works well and produces compact, readable representations.

--- src/java/com/threerings/msoy/server/JSONMarshaller.java
+++ src/java/com/threerings/msoy/server/JSONMarshaller.java
@@ -3,19 +3,26 @@
 
 package com.threerings.msoy.server;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import javax.persistence.Transient;
-
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
  * Handles the marshalling and unmarshalling of persistent instances to JSON objects.
- */
+ *
+ * This format is meant for fairly simple data structures. It will recurse through object
+ * fields, but only using their declared class, ignoring runtime class entirely. Do not
+ * aim this code at anything that relies on abstract objects or implementing interfaces.
+ * Most subclassing will fail. An Object[] array, for example, is useless: its elements
+ * will be serialized as Objects, regardless of what they actually are at runtime.
+  */
 public class JSONMarshaller<T>
 {
     /**
@@ -40,7 +47,7 @@ public class JSONMarshaller<T>
     }
 
     /**
-     * Return a JSON marshaller for a given class, either through cache lookup or creation.
+     * Returns a JSON marshaller for a given class, either through cache lookup or creation.
      */
     public static <T> JSONMarshaller getMarshaller (Class<T> pclass)
     {
@@ -60,8 +67,8 @@ public class JSONMarshaller<T>
         _pclass = pclass;
         for (Field field : pclass.getFields()) {
             int mods = field.getModifiers();
-            if (((mods & Modifier.PUBLIC) == 0) || ((mods & Modifier.STATIC) != 0) ||
-                field.getAnnotation(Transient.class) != null) {
+            if ((mods & Modifier.PUBLIC) == 0 || (mods & Modifier.STATIC) != 0 ||
+                (mods & Modifier.TRANSIENT) != 0) {
                 continue;
             }
             _fields.put(field.getName(), field);
@@ -70,21 +77,67 @@ public class JSONMarshaller<T>
 
     /**
      * Creates a new instance of the object for which we're responsible, and populates
-     * the object with the supplied state string, which should be on JSON format.
+     * the object with the supplied state, which should be on JSON format.
      */
     public T newInstance (byte[] json)
         throws JSONMarshallingException
     {
         try {
-            JSONObject state = new JSONObject(new String(json));
+            return (T) deserialize(new JSONObject(new String(json)), _pclass);
+        } catch (Exception e) {
+            throw new JSONMarshallingException("Failed to deserialize [class=" + _pclass + "]", e);
+        }
+    }
+    
+    /**
+     * Returns the JSON representation of the state of the given object.
+     */
+    public byte[] getState (T obj)
+        throws JSONMarshallingException
+    {
+        try {
+            return serialize(obj, obj.getClass()).toString().getBytes();
+        } catch (Exception e) {
+            throw new JSONMarshallingException("Failed to serialize [class=" + _pclass + "]", e);
+        }
+    }
+    
+    // TODO: it's idiotic that org.json does not have its objects implement a JSONValue interface
+    protected Object deserialize (Object state, Class dClass)
+         throws JSONMarshallingException
+    {
+        try {
+            if (isJSONPrimitive(dClass)) {
+                return state;
+            }
+            if (dClass.isArray()) {
+                if (!(state instanceof JSONArray)) {
+                    throw new JSONMarshallingException(
+                        "Can't stuff non-array state into array field [dClass=" + dClass + "]");
+                }
+                Class cClass = dClass.getComponentType();
+                JSONArray jArr = (JSONArray) state;
+                int sz = jArr.length();
+                Object rArr = Array.newInstance(cClass, sz);
+                for (int ii = 0; ii < sz; ii ++ ) {
+                    Array.set(rArr, ii, deserialize(jArr.get(ii), cClass));
+                }
+                return rArr;
+            }
+            if (!_pclass.equals(dClass)) {
+                return getMarshaller(dClass).deserialize(state, dClass);
+            }
+            JSONObject jObj = (JSONObject) state;
             T obj = _pclass.newInstance();
-            Iterator keys = state.keys();
+            Iterator keys = jObj.keys();
             while (keys.hasNext()) {
                 String key = (String) keys.next();
                 Field field = _fields.get(key);
-                if (field != null) {
-                    field.set(obj, state.get(key));
+                if (field == null) {
+                    // log? throw exception?
+                    continue;
                 }
+                field.set(obj, deserialize(jObj.get(key), field.getType()));
             }
             return obj;
         } catch (Exception e) {
@@ -92,25 +145,49 @@ public class JSONMarshaller<T>
         }
     }
 
-    /**
-     * Returns the JSON representation of the given object's state.
-     */
-    public byte[] getState (T obj)
-        throws JSONMarshallingException
+    // TODO: it's idiotic that org.json does not have its objects implement a JSONValue interface
+    protected Object serialize (Object value, Class dClass)
+        throws JSONException, IllegalAccessException
     {
-        try {
-            JSONObject state = new JSONObject();
-            for (Field field : _fields.values()) {
-                state.put(field.getName(), field.get(obj));
-            }
-            return state.toString().getBytes();
-        } catch (Exception e) {
-            throw new JSONMarshallingException("Failed to serialize [class=" + _pclass + "]", e);            
+        if (isJSONPrimitive(dClass)) {
+            return value;
         }
+        if (dClass.isArray()) {
+            Class cClass = dClass.getComponentType();
+            int sz = Array.getLength(value);
+            JSONArray jArr = new JSONArray();
+            for (int ii = 0; ii < sz; ii ++ ) {
+                jArr.put(serialize(Array.get(value, ii), cClass));
+            }
+            return jArr;
+        }
+        if (!_pclass.equals(dClass)) {
+            return getMarshaller(dClass).serialize(value, dClass);
+        }
+        JSONObject state = new JSONObject();
+        for (Field field : _fields.values()) {
+            state.put(field.getName(), serialize(field.get(value), field.getType()));
+        }
+        return state;
+    }
+    
+    // convenience method for categorizing anything JSON treats as primitive
+    protected boolean isJSONPrimitive(Class vClass) {
+        return (vClass.equals(Boolean.TYPE) || vClass.equals(Boolean.class) ||
+                vClass.equals(Byte.TYPE) || vClass.equals(Byte.class) ||
+                vClass.equals(Short.TYPE) || vClass.equals(Short.class) ||
+                vClass.equals(Integer.TYPE) || vClass.equals(Integer.class) ||
+                vClass.equals(Long.TYPE) || vClass.equals(Long.class) ||
+                vClass.equals(Float.TYPE) || vClass.equals(Float.class) ||
+                vClass.equals(Double.TYPE) || vClass.equals(Double.class) ||
+                vClass.equals(String.class));
     }
 
-    protected Map<String, Field> _fields = new HashMap<String, Field>();
+    /** The class for whom we're marshalling. */
     protected Class<T> _pclass;
+    /** Names of public, non-static, non-transient fields mapped to {@link Field} instances. */
+    protected Map<String, Field> _fields = new HashMap<String, Field>();
 
+    /** The static cache of instantiated marshallers. */
     protected static Map<Class, JSONMarshaller> _classMap = new HashMap<Class, JSONMarshaller>();
 }
