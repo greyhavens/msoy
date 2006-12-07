3d9a95842c8d40a5549956b77ffa6769bc58aa0c
Ray Greenwell <ray@threerings.net>
2006-12-07 19:06:19 +0000

    Comments!

--- src/gwt/com/threerings/io/Streamable.java
+++ src/gwt/com/threerings/io/Streamable.java
@@ -2,6 +2,12 @@ package com.threerings.io;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+/**
+ * An imposter interface that causes GWT to believe all
+ * Streamable classes are also IsSerializable. We also put a
+ * "kick me" sign on GWT's back.
+ */
 public interface Streamable extends IsSerializable
 {
+    // balloon juice
 }
