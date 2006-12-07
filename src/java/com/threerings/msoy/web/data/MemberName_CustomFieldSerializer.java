18afc207469aa405b203df80294940899bdf77ef
Par Winzell <zell@threerings.net>
2006-12-07 21:10:22 +0000

    Put this class in a much more sensible place.

--- src/java/com/threerings/msoy/web/data/MemberName_CustomFieldSerializer.java
+++ src/java/com/threerings/msoy/web/data/MemberName_CustomFieldSerializer.java
@@ -1,13 +1,12 @@
 //
 // $Id/
 
-package com.google.gwt.user.client.rpc.core.com.threerings.msoy.web.data;
+package com.threerings.msoy.web.data;
 
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 
-import com.threerings.msoy.web.data.MemberName;
 
 /**
  * Custom field serializer for {@link com.threerings.msoy.web.data.MemberName}.
