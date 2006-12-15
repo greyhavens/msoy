c3f81d08de3d6e5a19355f6728ce6281354517f7
Par Winzell <zell@threerings.net>
2006-12-15 05:51:31 +0000

    Make the new widget a little more useful. Tomorrow we'll make it do something, too.

--- src/gwt/client/catalog/CatalogPanel.java
+++ src/gwt/client/catalog/CatalogPanel.java
@@ -9,20 +9,16 @@ import java.util.Map;
 import client.item.ItemSearchSortPanel;
 import client.item.ItemTypePanel;
 import client.item.TagCloud;
+import client.util.WebContext;
 
-import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.SourcesTabEvents;
 import com.google.gwt.user.client.ui.TabListener;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
+import com.threerings.msoy.item.web.CatalogListing;
 import com.threerings.msoy.item.web.Item;
-
-import client.util.WebContext;
 
 /**
  * Displays a tabbed panel containing the catalog.
@@ -41,7 +37,10 @@ public class CatalogPanel extends VerticalPanel
         
         VerticalPanel uiBits = new VerticalPanel();
         uiBits.add(new ItemTypePanel(this));
-        uiBits.add(new ItemSearchSortPanel(this));
+        uiBits.add(new ItemSearchSortPanel( this,
+            new String[] { "Rating", "List Date" },
+            new byte[] { CatalogListing.SORT_BY_RATING, CatalogListing.SORT_BY_LIST_DATE }));
+
         topRow.add(uiBits);
         add(topRow);
 
@@ -76,17 +75,15 @@ public class CatalogPanel extends VerticalPanel
     }
 
     //from ItemSearchSortPanel.Listener
-    public void searchStringUpdated (String newText)
+    public void search (ItemSearchSortPanel panel)
     {
-        // TODO Auto-generated method stub
-        
+        Window.alert("I would search for: " + panel.search);
     }
 
     // from ItemSearchSortPanel.Listener
-    public void sortCriteriumUpdated (byte newCriterium)
+    public void sort (ItemSearchSortPanel panel)
     {
-        // TODO Auto-generated method stub
-        
+        Window.alert("I would search by criterium number: " + panel.sortBy);
     }
 
     protected WebContext _ctx;
--- src/gwt/client/item/ItemSearchSortPanel.java
+++ src/gwt/client/item/ItemSearchSortPanel.java
@@ -4,24 +4,38 @@
 package client.item;
 
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.threerings.gwt.ui.EnterClickAdapter;
 import com.threerings.msoy.item.web.CatalogListing;
 
 public class ItemSearchSortPanel extends HorizontalPanel
 {
+    /** An interface to use for instantiating an ItemSearchSortPanel. */
     public static interface Listener {
-        public void searchStringUpdated (String newText);
-        
-        public void sortCriteriumUpdated (byte newCriterium);
+        /**
+         * This method is called when the user hits enters while on the search
+         * field, or clicks the search button.
+         */
+        public void search (ItemSearchSortPanel panel);
+        /**
+         * This method is called when the user selects a sort criterium.
+         */
+        public void sort (ItemSearchSortPanel panel);
     }
-    public byte sortBy;
+    
+    /** The text in the search field. */
     public String search;
+    /** The current sort order. */
+    public byte sortBy;
 
 
-    public ItemSearchSortPanel (Listener listener)
+    public ItemSearchSortPanel (Listener listener, String[] sortNames, byte[] sortValues)
     {
         _listener = listener;
         
@@ -29,9 +43,22 @@ public class ItemSearchSortPanel extends HorizontalPanel
         
         TextBox searchBox = new TextBox();
         searchBox.setStyleName("itemSearchBox");
+        searchBox.addChangeListener(new ChangeListener() {
+            public void onChange (Widget widget) {
+                TextBox box = (TextBox) widget;
+                search = box.getText();
+            }
+        });
+        ClickListener clickListener = new ClickListener() {
+            public void onClick (Widget sender) {
+                _listener.search(ItemSearchSortPanel.this);
+            }
+        };
+        searchBox.addKeyboardListener(new EnterClickAdapter(clickListener));
         add(searchBox);
 
         Button searchButton = new Button("search");
+        searchButton.addClickListener(clickListener);
         add(searchButton);
         
         Label sortLabel = new Label("Sort by:");
@@ -40,8 +67,16 @@ public class ItemSearchSortPanel extends HorizontalPanel
         
         ListBox sortBox = new ListBox();
         sortBox.setStyleName("itemSortBox");
-        sortBox.addItem("Rating", String.valueOf(CatalogListing.SORT_BY_RATING));
-        sortBox.addItem("Listed Date", String.valueOf(CatalogListing.SORT_BY_LIST_DATE));
+        for (int ii = 0; ii < sortNames.length; ii ++) {
+            sortBox.addItem(sortNames[ii], String.valueOf(sortValues[ii]));
+        }
+        sortBox.addChangeListener(new ChangeListener() {
+            public void onChange (Widget widget) {
+                ListBox box = (ListBox) widget;
+                sortBy = Byte.parseByte(box.getValue(box.getSelectedIndex()));
+                _listener.sort(ItemSearchSortPanel.this);
+            }
+        });
         add(sortBox);
     }
     
