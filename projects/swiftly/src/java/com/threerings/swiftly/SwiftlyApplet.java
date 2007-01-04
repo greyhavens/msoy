01f4641e8788481905b863c549acd04a0626e8a6
Jonathan Le Plastrier <jonathan@threerings.net>
2007-01-04 00:22:31 +0000

    Goodbye Country, Hello Night Club. Let's try this using a Java Applet instead.

---
+++ projects/swiftly/src/java/com/threerings/swiftly/SwiftlyApplet.java
@@ -0,0 +1,10 @@
+package com.threerings.swiftly;
+
+import java.awt.*; 
+import java.applet.*;
+
+public class SwiftlyApplet extends Applet {
+    public void init() {
+        add(new TextArea(35, 85));
+  }
+} 
