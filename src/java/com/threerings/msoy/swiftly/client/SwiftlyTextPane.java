55a4901d9d42565fed13d01cc079b85a358a8924
Jonathan Le Plastrier <jonathan@threerings.net>
2007-01-07 00:25:29 +0000

    ContextMenu for the textpane. Cut/Copy/Paste. It looks awful, needs some design love.

--- src/java/com/threerings/msoy/swiftly/client/SwiftlyTextPane.java
+++ src/java/com/threerings/msoy/swiftly/client/SwiftlyTextPane.java
@@ -3,11 +3,16 @@ package com.threerings.msoy.swiftly.client;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Event;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.Action;
 import javax.swing.AbstractAction;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextPane;
 import javax.swing.KeyStroke;
 import javax.swing.event.UndoableEditEvent;
@@ -15,6 +20,7 @@ import javax.swing.event.UndoableEditListener;
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.CannotUndoException;
 import javax.swing.undo.UndoManager;
+import javax.swing.text.DefaultEditorKit;
 import javax.swing.text.StyledDocument;
 import javax.swing.text.BadLocationException;
 
@@ -24,7 +30,8 @@ public class SwiftlyTextPane extends JTextPane
     {
         _editor = editor;
         _document = document;
-        keyBindings();
+        addKeyBindings();
+        addPopupMenu();
 
         // load the document text and undo listener
         StyledDocument styledDoc = getStyledDocument();
@@ -41,7 +48,7 @@ public class SwiftlyTextPane extends JTextPane
         setCaretColor(Color.white);
     }
 
-    protected void keyBindings ()
+    protected void addKeyBindings ()
     {
         // ctrl-w closes the tab
         addKeyAction(new CloseTabAction(), KeyStroke.getKeyStroke(KeyEvent.VK_W, Event.CTRL_MASK));
@@ -53,11 +60,52 @@ public class SwiftlyTextPane extends JTextPane
         addKeyAction(_redoAction, KeyStroke.getKeyStroke(KeyEvent.VK_Y, Event.CTRL_MASK));
     }
 
+    protected void addPopupMenu ()
+    {
+        _popup = new JPopupMenu();
+
+        // Cut
+        JMenuItem menuItem = new JMenuItem("Cut");
+        Action action = new DefaultEditorKit.CutAction();
+        menuItem.addActionListener(action);
+        _popup.add(menuItem);
+        // Copy
+        menuItem = new JMenuItem("Copy");
+        action = new DefaultEditorKit.CopyAction();
+        menuItem.addActionListener(action);
+        _popup.add(menuItem);
+        // Paste
+        menuItem = new JMenuItem("Paste");
+        action = new DefaultEditorKit.PasteAction();
+        menuItem.addActionListener(action);
+        _popup.add(menuItem);
+
+        MouseListener popupListener = new PopupListener();
+        // add popupListener to the textpane
+        addMouseListener(popupListener);
+    }
+
     protected void addKeyAction (Action action, KeyStroke key)
     {
         // TODO getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW); vs. WHEN_FOCUSED
         getInputMap().put(key, action);
         getActionMap().put(action, action);
+    }
+
+    protected class PopupListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            maybeShowPopup(e);
+        }
+
+        public void mouseReleased(MouseEvent e) {
+            maybeShowPopup(e);
+        }
+
+        private void maybeShowPopup(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                _popup.show(e.getComponent(), e.getX(), e.getY());
+            }
+        }
     }
 
     protected class UndoHandler implements UndoableEditListener
@@ -140,6 +188,7 @@ public class SwiftlyTextPane extends JTextPane
         }
     }
     
+    protected JPopupMenu _popup;
     protected UndoManager _undo = new UndoManager();
     protected UndoableEditListener _undoHandler = new UndoHandler();
     protected UndoAction _undoAction = new UndoAction();
