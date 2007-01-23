afd360b0c58324c17b5346e1b8c4d5e51586480f
Par Winzell <zell@threerings.net>
2007-01-23 03:13:10 +0000

    You too.

---
+++ src/gwt/client/admin/AdminItemPopup.java
@@ -0,0 +1,45 @@
+//
+// $Id$
+
+package client.admin;
+
+import client.item.BaseItemDetailPopup;
+import client.shell.MsoyEntryPoint;
+
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+import com.threerings.gwt.ui.WidgetUtil;
+import com.threerings.msoy.item.web.Avatar;
+import com.threerings.msoy.item.web.Item;
+import com.threerings.msoy.item.web.MediaDesc;
+
+/**
+ * Displays a popup detail view of an item for an administrator.
+ */
+public class AdminItemPopup extends BaseItemDetailPopup
+{
+    public AdminItemPopup (AdminContext ctx, Item item, ReviewPopup parent)
+    {
+        super(ctx, item);
+        _parent = parent;
+    }
+
+    // @Override // BaseItemDetailPopup
+    protected Widget createPreview (Item item)
+    {
+        // TODO: maybe ItemUtil should handle this
+        if (item instanceof Avatar) {
+            MediaDesc avatarMedia = ((Avatar)_item).avatarMedia;
+            String path = MsoyEntryPoint.toMediaPath(avatarMedia.getMediaPath());
+            return WidgetUtil.createFlashContainer(
+                "avatarViewer", "/clients/avatarviewer.swf", 300, 500,
+                "avatar=" + URL.encodeComponent(path));
+        } else {
+            return super.createPreview(item);
+        }
+    }
+
+    protected ReviewPopup _parent;
+    protected Label _status;
+}
