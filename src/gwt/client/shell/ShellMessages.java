7426d7d1a1a3798c6e560ebf474a157110363df9
Nathan Curtis <nathan@threerings.net>
2007-02-07 00:15:06 +0000

    hah! needs this too

--- src/gwt/client/shell/ShellMessages.java
+++ src/gwt/client/shell/ShellMessages.java
@@ -8,20 +8,12 @@ package client.shell;
 public interface ShellMessages extends com.google.gwt.i18n.client.Messages {
   
   /**
-   * Translated "People".
+   * Translated "Flag".
    * 
-   * @return translated "People"
-   * @gwt.key menuPeople
+   * @return translated "Flag"
+   * @gwt.key tagFlag
    */
-  String menuPeople();
-
-  /**
-   * Translated "Submit".
-   * 
-   * @return translated "Submit"
-   * @gwt.key submit
-   */
-  String submit();
+  String tagFlag();
 
   /**
    * Translated "Stuff".
@@ -32,14 +24,6 @@ public interface ShellMessages extends com.google.gwt.i18n.client.Messages {
   String menuStuff();
 
   /**
-   * Translated "Places".
-   * 
-   * @return translated "Places"
-   * @gwt.key menuPlaces
-   */
-  String menuPlaces();
-
-  /**
    * Translated "Logon".
    * 
    * @return translated "Logon"
@@ -48,12 +32,28 @@ public interface ShellMessages extends com.google.gwt.i18n.client.Messages {
   String menuLogon();
 
   /**
-   * Translated "Me".
+   * Translated "Flag".
    * 
-   * @return translated "Me"
-   * @gwt.key menuMe
+   * @return translated "Flag"
+   * @gwt.key tagFlagFlagButton
    */
-  String menuMe();
+  String tagFlagFlagButton();
+
+  /**
+   * Translated "Add Tag:".
+   * 
+   * @return translated "Add Tag:"
+   * @gwt.key tagAddTag
+   */
+  String tagAddTag();
+
+  /**
+   * Translated "Internal Error adding tag: {0}".
+   * 
+   * @return translated "Internal Error adding tag: {0}"
+   * @gwt.key errTagInternalError
+   */
+  String errTagInternalError(String arg0);
 
   /**
    * Translated "Games".
@@ -64,12 +64,100 @@ public interface ShellMessages extends com.google.gwt.i18n.client.Messages {
   String menuGames();
 
   /**
-   * Translated "Refresh".
+   * Translated "Copyright Violation".
    * 
-   * @return translated "Refresh"
-   * @gwt.key refresh
+   * @return translated "Copyright Violation"
+   * @gwt.key tagCopyrightFlag
    */
-  String refresh();
+  String tagCopyrightFlag();
+
+  /**
+   * Translated "Mature".
+   * 
+   * @return translated "Mature"
+   * @gwt.key tagMatureFlag
+   */
+  String tagMatureFlag();
+
+  /**
+   * Translated "People".
+   * 
+   * @return translated "People"
+   * @gwt.key menuPeople
+   */
+  String menuPeople();
+
+  /**
+   * Translated "Invalid Tag: can't be more than 24 characters.".
+   * 
+   * @return translated "Invalid Tag: can't be more than 24 characters."
+   * @gwt.key errTagTooLong
+   */
+  String errTagTooLong();
+
+  /**
+   * Translated "Submit".
+   * 
+   * @return translated "Submit"
+   * @gwt.key submit
+   */
+  String submit();
+
+  /**
+   * Translated "Quick add:".
+   * 
+   * @return translated "Quick add:"
+   * @gwt.key tagQuickAdd
+   */
+  String tagQuickAdd();
+
+  /**
+   * Translated "Invalid tag: use letters, numbers, and underscore.".
+   * 
+   * @return translated "Invalid tag: use letters, numbers, and underscore."
+   * @gwt.key errTagInvalidCharacters
+   */
+  String errTagInvalidCharacters();
+
+  /**
+   * Translated "Places".
+   * 
+   * @return translated "Places"
+   * @gwt.key menuPlaces
+   */
+  String menuPlaces();
+
+  /**
+   * Translated "Me".
+   * 
+   * @return translated "Me"
+   * @gwt.key menuMe
+   */
+  String menuMe();
+
+  /**
+   * Translated "Cancel".
+   * 
+   * @return translated "Cancel"
+   * @gwt.key tagFlagCancelButton
+   */
+  String tagFlagCancelButton();
+
+  /**
+   * Translated "Flag item as {0}?".
+   * 
+   * @return translated "Flag item as {0}?"
+   * @gwt.key tagFlagPrompt
+   */
+  String tagFlagPrompt(String arg0);
+
+  /**
+   * Translated "Loading...".
+   * 
+   * @return translated "Loading..."
+   * @gwt.key tagLoading
+   */
+  String tagLoading();
 
   /**
    * Translated "Cancel".
@@ -78,4 +166,12 @@ public interface ShellMessages extends com.google.gwt.i18n.client.Messages {
    * @gwt.key cancel
    */
   String cancel();
+
+  /**
+   * Translated "Refresh".
+   * 
+   * @return translated "Refresh"
+   * @gwt.key refresh
+   */
+  String refresh();
 }
