7e1633cfb04a6da9fb73102ed2c073729c94155a
Michael Bayne <mdb@threerings.net>
2007-02-27 05:26:40 +0000

    Added GAME_TYPES.

--- src/java/com/threerings/msoy/item/web/Game.java
+++ src/java/com/threerings/msoy/item/web/Game.java
@@ -11,7 +11,10 @@ public class Game extends Item
     /** Identifies our lobby background table media. */
     public static final String TABLE_MEDIA = "table";
 
-    /** The type of game, @see GameConfig. */
+    /** Defines the number of different game types. See GameConfig. */
+    public static final int GAME_TYPES = 3;
+
+    /** The type of game, see GameConfig. */
     public byte gameType;
 
     /** The minimum number of players. */
@@ -52,7 +55,7 @@ public class Game extends Item
         // TODO: parse configuration as XML
         return config.trim().equals("avrg");
     }
-    
+
     /**
      * Returns a media descriptor for the media to be used
      * as a table background image.
