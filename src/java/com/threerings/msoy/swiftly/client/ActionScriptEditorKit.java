f4300b225d4863f272582d34c1d52fe69da95721
Jonathan Le Plastrier <jonathan@threerings.net>
2007-03-02 23:46:36 +0000

    These are no longer needed. We have better syntax highlighting and DocumentElement has become SwiftlyDocument [again].

--- src/java/com/threerings/msoy/swiftly/client/ActionScriptEditorKit.java
+++
@@ -1,9 +0,0 @@
-package com.threerings.msoy.swiftly.client;
-
-import javax.swing.text.StyledEditorKit;
-
-public class ActionScriptEditorKit extends StyledEditorKit
-{
-    // TODO override createDefaultDocument to return an ActionScriptStyledDocument
-    // registerEditorKitForContentType()
-}
