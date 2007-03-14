9eacc1f206431a8d2fd28c8c6399f6926fe683e8
Par Winzell <zell@threerings.net>
2007-03-14 18:07:12 +0000

    Don't get onto the dobj thread just to get onto an invoker thread. Also, we need some kind of security check in here. Right now it's entirely possible to just hit /game/#123 for every 123 imaginable. Surely it must be possible to have a private game in your inventory that only your friends can play...

--- src/java/com/threerings/msoy/web/server/GameServlet.java
+++ src/java/com/threerings/msoy/web/server/GameServlet.java
@@ -3,18 +3,24 @@
 
 package com.threerings.msoy.web.server;
 
+import java.util.logging.Level;
+
+import com.samskivert.io.PersistenceException;
 import com.threerings.msoy.server.MsoyServer;
 import com.threerings.msoy.server.ServerConfig;
 
+import com.threerings.msoy.item.server.persist.GameRecord;
+import com.threerings.msoy.item.server.persist.ItemRecord;
+import com.threerings.msoy.item.server.persist.ItemRepository;
 import com.threerings.msoy.item.web.Game;
 import com.threerings.msoy.item.web.Item;
-import com.threerings.msoy.item.web.ItemIdent;
 import com.threerings.msoy.item.web.MediaDesc;
 
 import com.threerings.msoy.web.client.GameService;
 import com.threerings.msoy.web.data.LaunchConfig;
 import com.threerings.msoy.web.data.ServiceException;
 import com.threerings.msoy.web.data.WebCreds;
+import com.threerings.presents.data.InvocationCodes;
 
 import static com.threerings.msoy.Log.log;
 
@@ -31,18 +37,18 @@ public class GameServlet extends MsoyServiceServlet
         // TODO: validate this user's creds
 
         // load up the metadata for this game
-        final ItemIdent ident = new ItemIdent(Item.GAME, gameId);
-        final ServletWaiter<Item> waiter = new ServletWaiter<Item>("loadItem[" + ident + "]");
-        MsoyServer.omgr.postRunnable(new Runnable() {
-            public void run () {
-                MsoyServer.itemMan.getItem(ident, waiter);
+        ItemRepository<ItemRecord, ?, ?, ?> repo = MsoyServer.itemMan.getRepository(Item.GAME);
+        GameRecord gRec;
+        try {
+            gRec = (GameRecord) repo.loadOriginalItem(gameId);
+            if (gRec == null) {
+                return null;
             }
-        });
-
-        Game game = (Game)waiter.waitForResult();
-        if (game == null) {
-            return null;
+        } catch (PersistenceException pe) {
+            log.log(Level.WARNING, "Failed to load game record [gameId=" + gameId + "]", pe);
+            throw new ServiceException(InvocationCodes.INTERNAL_ERROR);
         }
+        Game game = (Game) gRec.toItem();
 
         // create a launch config record for the game
         LaunchConfig config = new LaunchConfig();
