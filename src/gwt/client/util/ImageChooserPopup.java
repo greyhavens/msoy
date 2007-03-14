a314a6b19b9097896a9c9f6b82c5b87f24b8f73a
Par Winzell <zell@threerings.net>
2007-03-14 21:26:47 +0000

    Helpless to stop self from incurring wrath of mdb.

--- src/gwt/client/util/ImageChooserPopup.java
+++ src/gwt/client/util/ImageChooserPopup.java
@@ -3,14 +3,13 @@
 
 package client.util;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -19,7 +18,6 @@ import com.threerings.msoy.item.web.Item;
 import com.threerings.msoy.item.web.Photo;
 
 import client.shell.CShell;
-import client.shell.MsoyEntryPoint;
 import client.util.BorderedPopup;
 
 /**
@@ -34,7 +32,7 @@ public class ImageChooserPopup extends BorderedPopup
     {
         CShell.membersvc.loadInventory(CShell.creds, Item.PHOTO, new AsyncCallback() {
             public void onSuccess (Object result) {
-                new ImageChooserPopup((ArrayList)result, callback).show();
+                new ImageChooserPopup((List)result, callback).show();
             }
             public void onFailure (Throwable caught) {
                 callback.onFailure(caught);
@@ -46,7 +44,7 @@ public class ImageChooserPopup extends BorderedPopup
      * Creates an image chooser popup with the supplied list of Photo items and a callback to be
      * informed when one is selected.
      */
-    public ImageChooserPopup (ArrayList images, AsyncCallback callback)
+    public ImageChooserPopup (List images, AsyncCallback callback)
     {
         super(true);
         _callback = callback;
