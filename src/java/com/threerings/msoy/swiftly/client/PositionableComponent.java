726f55cfc78ffdf062a8cd2520494d8cb0fbe5aa
Jonathan Le Plastrier <jonathan@threerings.net>
2007-05-06 18:26:53 +0000

    Need this little guy.

---
+++ src/java/com/threerings/msoy/swiftly/client/PositionableComponent.java
@@ -0,0 +1,22 @@
+//
+// $Id$
+
+package com.threerings.msoy.swiftly.client;
+
+import java.awt.Component;
+
+/** 
+ * Provides the implementing Component with methods to chance the currently displayed position.  
+ */
+public interface PositionableComponent
+{
+    /**
+     * Returns the Component implementing this interface.
+     */
+    public Component getComponent ();
+
+    /**
+     * Requests that this component move to the row and column supplied if possible.
+     */
+    public void gotoLocation (int row, int column);
+}
