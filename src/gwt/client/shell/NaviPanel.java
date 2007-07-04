d2b765da000184c58a5a5bac0709d8b404003591
Michael Bayne <mdb@threerings.net>
2007-07-04 00:13:28 +0000

    Use friendlier name.

--- src/gwt/client/shell/NaviPanel.java
+++ src/gwt/client/shell/NaviPanel.java
@@ -112,7 +112,7 @@ public class NaviPanel extends FlexTable
         setMenu(menuidx++, "People", CShell.cmsgs.menuPeople(), new MenuPopper() {
             protected void populateMenu (Widget sender, MenuBar menu) {
                 MenuBar fmenu = new MenuBar(true);
-                fmenu.addItem("Search Profiles", true, new Command() {
+                fmenu.addItem("Find People", true, new Command() {
                     public void execute () {
                         History.newItem(Application.createLinkToken("profile", ""));
                         _popped.hide();
