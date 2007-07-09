d922d32775550cae77ddd6ba7dbf1a41bb7703e8
Ray Greenwell <ray@threerings.net>
2007-07-09 21:53:16 +0000

    Oops, this belonged to the last checkin.

---
+++ src/gwt/client/editem/ToyEditor.java
@@ -0,0 +1,28 @@
+//
+// $Id$
+
+package client.editem;
+
+import com.threerings.msoy.item.data.all.Item;
+import com.threerings.msoy.item.data.all.Toy;
+
+/**
+ * A class for creating and editing {@link Toy} digital items.
+ */
+public class ToyEditor extends ItemEditor
+{
+    // @Override from ItemEditor
+    public void setItem (Item item)
+    {
+        super.setItem(item);
+        _toy = (Toy)item;
+    }
+
+    // @Override from ItemEditor
+    public Item createBlankItem ()
+    {
+        return new Toy();
+    }
+
+    protected Toy _toy;
+}
