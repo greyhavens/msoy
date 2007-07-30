fb2f0a5d3aee961913bc208cd9c6d5f704159253
Nathan Curtis <nathan@threerings.net>
2007-07-30 22:06:58 +0000

    need the java version too...

---
+++ src/gwt/client/whirled/WhirledMessages.java
@@ -0,0 +1,33 @@
+package client.whirled;
+
+
+/**
+ * Interface to represent the messages contained in resource  bundle:
+ * 	/export/msoy/src/gwt/client/whirled/WhirledMessages.properties'.
+ */
+public interface WhirledMessages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Active People".
+   * 
+   * @return translated "Active People"
+   * @gwt.key headerPeople
+   */
+  String headerPeople();
+
+  /**
+   * Translated "Active Games".
+   * 
+   * @return translated "Active Games"
+   * @gwt.key headerGames
+   */
+  String headerGames();
+
+  /**
+   * Translated "Active Rooms".
+   * 
+   * @return translated "Active Rooms"
+   * @gwt.key headerRooms
+   */
+  String headerRooms();
+}
