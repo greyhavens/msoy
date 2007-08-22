a2659c5459b76d362bbe9b332861ab4df9af8e81
Par Winzell <zell@threerings.net>
2007-08-22 16:26:56 +0000

    Another shadow.

--- src/java/com/threerings/msoy/item/server/persist/ItemRepository.java
+++ src/java/com/threerings/msoy/item/server/persist/ItemRepository.java
@@ -64,7 +64,7 @@ public abstract class ItemRepository<
     RT extends RatingRecord<T>>
     extends DepotRepository
 {
-    @Computed
+    @Computed(shadowOf=RatingRecord.class)
     @Entity
     public static class RatingAverageRecord extends PersistentRecord {
         @Computed(fieldDefinition="count(*)")
@@ -679,7 +679,6 @@ public abstract class ItemRepository<
 
         RatingAverageRecord average =
             load(RatingAverageRecord.class,
-                 new FromOverride(getRatingClass()),
                  new Where(getRatingColumn(RatingRecord.ITEM_ID), itemId));
 
         float newRating = (float) ((average.count == 0) ? 0.0 : average.sum/average.count);
