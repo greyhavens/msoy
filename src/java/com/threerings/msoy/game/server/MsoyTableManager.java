1be5467f0c5af71a5c4c0a7f4896e794d3baf57b
Nathan Curtis <nathan@threerings.net>
2007-08-28 00:54:18 +0000

    Peerland is fun!
    don't clear out the member's pendingGame object (soon to be renamed to just game) until we're 
    actually done playing the game.

--- src/java/com/threerings/msoy/game/server/MsoyTableManager.java
+++ src/java/com/threerings/msoy/game/server/MsoyTableManager.java
@@ -3,18 +3,29 @@
 
 package com.threerings.msoy.game.server;
 
+import com.samskivert.util.ArrayIntSet;
+
 import com.threerings.crowd.data.BodyObject;
 
 import com.threerings.parlor.data.Table;
 
 import com.threerings.parlor.game.data.GameConfig;
+import com.threerings.parlor.game.data.GameObject;
 
 import com.threerings.parlor.server.TableManager;
+
+import com.threerings.presents.dobj.ObjectAddedEvent;
+import com.threerings.presents.dobj.ObjectRemovedEvent;
+import com.threerings.presents.dobj.OidListListener;
 
 import com.threerings.msoy.game.data.LobbyObject;
 import com.threerings.msoy.game.data.MsoyGameConfig;
 import com.threerings.msoy.game.data.MsoyTable;
 import com.threerings.msoy.game.data.PlayerObject;
+
+import com.threerings.msoy.data.all.MemberName;
+
+import static com.threerings.msoy.Log.log;
 
 public class MsoyTableManager extends TableManager
 {
@@ -54,15 +65,69 @@ public class MsoyTableManager extends TableManager
     @Override 
     protected Table notePlayerRemoved (int playerOid, BodyObject body)
     {
-        // mark this player as no longer "in" this game (TODO: if they are playing the game, keep
-        // them in it?)
-        if (body != null) {
-            PlayerObject plobj = (PlayerObject) body;
+        // mark this player as no longer "in" this game, unless this method is being called because
+        // the game itself started.
+        PlayerObject plobj = (PlayerObject) body;
+        if (plobj != null && !_membersPlaying.contains(plobj.getMemberId())) {
             MsoyGameServer.worldClient.updatePlayer(plobj.getMemberId(), null);
         }
 
         return super.notePlayerRemoved(playerOid, body);
     }
 
+    @Override
+    protected void gameCreated (Table table, GameObject gameobj)
+    {
+        for (int ii = 0; table.occupants != null && ii < table.occupants.length; ii++) {
+            if (table.occupants[ii] == null) {
+                continue;
+            }
+            MemberName member = (MemberName) table.occupants[ii];
+            _membersPlaying.add(member.getMemberId());
+        }
+
+        super.gameCreated(table, gameobj);
+        gameobj.addListener(_playerUpdater);
+    }
+
+    @Override
+    protected void purgeTable (Table table)
+    {
+        // check for occupants in local map - this is the last time we'll hear about this game, so
+        // make sure the players really are cleared out.
+        for (int ii = 0; table.occupants != null && ii < table.occupants.length; ii++) {
+            if (table.occupants[ii] == null) {
+                continue;
+            }
+
+            MemberName member = (MemberName) table.occupants[ii];
+            if (_membersPlaying.remove(member.getMemberId())) {
+                MsoyGameServer.worldClient.updatePlayer(member.getMemberId(), null);
+            }
+        }
+
+        super.purgeTable(table);
+    }
+
     protected LobbyObject _lobj;
+
+    protected ArrayIntSet _membersPlaying = new ArrayIntSet();
+
+    protected OidListListener _playerUpdater = new OidListListener () {
+        public void objectAdded (ObjectAddedEvent event) {
+            PlayerObject plobj = (PlayerObject) MsoyGameServer.omgr.getObject(event.getOid());
+            int memberId = plobj.getMemberId();
+            if (!_membersPlaying.contains(memberId)) {
+                MsoyGameServer.worldClient.updatePlayer(memberId, _lobj.game);
+            }
+            _membersPlaying.add(memberId);
+        }
+
+        public void objectRemoved (ObjectRemovedEvent event) {
+            PlayerObject plobj = (PlayerObject) MsoyGameServer.omgr.getObject(event.getOid());
+            int memberId = plobj.getMemberId();
+            MsoyGameServer.worldClient.updatePlayer(memberId, null);
+            _membersPlaying.remove(memberId);
+        }
+    };
 }
