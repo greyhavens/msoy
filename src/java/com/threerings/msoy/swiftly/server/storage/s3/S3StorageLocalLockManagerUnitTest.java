94136cc8aa47ef53ff2702fff4f972ea1d1a4146
Landon Fuller <landonf@threerings.net>
2007-09-07 01:04:11 +0000

    Whitespace fix: Replace eclipse's tab-magic with 4-space indents (how did that happen?)

--- src/java/com/threerings/msoy/swiftly/server/storage/s3/S3StorageLocalLockManagerUnitTest.java
+++ src/java/com/threerings/msoy/swiftly/server/storage/s3/S3StorageLocalLockManagerUnitTest.java
@@ -11,38 +11,38 @@ import com.threerings.msoy.swiftly.server.storage.s3.S3StorageLockManager.S3Obje
 import static org.junit.Assert.*;
 
 public class S3StorageLocalLockManagerUnitTest {
-	@Before
-	public void setUp () {
-		_lockMgr = new S3StorageLocalLockManager();
-	}
+    @Before
+    public void setUp () {
+        _lockMgr = new S3StorageLocalLockManager();
+    }
 
-	@Test
-	public void testLock ()
-		throws Exception
-	{
-		S3ObjectLock lock;
+    @Test
+    public void testLock ()
+        throws Exception
+    {
+        S3ObjectLock lock;
 
-		// Make sure we unlock properly
-		lock = _lockMgr.lockObject(this, "test", 20);
-		lock.unlock();
+        // Make sure we unlock properly
+        lock = _lockMgr.lockObject(this, "test", 20);
+        lock.unlock();
 
-		lock = _lockMgr.lockObject(this, "test", 20);
-		lock.unlock();
-	}
+        lock = _lockMgr.lockObject(this, "test", 20);
+        lock.unlock();
+    }
 
-	@Test(expected=S3StorageLockManager.StorageLockUnavailableException.class)
-	public void testLockTimeout ()
-		throws Exception
-	{
-		final S3ObjectLock lock = _lockMgr.lockObject(this, "test", 20);
-		
-		try {
-			// This lock will timeout
-			_lockMgr.lockObject(this, "test", 1);
-		} finally {
-			lock.unlock();
-		}
-	}
+    @Test(expected=S3StorageLockManager.StorageLockUnavailableException.class)
+    public void testLockTimeout ()
+        throws Exception
+    {
+        final S3ObjectLock lock = _lockMgr.lockObject(this, "test", 20);
+        
+        try {
+            // This lock will timeout
+            _lockMgr.lockObject(this, "test", 1);
+        } finally {
+            lock.unlock();
+        }
+    }
 
-	private S3StorageLocalLockManager _lockMgr;
+    private S3StorageLocalLockManager _lockMgr;
 }
