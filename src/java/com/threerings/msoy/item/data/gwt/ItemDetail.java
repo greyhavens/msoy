3e3ea0896a8112a6748f6ad7df12b3d6fa42577f
Michael Bayne <mdb@threerings.net>
2007-09-14 19:01:11 +0000

    Formatting.

--- src/java/com/threerings/msoy/item/data/gwt/ItemDetail.java
+++ src/java/com/threerings/msoy/item/data/gwt/ItemDetail.java
@@ -11,22 +11,21 @@ import com.threerings.msoy.data.all.MemberName;
 import com.threerings.msoy.item.data.all.Item;
 
 /**
- * This class supplies detailed information for an item, some of which is
- * relative to a given member.
+ * This class supplies detailed information for an item, some of which is relative to a given
+ * member.
  *
- * <p><em>Note:</em> this class and all derived classes are very strictly
- * limited in their contents as they must be translatable into JavaScript
- * ({@link IsSerializable}) and must work with the Presents streaming system
- * ({@link Streamable}).
+ * <p><em>Note:</em> this class and all derived classes are very strictly limited in their contents
+ * as they must be translatable into JavaScript ({@link IsSerializable}) and must work with the
+ * Presents streaming system ({@link Streamable}).
  */
 public class ItemDetail implements Streamable, IsSerializable
 {
     /** The Item of which we're a Detail. */
     public Item item;
-    
+
     /** A display-friendly expansion of Item.creatorId. */
-    public MemberName creator; 
-    
+    public MemberName creator;
+
     /** A display-friendly expansion of Item.ownerId, or null. */
     public MemberName owner;
 
