ed2b398b4a895a904e36055ec69b42b91ed703fd
Par Winzell <zell@threerings.net>
2007-09-17 21:51:47 +0000

    Squirm and squiggle to make this migration do the right thing on MySQL and PostgreSQL.

--- src/java/com/threerings/msoy/item/server/persist/CatalogIdMigration.java
+++ src/java/com/threerings/msoy/item/server/persist/CatalogIdMigration.java
@@ -10,6 +10,8 @@ import java.sql.Statement;
 
 import com.samskivert.jdbc.DatabaseLiaison;
 import com.samskivert.jdbc.JDBCUtil;
+import com.samskivert.jdbc.MySQLLiaison;
+import com.samskivert.jdbc.PostgreSQLLiaison;
 import com.samskivert.jdbc.depot.EntityMigration;
 
 import static com.threerings.msoy.Log.log;
@@ -30,6 +32,18 @@ public class CatalogIdMigration extends EntityMigration
     {
         log.info("Behold! A great big " + _tableName + " migration!");
 
+        boolean isMySQL;
+        if (liaison instanceof MySQLLiaison) {
+            isMySQL = true;
+        } else if (liaison instanceof PostgreSQLLiaison) {
+            isMySQL = false;
+        } else {
+            throw new IllegalArgumentException("Aii, we only know about MySQL and PostgreSQL.");
+        }
+
+        String tableStr = liaison.tableSQL(_tableName);
+        String columnStr = liaison.columnSQL("catalogId");
+
         // drop the old primary key on itemId
         String pkName = null;
         ResultSet rs = conn.getMetaData().getPrimaryKeys(null, null, _tableName);
@@ -42,21 +56,18 @@ public class CatalogIdMigration extends EntityMigration
         }
 
         // add the new catalogId column
-        log.info("Adding " + _tableName + ".catalogId column...");
-        liaison.addColumn(conn, _tableName, "catalogId", "integer not null", true);
+        log.info("Adding and auto-populating " + _tableName + ".catalogId column...");
+        liaison.addColumn(conn, _tableName, "catalogId", "serial unique", true);
 
-        // populate the catalogId column with unique ids
-        log.info("Populating " + _tableName + ".catalogId column...");
-        int nextId = 1, modified;
         Statement stmt = conn.createStatement();
         try {
-            do {
-                modified = stmt.executeUpdate(
-                    "update " + liaison.tableSQL(_tableName) +
-                    " set " + liaison.columnSQL("catalogId") + " = " + (nextId++) +
-                    " where " + liaison.columnSQL("catalogId") + " = 0 limit 1");
-            } while (modified != 0);
-            nextId--;
+            String update = "alter table " + tableStr + " ";
+            if (isMySQL) {
+                update += " modify " + columnStr + " integer not null";
+            } else {
+                update += " alter " + columnStr + " drop default";
+            }
+            stmt.executeUpdate(update);
 
         } finally {
             JDBCUtil.close(stmt);
@@ -69,12 +80,16 @@ public class CatalogIdMigration extends EntityMigration
         // finally set the next id in our sequence
         String seqName = _tableName.substring(0, _tableName.indexOf("CatalogRecord"));
         seqName = seqName.toUpperCase() + "_CATALOG";
-        log.info("Configuring id sequence: " + seqName + "=" + nextId);
+        log.info("Configuring id sequence: " + seqName);
         stmt = conn.createStatement();
         try {
+            // needless to say this simple function has to be called something completely
+            // different in our two supported database systems
+            String ifNull = isMySQL ? "IFNULL" : "COALESCE";
             stmt.executeUpdate(
-                "insert into " + liaison.tableSQL("IdSequences") +
-                " values ('" + seqName + "', " + nextId + ")");
+                "insert into " + liaison.tableSQL("IdSequences") + " values ('" + seqName +
+                "', (select " + ifNull + "(max(" + columnStr +")+1, 1) from " + tableStr + "))");
+
         } finally {
             JDBCUtil.close(stmt);
         }
