1dae9135ded6f3c670d02168fa28ecfc46b89e0c
Robert Zubeck <robert@threerings.net>
2007-09-25 19:37:29 +0000

    Match recent changes to panopticon. Also, start pushing logging events onto the invoker thread.

--- src/java/com/threerings/msoy/server/MsoyEventLogger.java
+++ src/java/com/threerings/msoy/server/MsoyEventLogger.java
@@ -3,16 +3,17 @@
 
 package com.threerings.msoy.server;
 
+import com.samskivert.util.Invoker;
 import com.samskivert.util.StringUtil;
 
 import com.threerings.panopticon.client.logging.EventLogger;
 import com.threerings.msoy.Log;
 
 /**
- * Event logger that sends individual event records to an appropriate logging server.
+ * Shim on EventLogger, providing Whirled-specific typesafe logging functions.
  *
- * <b>Note that this implementation is not synchronized.</b> Multiple threads should not access
- * an EventLogger concurrently. 
+ * All logging requests will be scheduled on the invoker thread, to avoid tying up
+ * the rest of the server.
  */
 public class MsoyEventLogger extends EventLogger
 {
@@ -21,34 +22,52 @@ public class MsoyEventLogger extends EventLogger
         super("com.threerings.msoy", host, port);
         Log.log.info("Events will be logged to " + host + ":" + port);
 
-        if (connect()) {
-            declareMsoySchemas();
-        }
+        connect();
     }
 
     @Override // from EventLogger
-    public void declareSchema (String table, Object ... definitions)
+    public void connect ()
+    {
+        // todo: actually connect
+
+        // every time we reconnect, make sure to redeclare our schemas
+        declareMsoySchemas();
+    }
+    
+    @Override // from EventLogger
+    public void declareSchema (String table, String[] names, Class[] types)
         throws IllegalArgumentException
     {
-        super.declareSchema(table, definitions);
+        super.declareSchema(table, names, types);
+
         // for testing only
-        Log.log.info("EventLogger define schema [table=" + table +
-                     ", data=" + definitions + "].");
+        Log.log.info("EventLogger define schema [table=" + table + ", names=" +
+                     StringUtil.toString(names) + ", types=" + StringUtil.toString(types) + "].");
     }
 
     @Override // from EventLogger
-    public void log (String table, Object ... namesAndValues)
+    public void log (String table, Object[] values)
         throws IllegalArgumentException
     {
-        super.log(table, namesAndValues);
+        super.log(table, values);
+
         // for testing only
-        Log.log.info("EventLogger processing [table=" + table +
-                     ", data=" + namesAndValues + "].");
+        Log.log.info("EventLogger processing [table=" + table + ", values=" +
+                     StringUtil.toString(values) + "].");
     }
 
     protected void declareMsoySchemas ()
     {
-        declareSchema("FlowTransaction",
-                      "type", int.class, "playerId", int.class, "time", long.class);
+        /*
+        // for testing only
+        MsoyServer.invoker.postUnit(new Invoker.Unit() {
+            public boolean invoke () {
+                declareSchema("FlowTransaction",
+                              new String[] { "type",       "playerId", "time" },
+                              new Class[]  { String.class, int.class,  Long.class });
+                return false;
+            }
+        });
+        */
     }        
 }
