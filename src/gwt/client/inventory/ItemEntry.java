da0aedd2fe0cb50b317cd4205f7dd477ad08541e
Michael Bayne <mdb@threerings.net>
2007-10-09 05:08:01 +0000

    Visually distinguish between original items and those purchased from the catalog. This may want for some artist lovin'.

--- src/gwt/client/inventory/ItemEntry.java
+++ src/gwt/client/inventory/ItemEntry.java
@@ -74,6 +74,9 @@ public class ItemEntry extends FlexTable
 
         setWidget(0, 1, MsoyUI.createActionLabel(ItemUtil.getName(item, true), clicker));
         getFlexCellFormatter().setStyleName(0, 1, "ThumbText");
+        if (_item.itemId > 0) { // if this item is an original, style it slightly differently
+            getFlexCellFormatter().addStyleName(0, 1, "OriginalThumbText");
+        }
         getFlexCellFormatter().setColSpan(0, 1, 2);
 
         setupListener();
