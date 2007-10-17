49e6ba4ba661dc4b5b7a9278f7f88573ecd8246c
Michael Bayne <mdb@threerings.net>
2007-10-17 19:18:42 +0000

    Back on the right.

--- src/gwt/client/shell/WorldClient.java
+++ src/gwt/client/shell/WorldClient.java
@@ -128,9 +128,8 @@ public class WorldClient extends Widget
         Page.displayingFlash = false;
 
         if (_isFlashClientPresent || _jclient != null) {
-//             int clientWidth = Math.max(
-//                 MIN_CLIENT_WIDTH, Window.getClientWidth() - MAX_CONTENT_WIDTH);
-            int clientWidth = MIN_CLIENT_WIDTH;
+            int clientWidth = Math.max(
+                MIN_CLIENT_WIDTH, Window.getClientWidth() - MAX_CONTENT_WIDTH);
             RootPanel.get("client").setWidth(clientWidth + "px");
             RootPanel.get("content").setWidth(Window.getClientWidth() - clientWidth + "px");
             clientMinimized(true);
