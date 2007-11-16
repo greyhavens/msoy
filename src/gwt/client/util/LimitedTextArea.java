475cd77a3de701a0498eec74cf111a2b99bd7415
Michael Bayne <mdb@threerings.net>
2007-11-16 20:48:31 +0000

    Conveniently, TextArea and other TextBoxBase derivations do not emit a ChangeEvent when you call setText() programmatically (it's probably a browser bug as they set the text in the underlying form element which should then report that it changed but it doesn't).
    So we have to wrap TextArea and hijack setText(). We could stick with
    CharacterLimiter and make a LimitedTextArea that extends TextArea, but it seems
    more convenient to make a composite widget that manages its own label and can
    be plugged right into wherever you would use a TextArea without a bunch of
    fiddling and wiring up.

---
+++ src/gwt/client/util/LimitedTextArea.java
@@ -0,0 +1,71 @@
+//
+// $Id$
+
+package client.util;
+
+import com.google.gwt.user.client.ui.KeyboardListenerAdapter;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import client.shell.CShell;
+
+/**
+ * A text area with a character limit that displays the limit to the user.
+ */
+public class LimitedTextArea extends VerticalPanel
+{
+    public LimitedTextArea (int maxChars)
+    {
+        _maxChars = maxChars;
+
+        add(_area = new TextArea());
+        _area.addKeyboardListener(_limiter);
+        setHorizontalAlignment(ALIGN_RIGHT);
+        add(_remaining = new Label());
+        _remaining.setStyleName("tipLabel");
+        updateRemaining();
+    }
+
+    public void setText (String text)
+    {
+        // we'd use a change listener here but browsers conveniently don't report a change event
+        // when a text area's text changes, awesome!
+        _area.setText(text);
+        updateRemaining();
+    }
+
+    public String getText ()
+    {
+        return _area.getText();
+    }
+
+    /**
+     * Returns the text area being limited. Don't call {@link TextArea#setText} directly on this
+     * area. Use {@link #setText}.
+     */
+    public TextArea getTextArea ()
+    {
+        return _area;
+    }
+
+    protected void updateRemaining ()
+    {
+        String text = _area.getText();
+        if (text.length() > _maxChars) {
+            _area.setText(text = text.substring(0, _maxChars));
+        }
+        _remaining.setText(CShell.emsgs.charRemaining(String.valueOf(_maxChars - text.length())));
+    }
+
+    protected KeyboardListenerAdapter _limiter = new KeyboardListenerAdapter() {
+        public void onKeyUp (Widget sender, char keyCode, int modifiers) {
+            updateRemaining();
+        }
+    };
+
+    protected int _maxChars;
+    protected TextArea _area;
+    protected Label _remaining;
+}
