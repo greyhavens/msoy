2c72e41ade7907c1fcec0137979d5e330e701bed
Michael Bayne <mdb@threerings.net>
2007-11-29 03:21:23 +0000

    A HashIntMap approximation to keep me company on those cold lonely nights when I'm surrounded by ints.

---
+++ src/gwt/client/util/HashIntMap.java
@@ -0,0 +1,27 @@
+//
+// $Id$
+
+package client.util;
+
+import java.util.HashMap;
+
+/**
+ * A HashMap with int keys. I've become spoiled by autoboxing and our own HashIntMap.
+ */
+public class HashIntMap extends HashMap
+{
+    public boolean containsKey (int key)
+    {
+        return containsKey(new Integer(key));
+    }
+
+    public Object get (int key)
+    {
+        return get(new Integer(key));
+    }
+
+    public Object put (int key, Object value)
+    {
+        return put(new Integer(key), value);
+    }
+}
