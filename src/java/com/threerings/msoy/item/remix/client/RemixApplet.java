a87fc931a830ff7347fc5851dda3e2845c61c1f9
Ray Greenwell <ray@threerings.net>
2008-01-25 04:01:23 +0000

    Beginnings of remix applet.

---
+++ src/java/com/threerings/msoy/item/remix/client/RemixApplet.java
@@ -0,0 +1,26 @@
+//
+// $Id$
+
+package com.threerings.msoy.item.remix.client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JApplet;
+
+public class RemixApplet extends JApplet
+{
+    @Override
+    public void init ()
+    {
+        super.init();
+
+        String mediaURL = getParameter("media");
+        add(new RemixPanel(mediaURL), BorderLayout.CENTER);
+    }
+
+    @Override
+    public String getAppletInfo ()
+    {
+        return "Whirled Remixer";
+    }
+}
