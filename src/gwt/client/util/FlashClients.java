a2baf38c7d7b2eb9aa0304d3ddca6dcfb8aed1f5
Ray Greenwell <ray@threerings.net>
2008-02-25 22:34:38 +0000

    Give some more vertical space to the avatarviewer so that it never cuts off the bottom of an owned avatar (owned avatars show the scale control, which takes some vertical space).

--- src/gwt/client/util/FlashClients.java
+++ src/gwt/client/util/FlashClients.java
@@ -95,7 +95,7 @@ public class FlashClients
         }
         return WidgetUtil.createFlashContainer(
             "avatarViewer", "/clients/" + DeploymentConfig.version + "/avatarviewer.swf",
-            360, 450, flashVars);
+            360, 518, flashVars);
     }
 
     /**
