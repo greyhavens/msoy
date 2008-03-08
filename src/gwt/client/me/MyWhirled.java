d14cecc2971f40b12d090bcf7389f37de90b0f0a
Michael Bayne <mdb@threerings.net>
2008-03-08 01:10:38 +0000

    Trimmity trim.

--- src/gwt/client/me/MyWhirled.java
+++ src/gwt/client/me/MyWhirled.java
@@ -3,34 +3,14 @@
 
 package client.me;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
-import com.threerings.gwt.ui.SmartTable;
-import com.threerings.gwt.ui.WidgetUtil;
-
-import com.threerings.msoy.item.data.all.MediaDesc;
 import com.threerings.msoy.item.data.all.Item;
-
-import com.threerings.msoy.person.data.Profile;
-import com.threerings.msoy.web.data.MemberCard;
 import com.threerings.msoy.web.data.MyWhirledData;
 
-import client.shell.Application;
-import client.shell.Args;
-import client.shell.Page;
-import client.util.MediaUtil;
 import client.util.MsoyCallback;
 import client.util.MsoyUI;
 import client.util.StuffNaviBar;
-import client.util.TongueBox;
 
 public class MyWhirled extends VerticalPanel
 {
@@ -55,6 +35,4 @@ public class MyWhirled extends VerticalPanel
         feed.setFeed(data.feed, false);
         add(feed);
     }
-
-    protected static final int PEOPLE_COLUMNS = 6;
 }
