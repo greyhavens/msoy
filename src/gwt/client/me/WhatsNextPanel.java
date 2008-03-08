1dbcac7289b78cb36241dff91302b45688fce238
Michael Bayne <mdb@threerings.net>
2008-03-08 00:33:31 +0000

    Display our online friends in their allotted location in the WhatsNextPanel.

--- src/gwt/client/me/WhatsNextPanel.java
+++ src/gwt/client/me/WhatsNextPanel.java
@@ -3,20 +3,30 @@
 
 package client.me;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.threerings.gwt.ui.EnterClickAdapter;
 import com.threerings.gwt.ui.SmartTable;
 import com.threerings.gwt.ui.WidgetUtil;
 
+import com.threerings.msoy.item.data.all.MediaDesc;
+import com.threerings.msoy.web.data.MemberCard;
 import com.threerings.msoy.web.data.MyWhirledData;
 
 import client.images.next.NextImages;
@@ -25,6 +35,7 @@ import client.shell.Args;
 import client.shell.Page;
 import client.util.MsoyUI;
 import client.util.RoundBox;
+import client.util.ThumbBox;
 
 /**
  * Displays blurbs about things to do and a player's online friends.
@@ -38,7 +49,7 @@ public class WhatsNextPanel extends SmartTable
         setWidget(0, 0, createPlay(data), 1, "Play");
         setWidget(0, 1, createExplore(data), 1, "Explore");
         if (data.friends.size() == 0) {
-            setWidget(0, 2, createNoFriends(data), 1, "Friends");
+            setWidget(0, 2, createNoFriends(data), 1, "NoFriends");
         } else {
             setWidget(0, 2, createFriends(data), 1, "Friends");
         }
@@ -125,10 +136,69 @@ public class WhatsNextPanel extends SmartTable
 
     protected Widget createFriends (MyWhirledData data)
     {
-        SmartTable friends = new SmartTable(0, 0);
-        friends.setHeight("100%");
-        friends.setText(0, 0, CMe.msgs.nextFriends(), 1, "Title");
-        return friends;
+        FlowPanel friends = new FlowPanel();
+        friends.add(MsoyUI.createLabel(CMe.msgs.nextFriends(), "Title"));
+        friends.add(MsoyUI.createLabel(CMe.msgs.nextFriendClick(), "ClickTip"));
+
+        // if we have few friends, show larger photo images
+        int size = (data.friends.size() > 6) ?
+            MediaDesc.QUARTER_THUMBNAIL_SIZE : MediaDesc.HALF_THUMBNAIL_SIZE;
+
+        // group our friends by location (in rooms or games)
+        Map games = new HashMap();
+        Map rooms = new HashMap();
+        for (int ii = 0; ii < data.friends.size(); ii++) {
+            MemberCard card = (MemberCard)data.friends.get(ii);
+            if (card.status instanceof MemberCard.InScene) {
+                int sceneId = ((MemberCard.InScene)card.status).sceneId;
+                FlowPanel room = getPlacePanel(
+                    rooms, sceneId, ((MemberCard.InScene)card.status).sceneName);
+                ClickListener onClick = Application.createLinkListener(Page.WORLD, "s"+sceneId);
+                Widget member = makeMemberWidget(card, size, onClick);
+                member.addStyleName("MemberRoom");
+                room.add(member);
+
+            } else if (card.status instanceof MemberCard.InGame) {
+                int gameId = ((MemberCard.InGame)card.status).gameId;
+                FlowPanel game = getPlacePanel(
+                    games, gameId, ((MemberCard.InGame)card.status).gameName);
+                ClickListener onClick = Application.createLinkListener(
+                    Page.WORLD, Args.compose("game", "l", ""+gameId));
+                Widget member = makeMemberWidget(card, size, onClick);
+                member.addStyleName("MemberGame");
+                game.add(member);
+            }
+        }
+
+        // now add the rooms and games to our scrolling contents (rooms first)
+        for (Iterator iter = rooms.values().iterator(); iter.hasNext(); ) {
+            friends.add((FlowPanel)iter.next());
+        }
+        for (Iterator iter = games.values().iterator(); iter.hasNext(); ) {
+            friends.add((FlowPanel)iter.next());
+        }
+
+        return new ScrollPanel(friends);
+    }
+
+    protected FlowPanel getPlacePanel (Map places, int placeId, String placeName)
+    {
+        Integer key = new Integer(placeId);
+        FlowPanel place = (FlowPanel)places.get(key);
+        if (place == null) {
+            places.put(key, place = new FlowPanel());
+            place.add(MsoyUI.createLabel(placeName, "PlaceName"));
+        }
+        return place;
+    }
+
+    protected Widget makeMemberWidget (MemberCard card, int size, ClickListener onClick)
+    {
+        SmartTable member = new SmartTable("Member", 0, 0);
+        member.setWidget(0, 0, new ThumbBox(card.photo, size, onClick), 1, "Photo");
+        member.setWidget(0, 1, MsoyUI.createActionLabel(card.name.toString(), null, onClick),
+                         1, "Name");
+        return member;
     }
 
     /** Our screenshot images. */
