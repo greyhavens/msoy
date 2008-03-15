bf8c7d28cd1cac5b9bb06ada91005611da6a7440
Michael Bayne <mdb@threerings.net>
2008-03-15 22:25:50 +0000

    Coming soon.

---
+++ src/gwt/client/account/WelcomePanel.java
@@ -0,0 +1,19 @@
+//
+// $Id$
+
+package client.account;
+
+import com.threerings.gwt.ui.SmartTable;
+
+/**
+ * Displays a welcome message to newly registered users.
+ */
+public class WelcomePanel extends SmartTable
+{
+    public WelcomePanel ()
+    {
+        super("welcome", 0, 10);
+
+        // TODO
+    }
+}
