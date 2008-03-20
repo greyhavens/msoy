67d8c8899948087630031f635de2e11a1729d7e2
Robert Zubeck <robert@threerings.net>
2008-03-20 20:32:54 +0000

    Nope, that was right after all.

--- src/gwt/client/util/FlashClients.java
+++ src/gwt/client/util/FlashClients.java
@@ -95,7 +95,7 @@ public class FlashClients
         }
         return WidgetUtil.createFlashContainer(
             "avatarViewer", "/clients/" + DeploymentConfig.version + "/avatarviewer.swf",
-            320, 385, flashVars);
+            360, 385, flashVars);
     }
 
     /**
