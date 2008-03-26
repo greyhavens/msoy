85f3a0fde8d3a004c9565cb1111f12155abd5b6f
Robert Zubeck <robert@threerings.net>
2008-03-26 01:51:28 +0000

    Inspired by MDB, here's a little servlet that returns some basic user stats for the currently logged-in user, as a JSON object (or a 403 if the user's not logged in). For example:   {"level":8, "coins":1900, "friendsOnline":0, "name":"robert"}
    It's tiny, but enough to get me started on my grand plans for a Whirled desktop 
    widget for Vista. :)

---
+++ src/java/com/threerings/msoy/web/server/MyStatsServlet.java
@@ -0,0 +1,84 @@
+//
+// $Id$
+
+package com.threerings.msoy.web.server;
+
+import static com.threerings.msoy.Log.log;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.logging.Level;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.samskivert.io.StreamUtil;
+import com.samskivert.servlet.util.CookieUtil;
+import com.samskivert.util.IntSet;
+import com.threerings.msoy.server.MsoyServer;
+import com.threerings.msoy.server.persist.MemberRecord;
+import com.threerings.msoy.web.data.MemberCard;
+import com.threerings.msoy.web.data.WebIdent;
+
+/**
+ * Reports server status in plain text.
+ */
+public class MyStatsServlet extends HttpServlet
+{
+    @Override // from HttpServlet
+    protected void doGet (HttpServletRequest req, HttpServletResponse rsp)
+        throws IOException
+    {
+        try {
+            // pull out session token from the request header
+            String token = CookieUtil.getCookieValue(req, "creds");
+            if (token == null) {
+                rsp.sendError(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
+
+            // make sure the user is authenticated, and pull out their record object
+            WebIdent ident = new WebIdent(ServletUtil.getMemberId(token), token);
+            MemberRecord member = MsoyServiceServlet.getAuthedUser(ident);
+
+            if (member == null) {
+                rsp.sendError(HttpServletResponse.SC_FORBIDDEN);
+                return;
+            }
+
+            // now get their friend ids
+            IntSet friendIds = MsoyServer.memberRepo.loadFriendIds(member.memberId);
+            List<MemberCard> friends = ServletUtil.resolveMemberCards(friendIds, true, friendIds);
+
+            // and print out the response
+            String results = makeResults(member, friends);
+            rsp.getOutputStream().println(results);
+            StreamUtil.close(rsp.getOutputStream());
+
+        } catch (Exception e) {
+            log.log(Level.WARNING, "Failed to gather user stats.", e);
+            rsp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+    }
+
+    protected String makeResults (MemberRecord member, List<MemberCard> friends)
+        throws JSONException, UnsupportedEncodingException
+    {
+        JSONObject result = new JSONObject();
+        
+        result.put("name", URLEncoder.encode(member.name, "UTF-8"));
+        result.put("coins", member.flow);
+        result.put("level", member.level);
+        result.put("friendsOnline", friends.size());
+        
+        return result.toString();
+    }
+}
