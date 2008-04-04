8f30681592c79cc50aa73cad4c8322ef3b15bd2f
Michael Bayne <mdb@threerings.net>
2008-04-04 19:51:23 +0000

    A handle focus listener that makes text input AJAXy.

---
+++ src/gwt/client/util/DefaultTextListener.java
@@ -0,0 +1,47 @@
+//
+// $Id$
+
+package client.util;
+
+import com.google.gwt.user.client.ui.FocusListener;
+import com.google.gwt.user.client.ui.TextBoxBase;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Displays default text in a text box or area and clears that text out when the box is focused.
+ */
+public class DefaultTextListener
+    implements FocusListener
+{
+    public static void configure (TextBoxBase target, String defaultText)
+    {
+        DefaultTextListener listener = new DefaultTextListener(target, defaultText);
+        target.addFocusListener(listener);
+    }
+
+    // from interface FocusListener
+    public void onFocus (Widget widget)
+    {
+        if (_target.getText().equals(_defaultText)) {
+            _target.setText("");
+        }
+    }
+
+    // from interface FocusListener
+    public void onLostFocus (Widget sender)
+    {
+        if (_target.getText().trim().equals("")) {
+            _target.setText(_defaultText);
+        }
+    }
+
+    protected DefaultTextListener (TextBoxBase target, String defaultText)
+    {
+        _target = target;
+        _defaultText = defaultText;
+        _target.setText(defaultText);
+    }
+
+    protected TextBoxBase _target;
+    protected String _defaultText;
+}
