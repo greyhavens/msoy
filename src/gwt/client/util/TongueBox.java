73c2167a101de446e753f922336164d7e149251b
Michael Bayne <mdb@threerings.net>
2008-04-10 17:43:22 +0000

    Adjust the way we handle footer labels to avoid them stretching across the entire width of the tongue box.

--- src/gwt/client/util/TongueBox.java
+++ src/gwt/client/util/TongueBox.java
@@ -4,6 +4,8 @@
 package client.util;
 
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HasAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
@@ -77,8 +79,12 @@ public class TongueBox extends SmartTable
 
     public Label setFooterLabel (String text, ClickListener onClick)
     {
+        // annoyingly we have to stick our label into a table otherwise the Label (a div) will
+        // actually be the entire width of the page; if it's clickable, that is very weird
+        HorizontalPanel box = new HorizontalPanel();
         Label label = MsoyUI.createActionLabel(text, onClick);
-        setFooter(label);
+        box.add(label);
+        setFooter(box);
         return label;
     }
 
@@ -88,6 +94,7 @@ public class TongueBox extends SmartTable
             clearCell(2, 0);
         } else {
             setWidget(2, 0, widget, 1, "Footer");
+            getFlexCellFormatter().setHorizontalAlignment(2, 0, HasAlignment.ALIGN_RIGHT);
         }
     }
 }
