bd39c0339f2adea5e804baba77557ebc2146ad4a
Ray Greenwell <ray@threerings.net>
2008-04-14 00:58:41 +0000

    Added a TODO, someone else more familiar with GWT might know how to fix this in a fraction of the time it would take me.

--- src/gwt/client/util/MsoyCallback.java
+++ src/gwt/client/util/MsoyCallback.java
@@ -17,5 +17,15 @@ public abstract class MsoyCallback implements AsyncCallback
     {
         MsoyUI.error(CShell.serverError(cause));
         CShell.log("Service request failed", cause);
+        // TODO: It seems 3 possible things happen on failure:
+        // 1) The failure comes back and this displays a message and everything is fine.
+        // 2) The URL was changed in the browser, then this failure occurs retrieving
+        // content for the new page and the user is left viewing their old page but with
+        // an incorrect URL.
+        // 3) Somtimes, the user tries to load a URL fresh, this failure occurs, and the user
+        // sees the nice error message but is left on a totally blank page.
+        //
+        // 2 & 3 should be handled, and for #3 the user should be directed to the home page or
+        // given a 404-type page that has a link to the home page.
     }
 }
