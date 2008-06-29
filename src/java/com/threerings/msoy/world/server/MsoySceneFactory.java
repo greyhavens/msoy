049d849d097becd6232f6c29d1df66b55b9c609e
Michael Bayne <mdb@threerings.net>
2008-06-29 14:21:53 +0000

    This is now injected.

--- src/java/com/threerings/msoy/world/server/MsoySceneFactory.java
+++ src/java/com/threerings/msoy/world/server/MsoySceneFactory.java
@@ -3,6 +3,8 @@
 
 package com.threerings.msoy.world.server;
 
+import com.google.inject.Singleton;
+
 import com.threerings.crowd.data.PlaceConfig;
 
 import com.threerings.whirled.data.Scene;
@@ -17,20 +19,17 @@ import com.threerings.msoy.world.data.MsoySceneModel;
 /**
  * Scene and config factory for Msoy.
  */
+@Singleton
 public class MsoySceneFactory
     implements SceneFactory, SceneRegistry.ConfigFactory
 {
-    public MsoySceneFactory ()
-    {
-    }
-
-    // documentation inherited from interface SceneFactory
+    // from interface SceneFactory
     public Scene createScene (SceneModel model, PlaceConfig config)
     {
         return new MsoyScene((MsoySceneModel) model, config);
     }
 
-    // documentation inherited from interface SceneRegistry.ConfigFactory
+    // from interface SceneRegistry.ConfigFactory
     public PlaceConfig createPlaceConfig (SceneModel smodel)
     {
         MsoySceneModel model = (MsoySceneModel) smodel;
