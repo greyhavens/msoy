734270e464e53e551f74869de04d7a2112574dd1
Par Winzell <zell@threerings.net>
2008-07-09 10:00:48 +0000

    A new distributed object for communication between the game server and the agent only. Will presumably need ActionScript version.

---
+++ src/java/com/threerings/msoy/avrg/data/AVRGameAgentObject.java
@@ -0,0 +1,71 @@
+//
+// $Id: AVRGameObject.java 8844 2008-04-15 17:05:43Z nathan $
+
+package com.threerings.msoy.avrg.data;
+
+import com.threerings.presents.dobj.DObject;
+import com.threerings.presents.dobj.DSet;
+
+/**
+ * The data shared between server and agent for an AVR game.
+ */
+public class AVRGameAgentObject extends DObject
+{
+    // AUTO-GENERATED: FIELDS START
+    /** The field name of the <code>scenes</code> field. */
+    public static final String SCENES = "scenes";
+    // AUTO-GENERATED: FIELDS END
+
+    /** A set of scenes containing (or having recently contained) players of this AVRG. */
+    public DSet<SceneInfo> scenes = new DSet<SceneInfo>();
+
+    // AUTO-GENERATED: METHODS START
+    /**
+     * Requests that the specified entry be added to the
+     * <code>scenes</code> set. The set will not change until the event is
+     * actually propagated through the system.
+     */
+    public void addToScenes (SceneInfo elem)
+    {
+        requestEntryAdd(SCENES, scenes, elem);
+    }
+
+    /**
+     * Requests that the entry matching the supplied key be removed from
+     * the <code>scenes</code> set. The set will not change until the
+     * event is actually propagated through the system.
+     */
+    public void removeFromScenes (Comparable key)
+    {
+        requestEntryRemove(SCENES, scenes, key);
+    }
+
+    /**
+     * Requests that the specified entry be updated in the
+     * <code>scenes</code> set. The set will not change until the event is
+     * actually propagated through the system.
+     */
+    public void updateScenes (SceneInfo elem)
+    {
+        requestEntryUpdate(SCENES, scenes, elem);
+    }
+
+    /**
+     * Requests that the <code>scenes</code> field be set to the
+     * specified value. Generally one only adds, updates and removes
+     * entries of a distributed set, but certain situations call for a
+     * complete replacement of the set value. The local value will be
+     * updated immediately and an event will be propagated through the
+     * system to notify all listeners that the attribute did
+     * change. Proxied copies of this object (on clients) will apply the
+     * value change when they received the attribute changed notification.
+     */
+    public void setScenes (DSet<com.threerings.msoy.avrg.data.SceneInfo> value)
+    {
+        requestAttributeChange(SCENES, value, this.scenes);
+        @SuppressWarnings("unchecked") DSet<com.threerings.msoy.avrg.data.SceneInfo> clone =
+            (value == null) ? null : value.typedClone();
+        this.scenes = clone;
+    }
+    // AUTO-GENERATED: METHODS END
+}
---
+++ src/java/com/threerings/msoy/avrg/data/SceneInfo.java
@@ -0,0 +1,65 @@
+//
+// $Id: GameState.java 8844 2008-04-15 17:05:43Z nathan $
+
+package com.threerings.msoy.avrg.data;
+
+import com.google.common.collect.Comparators;
+
+import com.threerings.io.SimpleStreamableObject;
+
+import com.threerings.presents.dobj.DSet;
+
+/**
+ * Helps an AVRG keep track of which world server is currently hosting a given scene. 
+ */
+public class SceneInfo extends SimpleStreamableObject
+    implements DSet.Entry, Comparable<SceneInfo>
+{
+    /** The id of the scene occupied by one or more players in our AVRG. */
+    public int sceneId;
+
+    /** The hostname of the world server hosting this scene. */
+    public String hostname;
+
+    /** The port on which to connect to said world server. */
+    public int port;
+
+    public SceneInfo ()
+    {
+    }
+
+    public SceneInfo (int sceneId, String hostname, int port)
+    {
+        this.sceneId = sceneId;
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    // from interface DSet.Entry
+    public Comparable getKey ()
+    {
+        return sceneId;
+    }
+
+    // from interface Comparable<SceneInfo>
+    public int compareTo (SceneInfo other)
+    {
+        return Comparators.compare(sceneId, other.sceneId);
+    }
+
+    @Override // from Object
+    public boolean equals (Object other)
+    {
+        if (other == null || !other.getClass().equals(getClass())) {
+            return false;
+        }
+        SceneInfo info = (SceneInfo) other;
+        return sceneId == info.sceneId && hostname.equals(info.hostname) && port == info.port;
+    }
+
+    @Override // from Object
+    public int hashCode ()
+    {
+        return sceneId;
+    }
+}
