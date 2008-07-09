d53e43a2d1019d7ba502a4ed0da207377b8de93c
Jamie Doornbos <jamie@threerings.net>
2008-07-09 23:23:40 +0000

    Removed debug log message (from debugging client factory issues yesterday)

--- src/java/com/threerings/msoy/bureau/data/BureauLauncherClientObject.java
+++ src/java/com/threerings/msoy/bureau/data/BureauLauncherClientObject.java
@@ -4,9 +4,5 @@ import com.threerings.presents.data.ClientObject;
 
 public class BureauLauncherClientObject extends ClientObject
 {
-    public BureauLauncherClientObject ()
-    {
-        com.threerings.msoy.Log.log.info("Created client obj", "class", getClass());
-    }
 }
 
