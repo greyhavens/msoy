6c66f079fbb6805b9e4a124c007c1736d8e38181
Tom Conkling <tim@threerings.net>
2008-07-10 23:00:15 +0000

    - BadgeUtil moved - Badge.Type -> BadgeType - create the BadgeType:crcHash mapping in a static block, to avoid static initialization order weirdness

--- src/java/com/threerings/msoy/badge/data/Badge.java
+++ src/java/com/threerings/msoy/badge/data/Badge.java
@@ -3,109 +3,13 @@
 
 package com.threerings.msoy.badge.data;
 
-import java.util.zip.CRC32;
-
-import com.samskivert.util.HashIntMap;
-import com.threerings.io.SimpleStreamableObject;
-import com.threerings.msoy.data.MemberObject;
-import com.threerings.stats.Log;
-
 import com.google.gwt.user.client.rpc.IsSerializable;
+
+import com.threerings.io.SimpleStreamableObject;
 
 public abstract class Badge extends SimpleStreamableObject
     implements IsSerializable
 {
-    /**
-     * Defines the various badge categories, which can be used to suggest to the user
-     * which badges to pursue next, based on their past activity.
-     */
-    public static enum Category
-    {
-        SOCIAL, GAME, CREATION, EXPLORATION
-    };
-
-    /** Defines the various badge types. */
-    public static enum Type
-    {
-        FRIEND_1(Category.SOCIAL, 1000),
-
-        ;
-
-        /**
-         * Badge types can override this to apply constraints to Badges (e.g., only unlocked when
-         * another badge is earned.)
-         */
-        public boolean isUnlocked (MemberObject user) {
-            return true;
-        }
-
-        /**
-         * Overridden by badge types to indicate whether the specified user qualifies
-         * for this badge.
-         */
-        public String getProgress (MemberObject user) {
-            return "";
-        }
-
-        /**
-         * Returns the unique code for this badge type, which is a function of its name.
-         */
-        public final int getCode() {
-            return _code;
-        }
-
-        /**
-         * Returns the Category this badge falls under.
-         */
-        public Category getCategory () {
-            return _category;
-        }
-
-        /**
-         * Returns the number of coins awarded to a player who completes this badge.
-         */
-        public int getCoinValue () {
-            return _coinValue;
-        }
-
-        Type (Category category, int coinValue) {
-            _category = category;
-            _coinValue = coinValue;
-
-            // compute a code for this Type using the CRC32 hash of its name
-            _code = CRCUtil.crc32(name());
-
-            if (_codeToType == null) {
-                _codeToType = new HashIntMap<Type>();
-            }
-            if (_codeToType.containsKey(_code)) {
-                Log.log.warning("Badge type collision! " + this + " and " + _codeToType.get(_code) +
-                    " both map to '" + _code + "'.");
-            } else {
-                _codeToType.put(_code, this);
-            }
-        }
-
-        protected Category _category;
-        protected int _coinValue;
-        protected int _code;
-
-        /**
-         * Helper class to calculate CRC hashes for Badge types.
-         */
-        protected static class CRCUtil
-        {
-            public static int crc32 (String value)
-            {
-                _crc.reset();
-                _crc.update(value.getBytes());
-                return (int) _crc.getValue();
-            }
-
-            protected static CRC32 _crc = new CRC32();
-        }
-    };
-
     /** The unique code representing the type of this badge. */
     public int badgeCode;
 
@@ -121,19 +25,8 @@ public abstract class Badge extends SimpleStreamableObject
     //public String getImageUrl (); TODO
 
     /** Returns this Badge's Type */
-    public Type getType ()
+    public BadgeType getType ()
     {
-        return getType(badgeCode);
+        return BadgeType.getType(badgeCode);
     }
-
-    /**
-     * Maps a {@link Type}'s code code back to a {@link Type} instance.
-     */
-    public static Type getType (int code)
-    {
-        return _codeToType.get(code);
-    }
-
-    /** The table mapping stat codes to enumerated types. */
-    protected static HashIntMap<Type> _codeToType;
 }
---
+++ src/java/com/threerings/msoy/badge/data/BadgeType.java
@@ -0,0 +1,138 @@
+//
+// $Id$
+
+package com.threerings.msoy.badge.data;
+
+import java.util.zip.CRC32;
+
+import com.samskivert.util.HashIntMap;
+import com.threerings.msoy.data.MemberObject;
+import com.threerings.stats.Log;
+
+/** Defines the various badge types. */
+public enum BadgeType
+{
+    // social badges
+    FRIEND_1(Category.SOCIAL, 1000) {
+        public boolean getProgress (MemberObject user) {
+            return user.stats.getIntStat(StatType.FRIENDS_MADE) >= 1;
+        }
+    },
+
+    FRIEND_2(Category.SOCIAL, 1000) {
+        public boolean getProgress (MemberObject user) {
+            return user.stats.getIntStat(StatType.FRIENDS_MADE) >= 5;
+        }
+    },
+
+    ;
+
+    /**
+     * Defines the various badge categories, which can be used to suggest to the user
+     * which badges to pursue next, based on their past activity.
+     */
+    public static enum Category
+    {
+        SOCIAL, GAME, CREATION, EXPLORATION
+    };
+
+    /**
+     * Maps a {@link BadgeType}'s code back to a {@link BadgeType} instance.
+     */
+    public static BadgeType getType (int code)
+    {
+        return _codeToType.get(code);
+    }
+
+    /**
+     * Badge types can override this to apply constraints to Badges (e.g., only unlocked when
+     * another badge is earned.)
+     */
+    public boolean isUnlocked (MemberObject user)
+    {
+        return true;
+    }
+
+    /**
+     * Overridden by badge types to indicate whether the specified user qualifies
+     * for this badge.
+     * TODO: eventually, this function will be changed to return a String indicating
+     * how much progress has been made. For now, it returns true if the Badge has been earned.
+     */
+    public boolean getProgress (MemberObject user)
+    {
+        return false.
+    }
+
+    /**
+     * Returns the unique code for this badge type, which is a function of its name.
+     */
+    public final int getCode()
+    {
+        return _code;
+    }
+
+    /**
+     * Returns the Category this badge falls under.
+     */
+    public Category getCategory ()
+    {
+        return _category;
+    }
+
+    /**
+     * Returns the number of coins awarded to a player who completes this badge.
+     */
+    public int getCoinValue ()
+    {
+        return _coinValue;
+    }
+
+    BadgeType (Category category, int coinValue)
+    {
+        _category = category;
+        _coinValue = coinValue;
+    }
+
+    /**
+     * Create the hash<->BadgeType mapping for each BadgeType.
+     * This is done in a static block because it's an error for an enum
+     * to access its static members in its constructor.
+     */
+    static
+    {
+        for (BadgeType type : BadgeType.values()) {
+            type._code = mapCodeForType(type);
+        }
+    }
+
+    protected static int mapCodeForType (BadgeType type)
+    {
+        // compute the CRC32 hash
+        _crc.reset();
+        _crc.update(type.name().getBytes());
+        int code = (int) _crc.getValue();
+
+        // store the hash in a map
+        if (_codeToType == null) {
+            _codeToType = new HashIntMap<BadgeType>();
+        }
+        if (_codeToType.containsKey(code)) {
+            Log.log.warning("Badge type collision! " + type + " and " + _codeToType.get(code) +
+                " both map to '" + code + "'.");
+        } else {
+            _codeToType.put(code, type);
+        }
+
+        return code;
+    }
+
+    protected Category _category;
+    protected int _coinValue;
+    protected int _code;
+
+    /** The table mapping stat codes to enumerated types. */
+    protected static HashIntMap<BadgeType> _codeToType;
+
+    protected static CRC32 _crc = new CRC32();
+};
