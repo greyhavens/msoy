c2c298354c46f71c16f8ef59b3e049dac7b645eb
Sarah Northway <sarah@threerings.net>
2008-07-11 20:26:30 +0000

    Bypass the welcome screen and dump new registrants directly into their home room.

--- src/gwt/client/account/index.java
+++ src/gwt/client/account/index.java
@@ -39,8 +39,8 @@ public class index extends Page
             setContent(CAccount.msgs.createTitle(), 
                 new CreateAccountPanel(new CreateAccountPanel.RegisterListener() {
                     public void didRegister () {
-                        _onLogonPage = ACCOUNT;
-                        _onLogonArgs = "welcome";
+                        _onLogonPage = WORLD;                       
+                        _onLogonArgs = "h";
                     }
                 }));
 
