302b66005d615548cf795668ce35600d3041e483
Sarah Northway <sarah@threerings.net>
2008-07-15 22:26:21 +0000

    Update the schema version to be sure the defaults are taken out.

--- src/java/com/threerings/msoy/admin/server/persist/ABTestRecord.java
+++ src/java/com/threerings/msoy/admin/server/persist/ABTestRecord.java
@@ -149,7 +149,7 @@ public class ABTestRecord extends PersistentRecord
 
     /** Increment this value if you modify the definition of this persistent object in a way that
      * will result in a change to its SQL counterpart. */
-    public static final int SCHEMA_VERSION = 1;
+    public static final int SCHEMA_VERSION = 2;
 
     /**
      * Build a POJO version of this Record, for use outside the persistence system.
