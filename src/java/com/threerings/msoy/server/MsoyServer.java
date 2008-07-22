fae5e045b5757ae1d3ea9acb30719a69ec95acd6
Ray Greenwell <ray@threerings.net>
2008-07-22 02:26:48 +0000

    This import was in the copy I tested with, then I updated before checking in...

--- src/java/com/threerings/msoy/server/MsoyServer.java
+++ src/java/com/threerings/msoy/server/MsoyServer.java
@@ -29,6 +29,7 @@ import com.threerings.presents.server.ClientFactory;
 import com.threerings.presents.server.ClientResolver;
 import com.threerings.presents.server.InvocationException;
 import com.threerings.presents.server.PresentsClient;
+import com.threerings.presents.server.PresentsServer;
 import com.threerings.presents.server.PresentsDObjectMgr;
 import com.threerings.presents.server.ShutdownManager;
 import com.threerings.presents.client.InvocationService;
