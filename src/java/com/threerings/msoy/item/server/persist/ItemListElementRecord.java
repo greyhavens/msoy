28b1a0f3fa2c96e1df363441091651c67a890ad8
Michael Bayne <mdb@threerings.net>
2008-07-29 10:39:18 +0000

    Nixed no longer needed imports.

--- src/java/com/threerings/msoy/item/server/persist/ItemListElementRecord.java
+++ src/java/com/threerings/msoy/item/server/persist/ItemListElementRecord.java
@@ -7,8 +7,6 @@ import com.samskivert.jdbc.depot.Key;
 import com.samskivert.jdbc.depot.PersistentRecord;
 import com.samskivert.jdbc.depot.annotation.Entity;
 import com.samskivert.jdbc.depot.annotation.Id;
-import com.samskivert.jdbc.depot.annotation.Index;
-import com.samskivert.jdbc.depot.annotation.UniqueConstraint;
 import com.samskivert.jdbc.depot.expression.ColumnExp;
 
 import com.threerings.io.Streamable;
