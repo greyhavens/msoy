33b4742f99bbc34f764a9c62fb32341ddee08fc3
Matt Jensen <matt@threerings.net>
2008-07-29 00:46:13 +0000

    Not that this is being built anyway, but fixed import.

--- src/java/com/threerings/msoy/game/chiyogami/server/BossObject.java
+++ src/java/com/threerings/msoy/game/chiyogami/server/BossObject.java
@@ -13,7 +13,7 @@ import com.threerings.msoy.data.MsoyBodyObject;
 import com.threerings.msoy.item.data.all.Item;
 import com.threerings.msoy.item.data.all.Avatar;
 import com.threerings.msoy.item.data.all.ItemIdent;
-import com.threerings.msoy.item.data.all.MediaDesc;
+import com.threerings.msoy.data.all.MediaDesc;
 
 import com.threerings.msoy.world.data.ActorInfo;
 
