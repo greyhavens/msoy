24e5407e45191c40bd770d0d9231b1312db854b1
Michael Bayne <mdb@threerings.net>
2008-08-05 16:21:19 +0000

    Alas, neither sed nor Eclipse reminded me to svn add this file.

---
+++ src/gwt/client/shell/Pages.java
@@ -0,0 +1,40 @@
+//
+// $Id$
+
+package client.shell;
+
+/**
+ * Enumerates all of our available pages.
+ */
+public enum Pages
+{
+    ACCOUNT(Frame.Tabs.ME),
+    ADMIN(Frame.Tabs.ME),
+    CREATE(null), // TODO: Frame.Tabs.CREATE
+    GAMES(Frame.Tabs.GAMES), 
+    HELP(Frame.Tabs.HELP),
+    LANDING(null),
+    MAIL(Frame.Tabs.ME),
+    ME(Frame.Tabs.ME),
+    PEOPLE(Frame.Tabs.FRIENDS),
+    SHOP(Frame.Tabs.SHOP),
+    STUFF(Frame.Tabs.ME),
+    SUPPORT(Frame.Tabs.HELP),
+    SWIFTLY(null), // TODO: Frame.Tabs.CREATE
+    WHIRLEDS(Frame.Tabs.WHIRLEDS),
+    WORLD(null);
+
+    public String getPath () {
+    	return toString().toLowerCase();
+    }
+    
+    public Frame.Tabs getTab () {
+        return _tab;
+    }
+
+    Pages (Frame.Tabs tab) {
+        _tab = tab;
+    }
+
+    protected Frame.Tabs _tab;
+}
