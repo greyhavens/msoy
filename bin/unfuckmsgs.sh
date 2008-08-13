e2fc27df8c709f43f2a2556cf2cb164784597482
Michael Bayne <mdb@threerings.net>
2008-08-13 22:18:42 +0000

    Fucking svnclean can't handle an unmodified file with a changed timestamp.

--- bin/unfuckmsgs.sh
+++ bin/unfuckmsgs.sh
@@ -3,6 +3,10 @@
 # Works around a bug in GWT's I18NSync class; when they fix it, this can go away
 
 for FILE in `find src/gwt -name '*Messages.java'`; do
-    grep -v gwt.key $FILE > $FILE.new
-    mv $FILE.new $FILE
+    if [ "$FILE" != "src/gwt/client/shell/DynamicMessages.java" -a \
+        "$FILE" != "src/gwt/client/shell/ServerMessages.java" ]; then
+        echo Fixing $FILE
+        grep -v gwt.key $FILE > $FILE.new
+        mv $FILE.new $FILE
+    fi
 done
