7a39f08d53d06e48bfcfaaca3bf5f3335f55dd79
Nathan Curtis <nathan@threerings.net>
2008-08-13 15:28:18 +0000

    Not only would that code not have worked, the value was already being computed a different way.

--- src/gwt/client/item/ItemRating.java
+++ src/gwt/client/item/ItemRating.java
@@ -111,9 +111,8 @@ public class ItemRating extends FlexTable
     {
         final boolean isFirstRating = _memberRating == 0;
         _playerStars.setRating(_memberRating = newRating);
-        boolean previouslyRated = _playerStars.getRating() != 0;
         ItemIdent ident = _item.getIdent();
-        _itemsvc.rateItem(ident, newRating, previouslyRated, new MsoyCallback<Float>() {
+        _itemsvc.rateItem(ident, newRating, !isFirstRating, new MsoyCallback<Float>() {
             public void onSuccess (Float result) {
                 _averageStars.setRating(_item.rating = result);
                 if (isFirstRating) {
