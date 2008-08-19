5e8fb887fd12d8eda9fdea204f448f94c5388b77
Jamie Doornbos <jamie@threerings.net>
2008-08-19 21:48:09 +0000

    New methods for validating a window client with respect to a specific game id

--- src/java/com/threerings/msoy/bureau/data/WindowClientObject.java
+++ src/java/com/threerings/msoy/bureau/data/WindowClientObject.java
@@ -3,7 +3,10 @@
 
 package com.threerings.msoy.bureau.data;
 
+import static com.threerings.msoy.Log.log;
+
 import com.threerings.presents.data.ClientObject;
+import com.whirled.bureau.data.BureauTypes;
 
 /** Client object purely for distinguishing windows in service methods. */
 public class WindowClientObject extends ClientObject
@@ -33,4 +36,33 @@ public class WindowClientObject extends ClientObject
         this.bureauId = value;
     }
     // AUTO-GENERATED: METHODS END
+
+    /**
+     * Checks if an arbitrary client is a window client and was established by the bureau for a
+     * specified game id.
+     */
+    public static boolean isForGame (ClientObject caller, int gameId)
+    {
+        if (!(caller instanceof WindowClientObject)) {
+            return false;
+        }
+        return ((WindowClientObject)caller).isForGame(gameId);
+    }
+    
+    /**
+     * Checks if this window client was established by the bureau for the given game id.
+     */
+    public boolean isForGame (int gameId)
+    {
+        // Fish out the game id
+        if (!bureauId.startsWith(BureauTypes.GAME_BUREAU_ID_PREFIX)) {
+            log.warning("Bad bureau id", "client", this);
+            return false;
+        }
+
+        int thisGameId = Integer.parseInt(bureauId.substring(
+            BureauTypes.GAME_BUREAU_ID_PREFIX.length()));
+        
+        return thisGameId == gameId;
+    }
 }
