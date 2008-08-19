124ed30c075490a7e9db2b465e8b075ebdf16b75
Michael Bayne <mdb@threerings.net>
2008-08-19 20:04:01 +0000

    A few things:
    1. Don't call super() manually. There's no mystery about whether or not the
    default superclass constructor is going to get called.
    
    2. Try to use hierarchical styles: style the container and then style the
    individual elements as necessary. It allows for more flexibility when doing
    things in your stylesheet. In this case, we can style the container as
    "favorite" and then add a selector in our CSS to say that any toggle button
    inside our .favorite needs special styling. We can also then avoid adding
    custom styles to any of our elements.
    
    3. Don't put styling in the code, use the stylesheets. Setting the width to
    60px turned out to be unnecessary, but should not be done in code if ever it is
    necessary. It should be done in the ".favorite" CSS block. Setting horizontal
    alignment should also be done in the stylesheet (there are times when one can't
    avoid using code, but this is not one of those times). Setting the vertical
    alignment also turned out not to be necessary.
    
    4. Don't put your style class names in constants. They are usually only ever
    used once in the code and it's indirect and verbose to have them down at the
    bottom of the classfile in a constant.
    
    5. Don't use HorizontalPanel or VerticalPanel if you can avoid it. Those wrap
    everything in tables and tables bring along a whole host of annoyances and
    problems that are best avoided unless there's no other way to accomplish what
    you need.
    
    6. There are times when a named inner class is useful to clarify the code, but
    this was not one of those times. If you do find yourself in a situation where
    the code in a ClickListener is large and unweildy, then keep the click listener
    declaration inline and just call a method. For example:
    
        toggle.addClickListener(new ClickListener() {
            public void onClick (Widget sender) {
                setFavorite(((ToggleButton) sender).isDown());
            }
        });
    
        protected void setFavorite (final boolean favorite)
        {
            _itemsvc.setFavorite(_item.getIdent(), favorite, new MsoyCallback<Void>() {
                public void onSuccess (Void result) {
                    _memberItemInfo.favorite = favorite;
                }
            });
        }                                  
    
    This keeps incidental information like the handling of a button click near the
    button declaration and cleanly extracts the (potentially more complex) action
    being taken as a result of that click into a method that knows and says nothing
    about buttons or clicking.

--- src/gwt/client/item/FavoriteIndicator.java
+++ src/gwt/client/item/FavoriteIndicator.java
@@ -5,14 +5,13 @@ package client.item;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ToggleButton;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import com.threerings.msoy.item.data.all.Item;
-import com.threerings.msoy.item.data.all.ItemIdent;
 import com.threerings.msoy.item.gwt.ItemService;
 import com.threerings.msoy.item.gwt.ItemServiceAsync;
 import com.threerings.msoy.item.gwt.MemberItemInfo;
@@ -21,41 +20,30 @@ import client.ui.MsoyUI;
 import client.util.MsoyCallback;
 import client.util.ServiceUtil;
 
-public class FavoriteIndicator extends VerticalPanel
+public class FavoriteIndicator extends FlowPanel
 {
     public FavoriteIndicator(Item item, MemberItemInfo memberItemInfo)
     {
-        super();
+        setStyleName("favorite");
         _item = item;
         _memberItemInfo = memberItemInfo;
 
-        setWidth("60px");
-        setHorizontalAlignment(ALIGN_CENTER);
-        setVerticalAlignment(ALIGN_TOP);
+        add(new Label(_imsgs.favorite()));
 
-        Label label = new Label(_imsgs.favorite());
-        label.setStyleName(STYLE_FAVORITE_TEXT);
-        add(label);
-
-        ToggleButton favoriteToggle = new ToggleButton(ADD_FAVORITE_IMAGE, FAVORITE_IMAGE);
-        favoriteToggle.setStyleName(STYLE_FAVORITE_TOGGLE);
-        favoriteToggle.addStyleName("actionLabel");
-        favoriteToggle.setDown(memberItemInfo.favorite);
-        favoriteToggle.addClickListener(new FavoriteClickListener());
-        add(favoriteToggle);
-    }
-
-    protected class FavoriteClickListener implements ClickListener
-    {
-        public void onClick (Widget sender) {
-            final boolean favorite = ((ToggleButton) sender).isDown();
-            ItemIdent item = _item.getIdent();
-            _itemsvc.setFavorite(item, favorite, new MsoyCallback<Void>() {
-                public void onSuccess (Void result) {
-                    _memberItemInfo.favorite = favorite;
-                }
-            });
-        }
+        ToggleButton toggle = new ToggleButton(ADD_FAVORITE_IMAGE, FAVORITE_IMAGE);
+        toggle.addStyleName("actionLabel");
+        toggle.setDown(memberItemInfo.favorite);
+        toggle.addClickListener(new ClickListener() {
+            public void onClick (Widget sender) {
+                final boolean favorite = ((ToggleButton) sender).isDown();
+                _itemsvc.setFavorite(_item.getIdent(), favorite, new MsoyCallback<Void>() {
+                    public void onSuccess (Void result) {
+                        _memberItemInfo.favorite = favorite;
+                    }
+                });
+            }
+        });
+        add(toggle);
     }
 
     protected Item _item;
@@ -65,8 +53,6 @@ public class FavoriteIndicator extends VerticalPanel
     protected static final ItemServiceAsync _itemsvc = (ItemServiceAsync) ServiceUtil.bind(
         GWT.create(ItemService.class), ItemService.ENTRY_POINT);
 
-    protected static final String STYLE_FAVORITE_TEXT = "favoriteText";
-    protected static final String STYLE_FAVORITE_TOGGLE = "favoriteToggle";
     protected static final Image FAVORITE_IMAGE = MsoyUI.createImage(
         "/images/ui/favorites/favorite.png", null);
     protected static final Image ADD_FAVORITE_IMAGE = MsoyUI.createImage(
