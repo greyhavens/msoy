ee6cb7dae543f868a0baa045fbb7a8be03fe9dd7
Nathan Curtis <nathan@threerings.net>
2008-08-21 18:41:09 +0000

    Well, that's why I was displaying the largest name twice.  They're still arriving in reverse order though...

--- src/java/com/threerings/msoy/notify/data/BadgeEarnedNotification.java
+++ src/java/com/threerings/msoy/notify/data/BadgeEarnedNotification.java
@@ -9,7 +9,10 @@ public class BadgeEarnedNotification extends Notification
 {
     public BadgeEarnedNotification (EarnedBadge badge)
     {
-        _badge = badge;
+        // we need a clone of this badge so that modifications don't affect this potentially
+        // deferred notification.
+        _badge = new EarnedBadge(badge.badgeCode, badge.level, badge.levelUnits, badge.coinValue,
+            badge.whenEarned);
     }
 
     @Override
