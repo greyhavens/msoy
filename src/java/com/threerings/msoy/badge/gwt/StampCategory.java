3523c9a58fdd44fe41389d1794af1b1749aedf32
Michael Bayne <mdb@threerings.net>
2008-08-21 23:10:11 +0000

    This is sent over the wire in PassportData and thus must implement IsSerializable.

--- src/java/com/threerings/msoy/badge/gwt/StampCategory.java
+++ src/java/com/threerings/msoy/badge/gwt/StampCategory.java
@@ -3,11 +3,14 @@
 
 package com.threerings.msoy.badge.gwt;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
 /**
  * Defines the various badge categories, which can be used to suggest to the user
  * which badges to pursue next, based on their past activity.
  */
 public enum StampCategory
+    implements IsSerializable
 {
     SOCIAL, GAME, CREATION, SHOPPING
 }
