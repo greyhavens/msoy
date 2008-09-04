3d08b0a5ab04f2bb5239dc6c6332c29117240de3
Bruno Garcia <bruno@threerings.net>
2008-09-04 23:56:08 +0000

    Fixity

--- src/gwt/client/shop/ShopMessagesLookup.java
+++ src/gwt/client/shop/ShopMessagesLookup.java
@@ -6,6 +6,6 @@ package client.shop;
 import com.threerings.gwt.util.MessagesLookup;
 
 @MessagesLookup.Lookup(using="client.shop.ShopMessages")
-public interface ShopMessagesLookup extends MessagesLookup
+public abstract class ShopMessagesLookup extends MessagesLookup
 {
 }
