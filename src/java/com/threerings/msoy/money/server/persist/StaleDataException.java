1ca3d99d46e3618469c47f2b7784372a960e2748
Kyle Sampson <kyle@threerings.net>
2008-09-05 20:24:23 +0000

    Removed RepositoryException in favor of the new DatabaseException.

--- src/java/com/threerings/msoy/money/server/persist/StaleDataException.java
+++ src/java/com/threerings/msoy/money/server/persist/StaleDataException.java
@@ -3,13 +3,15 @@
 
 package com.threerings.msoy.money.server.persist;
 
+import com.samskivert.jdbc.depot.DatabaseException;
+
 /**
  * Indicates an update or insert was attempted on an entity, but the entity was updated in another
  * thread since it was last loaded.
  * 
  * @author Kyle Sampson <kyle@threerings.net>
  */
-public class StaleDataException extends RepositoryException
+public class StaleDataException extends DatabaseException
 {
     public StaleDataException (final String message)
     {
