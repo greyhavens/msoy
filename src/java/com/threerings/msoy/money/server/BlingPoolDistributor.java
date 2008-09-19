8e182bd0a14f404f6597ef171c1df8639f557c7b
Ray Greenwell <ray@threerings.net>
2008-09-19 00:56:20 +0000

    - Handle the case of the blingPoolSize being set to a negative number-   don't try to distribute negative bling. - If the blingPoolSize is 0, that means "distribute nothing", not   "don't distribute". The difference is that we record that we did   a distribution, so that we don't go back later when it's set to nonzero   and redo those old days. - Did things more midnightly. Although Kyle points out that the lastRun time   is stored in a Date column, so it will already be like that. I'll doublecheck. - A bit more logging. Log the day as normal when we distribute 0. - Formatting: there should be no spaces after the last visible character   on any line.

--- src/java/com/threerings/msoy/money/server/BlingPoolDistributor.java
+++ src/java/com/threerings/msoy/money/server/BlingPoolDistributor.java
@@ -60,15 +60,15 @@ public class BlingPoolDistributor
     {
         _repo = repo;
         _gameRepo = gameRepo;
-        
+
         sm.registerShutdowner(this);
-        
+
         try {
             // Create the quartz scheduler, with 1 thread.
             DirectSchedulerFactory.getInstance().createVolatileScheduler(1);
             _scheduler = DirectSchedulerFactory.getInstance().getScheduler();
             _scheduler.setJobFactory(new DistributorFactory());
-            
+
             final JobDetail job = new JobDetail("Bling Distributor", GROUP, DistributorJob.class);
             final CronTrigger trigger = new CronTrigger("Bling Distributor", GROUP);
             trigger.setCronExpression(EXECUTE_SCHEDULE);
@@ -79,7 +79,7 @@ public class BlingPoolDistributor
             throw new IllegalStateException(pe);
         }
     }
-    
+
     /**
      * Starts the distributor, which will be invoked at a later time.
      */
@@ -87,19 +87,19 @@ public class BlingPoolDistributor
     {
         // Get the money config record, automatically creating it if it doesn't currently exist.
         MoneyConfigRecord confRecord = _repo.getMoneyConfig(false);
-        
+
         // Ensure no one has the lock.  This is necessary in case the lock wasn't successfully
         // released when it was last shutdown.  We need to ensure that we're not starting up one
         // node while the bling distribution is occurring (highly unlikely).
         _repo.completeBlingDistribution(confRecord.lastDistributedBling);
-        
+
         try {
             _scheduler.start();
         } catch (SchedulerException se) {
             throw new IllegalStateException(se);
         }
     }
-    
+
     public void shutdown ()
     {
         try {
@@ -108,42 +108,53 @@ public class BlingPoolDistributor
             throw new IllegalStateException(se);
         }
     }
-    
+
     /**
      * Actually process the bling to be distributed.
      */
     protected void run ()
     {
-        // If no bling should be awarded, exit outta here
-        int blingPool = RuntimeConfig.server.blingPoolSize * 100;
-        if (blingPool == 0) {
-            logger.info("No bling to distribute, canceling.");
-            return;
-        }
-        
         // Get the last time we ran this and calculate the number of days since that time
         // has passed.
         MoneyConfigRecord confRecord = _repo.getMoneyConfig(true);
         if (confRecord == null) {
-            logger.info("Another process is currently distributing bling, canceling.");
+            log.info("Another process is currently distributing bling, canceling.");
             return;
         }
+        // from here down we need to do everything in a try so that we release the lock we just
+        // created
 
-        Calendar calLastRun = toCal(confRecord.lastDistributedBling);
+        Calendar now = Calendar.getInstance();
         try {
-            int days = CalendarUtil.getDaysBetween(calLastRun, Calendar.getInstance());
-            
-            // We'll repeat this for the number of days since we last executed it.
-            Calendar cal2 = toCal(confRecord.lastDistributedBling);
-            cal2.add(Calendar.DATE, 1);
+            // figure out how much we'll be distributing
+            int blingPool = RuntimeConfig.server.blingPoolSize * 100;
+
+            Calendar lastRun = Calendar.getInstance();
+            lastRun.setTime(confRecord.lastDistributedBling); // make it lastRun
+
+            Calendar midnight1 = (Calendar) lastRun.clone();
+            CalendarUtil.zeroTime(midnight1); // make it midnight
+            Calendar midnight2 = (Calendar) now.clone();
+            CalendarUtil.zeroTime(midnight2); // make it midnight
+            int days = CalendarUtil.getDaysBetween(midnight1, midnight2);
+
+            // now set midnight2 to be the day after midnight1, in case we're doing more than one day
+            midnight2 = (Calendar) midnight1.clone();
+            midnight2.add(Calendar.DATE, 1);
+
+            // We'll repeat this for the number of days since we last executed it (could be 0).
             for (int i = 0; i < days; i++) {
-                logger.info("Distributing bling for " + 
-                    DateFormat.getDateInstance().format(calLastRun.getTime()));
-                
-                // Get all the game play sessions for this day.
+                log.info("Distributing bling.",
+                    "day", DateFormat.getDateInstance().format(midnight1.getTime()),
+                    "bling", RuntimeConfig.server.blingPoolSize); // don't log centibling..
+                if (blingPool <= 0) {
+                    continue; // but we did the logging...
+                }
+
+                // Get all the game play sessions for that day.
                 Collection<GamePlayRecord> gamePlays = _gameRepo.getGamePlaysBetween(
-                    calLastRun.getTimeInMillis(), cal2.getTimeInMillis());
-                
+                    midnight1.getTimeInMillis(), midnight2.getTimeInMillis());
+
                 // Calculate a total and a map of game ID to the total minutes spent in the game
                 long totalMinutes = 0;
                 Map<Integer, Long> minutesPerGame = new HashMap<Integer, Long>();
@@ -156,13 +167,13 @@ public class BlingPoolDistributor
                             minutesPerGame.get(gamePlay.gameId) + gamePlay.playerMins);
                     }
                 }
-                
+
                 // Get all the games, since we need the creatorId from them.
                 Map<Integer, GameRecord> gameMap = new HashMap<Integer, GameRecord>();
                 for (GameRecord game : _gameRepo.loadItems(minutesPerGame.keySet())) {
                     gameMap.put(game.itemId, game);
                 }
-                
+
                 // Assuming we have a non-zero number of minutes games were played this day, grant a
                 // portion of the bling pool to each game's creator.
                 if (totalMinutes > 0) {
@@ -171,19 +182,18 @@ public class BlingPoolDistributor
                         awardBling(gameMap.get(entry.getKey()), awardedBling);
                     }
                 }
-                
-                // Increment the day to use when retrieving game play records
-                calLastRun.add(Calendar.DATE, 1);
-                cal2.add(Calendar.DATE, 1);
+
+                // increment one dates
+                midnight1.add(Calendar.DATE, 1);
+                midnight2.add(Calendar.DATE, 1);
             }
-            
-            logger.info("Finished distributing bling.");
+
         } finally {
             // Complete bling distribution up to the date that we completed successfully in here.
-            _repo.completeBlingDistribution(new java.sql.Date(calLastRun.getTimeInMillis()));
+            _repo.completeBlingDistribution(new java.sql.Date(now.getTimeInMillis()));
         }
     }
-    
+
     /**
      * Awards some amount of bling to the creator of the given game.
      * 
@@ -197,20 +207,12 @@ public class BlingPoolDistributor
         String description = MessageBundle.tcompose("m.game_plays_bling_awarded", amount,
             game.itemId, game.description);
         MoneyTransactionRecord tx = account.gamePlaysPayout(description, game.itemId, amount);
-        
+
         // Save the account and new transaction records.
         _repo.saveAccount(account);
         _repo.addTransaction(tx);
     }
-    
-    protected Calendar toCal (Date date)
-    {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        CalendarUtil.zeroTime(cal);
-        return cal;
-    }
-    
+
     /** Necessary because DistributorJob is not a static class. */
     protected class DistributorFactory implements JobFactory
     {
@@ -219,7 +221,7 @@ public class BlingPoolDistributor
             return new DistributorJob();
         }
     }
-    
+
     /**
      * Job to execute when distribution should be performed.
      */
@@ -231,20 +233,20 @@ public class BlingPoolDistributor
             try {
                 run();
             } catch (Exception e) {
-                logger.error("Exception occurried while running the bling distributor.", e);
+                log.error("Exception occurried while running the bling distributor.", e);
                 throw new JobExecutionException(e);
             }
         }
     }
-    
+
     /** Schedule to execute this distributor.  Daily at 3:00a. */
     protected static final String EXECUTE_SCHEDULE = "0 0 3 * * ?";
-    
+
     /** Default group for scheduled jobs and triggers. */
     protected static final String GROUP = "group1"; 
-    
-    protected static final Logger logger = Logger.getLogger(BlingPoolDistributor.class);
-    
+
+    protected static final Logger log = Logger.getLogger(BlingPoolDistributor.class);
+
     protected final MoneyRepository _repo;
     protected final GameRepository _gameRepo;
     protected final Scheduler _scheduler;
