506943c45acc1fddce1ec8b6114ae7d28d94fac9
Ray Greenwell <ray@threerings.net>
2008-09-21 22:21:55 +0000

    Clean this up. I wondered if I could move the cookie setting back to the server, but the "#tokens" aren't sent, which I should have realized.

--- src/java/com/threerings/msoy/web/server/AffiliateCookie.java
+++ src/java/com/threerings/msoy/web/server/AffiliateCookie.java
@@ -10,40 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 import com.samskivert.servlet.util.CookieUtil;
 
 /**
- * Affiliate Cookie Abstraction Jubilee. Bling bling!
+ * The server-side of {@link client.shell.AffiliateCookie}.
  */
 public class AffiliateCookie
 {
+    /** The cookie name. */
     public static final String NAME = "baff";
 
-//    public static void check (HttpServletRequest req, HttpServletResponse rsp)
-//    {
-//        String affiliate = req.getParameter("aff"); // "aff" is the parameter name ?aff=foo
-//        if (affiliate == null) {
-//            return;
-//        }
-//
-//        if (false /* FIRST WINS */ && CookieUtil.getCookie(req, NAME) != null) {
-//            return;
-//        }
-//        // LAST WINS, or FIRST WINS but this is the first!
-//        Cookie cookie = new Cookie(NAME, affiliate);
-//        cookie.setMaxAge(365 * 24 * 60 * 60); // one year
-//        cookie.setPath("/");
-//        rsp.addCookie(cookie); // nom nom nom
-//    }
-
+    /**
+     * Return the affiliate cookie value.
+     */
     public static String get (HttpServletRequest req)
     {
         return CookieUtil.getCookieValue(req, NAME);
     }
-
-//    /**
-//     * Clear the cookie.
-//     * Note: this code path is not used. We clear the cookie on the client.
-//     */
-//    public static void clear (HttpServletResponse rsp)
-//    {
-//        CookieUtil.clearCookie(rsp, NAME);
-//    }
 }
