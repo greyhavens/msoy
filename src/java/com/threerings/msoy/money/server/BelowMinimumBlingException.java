d0ea8ccdd1de2f76a482695365d0f3bd3fd698f1
Kyle Sampson <kyle@threerings.net>
2008-09-25 19:46:01 +0000

    Maybe I should just stop adding files...

---
+++ src/java/com/threerings/msoy/money/server/BelowMinimumBlingException.java
@@ -0,0 +1,30 @@
+//
+// $Id$
+
+package com.threerings.msoy.money.server;
+
+public class BelowMinimumBlingException extends MoneyException
+{
+    public final int memberId;
+    public final int amountRequested;
+    public final int minimumAmount;
+    
+    public BelowMinimumBlingException (int memberId, int amountRequested, int minimumAmount)
+    {
+        super("The amount of bling requested, " + amountRequested + 
+            ", is below the minimum amount allowed: " + minimumAmount);
+        this.memberId = memberId;
+        this.amountRequested = amountRequested;
+        this.minimumAmount = minimumAmount;
+    }
+    
+    public BelowMinimumBlingException (int memberId, int amountRequested, int minimumAmount, 
+        Throwable cause)
+    {
+        super("The amount of bling requested, " + amountRequested + 
+            ", is below the minimum amount allowed: " + minimumAmount, cause);
+        this.memberId = memberId;
+        this.amountRequested = amountRequested;
+        this.minimumAmount = minimumAmount;
+    }
+}
