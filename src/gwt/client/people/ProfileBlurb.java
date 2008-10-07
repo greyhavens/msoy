4921f5a800a928dd7248d5d5c02e97244b076611
Sarah Northway <sarah@threerings.net>
2008-10-07 19:50:24 +0000

    Turn galleries back off for now, these won't go to production today.

--- src/gwt/client/people/ProfileBlurb.java
+++ src/gwt/client/people/ProfileBlurb.java
@@ -21,6 +21,7 @@ import com.google.gwt.user.client.ui.Widget;
 import com.threerings.gwt.ui.Anchor;
 import com.threerings.gwt.ui.SmartTable;
 
+import com.threerings.msoy.data.all.DeploymentConfig;
 import com.threerings.msoy.data.all.MediaDesc;
 import com.threerings.msoy.data.all.MemberName;
 import com.threerings.msoy.item.data.all.Item;
@@ -75,9 +76,13 @@ public class ProfileBlurb extends Blurb
         photo.setStyleName("Photo");
         String mepics = Args.compose("pgallery", _name.getMemberId());
         ClickListener onClick = null;
-        onClick = Link.createListener(Pages.PEOPLE, mepics);
+        if (DeploymentConfig.devDeployment) { // TODO - make public
+            onClick = Link.createListener(Pages.PEOPLE, mepics);
+        }
         photo.add(MediaUtil.createMediaView(_profile.photo, MediaDesc.THUMBNAIL_SIZE, onClick));
-        photo.add(Link.create(_msgs.photosOfMe(), Pages.PEOPLE, mepics));
+        if (DeploymentConfig.devDeployment) { // TODO - make public
+            photo.add(Link.create(_msgs.photosOfMe(), Pages.PEOPLE, mepics));
+        }
 
         // create the info section with their name, a/s/l, etc.
         SmartTable info = new SmartTable("Info", 0, 5);
