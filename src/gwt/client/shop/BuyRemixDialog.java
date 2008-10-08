e5f098efed779a43a1b9807c689093dbede43bd3
Ray Greenwell <ray@threerings.net>
2008-10-08 03:44:28 +0000

    Touched-up the ui on the remix-before-you-buy pop-up dialog.

--- src/gwt/client/shop/BuyRemixDialog.java
+++ src/gwt/client/shop/BuyRemixDialog.java
@@ -5,11 +5,15 @@ package client.shop;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import com.threerings.msoy.item.data.all.Item;
 import com.threerings.msoy.item.gwt.CatalogListing;
 
+import client.shell.ShellMessages;
 import client.ui.BorderedDialog;
 import client.ui.MsoyUI;
 
@@ -26,7 +30,8 @@ public class BuyRemixDialog extends BorderedDialog
         setHeaderTitle(_msgs.buyRemixTitle());
         _callback = callback;
 
-        VerticalPanel panel = new VerticalPanel();
+        FlowPanel panel = new FlowPanel();
+        panel.setStyleName("listingDetailPanel");
         panel.addStyleName("buyRemixPanel");
         panel.add(MsoyUI.createLabel(_msgs.buyRemixPrompt(), null));
 
@@ -39,6 +44,12 @@ public class BuyRemixDialog extends BorderedDialog
             }
         }));
         setContents(panel);
+        addButton(new Button(_cmsgs.cancel(), new ClickListener() {
+            public void onClick (Widget sender) {
+                hide();
+                _callback.onSuccess(null);
+            }
+        }));
         show();
     }
 
@@ -50,4 +61,5 @@ public class BuyRemixDialog extends BorderedDialog
     protected AsyncCallback<Item> _callback;
 
     protected static final ShopMessages _msgs = GWT.create(ShopMessages.class);
+    protected static final ShellMessages _cmsgs = GWT.create(ShellMessages.class);
 }
