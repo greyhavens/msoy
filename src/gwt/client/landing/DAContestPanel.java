f8f1f2925b59b0573ee17799271a1ed452ac4625
Sarah Northway <sarah@threerings.net>
2008-10-21 19:26:43 +0000

    Finish content for steps 2 and 3 of dA contest.  Rooms to display down the right are still TBD.

--- src/gwt/client/landing/DAContestPanel.java
+++ src/gwt/client/landing/DAContestPanel.java
@@ -3,6 +3,7 @@
 
 package client.landing;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.ClickListener;
@@ -17,8 +18,10 @@ import client.ui.MsoyUI;
 import client.util.Link;
 
 /**
- * Landing page for the Deviant Art "Design Your Whirled" contest. Most content is in images, and
- * many contain English text at fixed sizes.
+ * Landing page for the Deviant Art "Design Your Whirled" contest. Images contain English text at
+ * fixed sizes. Production-specific content links are present here and in
+ * LandingMessages.properties. This contest will be live for three weeks starting 10/24/2008, then
+ * permanently taken down.
  */
 public class DAContestPanel extends SimplePanel
 {
@@ -42,43 +45,56 @@ public class DAContestPanel extends SimplePanel
             String roomName = COOL_ROOM_NAMES[ii];
             String roomImageURL = "/images/landing/dacontest_room_" + sceneId + ".png";
             ClickListener onClick = Link.createListener(Pages.WORLD, "s" + sceneId);
-            roomsList.add(MsoyUI.createImage(roomImageURL, "RoomImage"));
+            Image roomImage = MsoyUI.createActionImage(roomImageURL, roomName, onClick);
+            roomImage.addStyleName("RoomImage");
+            roomsList.add(roomImage);
             roomsList.add(MsoyUI.createActionLabel(roomName, "RoomName", onClick));
         }
         rooms.add(new Image("/images/landing/dacontest_rooms_bottom.png"));
 
-        // TODO make anything else in this panel a link or text?
+        // how to enter and step panels are large images that include text
         AbsolutePanel howToEnter = MsoyUI.createAbsolutePanel("HowToEnter");
         content.add(howToEnter);
         howToEnter.add(MsoyUI.createButton(MsoyUI.LONG_THICK, "Join Whirled!",
-            Link.createListener(Pages.ACCOUNT, "create")), 455, 370);
+            Link.createListener(Pages.ACCOUNT, "create")), 455, 345);
 
         AbsolutePanel step1 = MsoyUI.createAbsolutePanel("Step1");
         content.add(step1);
-        // TODO Make text and links ActionLabels when content is finalized
-        step1.add(createInvisiLink(Link.createListener(Pages.SHOP, "3"), 60, 30), 550, 130);
-        step1.add(createInvisiLink(new ClickListener() {
-            public void onClick (Widget sender) {
-                Window.open("http://wiki.whirled.com/", "_blank", null);
-            }
-        }, 100, 30), 460, 150);
+        step1.add(createInvisiLink(Link.createListener(Pages.SHOP, "3"), 50, 20), 560, 145);
+        step1.add(createInvisiLink("http://wiki.whirled.com/", 90, 20), 490, 160);
 
         AbsolutePanel step2 = MsoyUI.createAbsolutePanel("Step2");
         content.add(step2);
 
         AbsolutePanel step3 = MsoyUI.createAbsolutePanel("Step3");
         content.add(step3);
+        step3.add(createInvisiLink(
+            "http://www.deviantart.com/#catpath=projects/contests/2008/whirled", 290, 20), 30,
+            480);
 
+        // Rules are in text
         AbsolutePanel rules = MsoyUI.createAbsolutePanel("Rules");
         content.add(rules);
-        // TODO make a bigger button as in mockup
-        rules.add(MsoyUI.createButton(MsoyUI.LONG_THICK, "Join Whirled!", Link.createListener(
-            Pages.ACCOUNT, "create")), 570, 220);
-        rules.add(createInvisiLink(new ClickListener() {
+        rules.add(MsoyUI.createHTML(_msgs.dacontestRules(), null), 70, 70);
+        rules.add(MsoyUI.createHTML(_msgs.dacontestJudging(), "Judging"), 450, 80);
+        rules.add(MsoyUI.createActionImage("/images/landing/dacontest_rules_join.png",
+            Link.createListener(Pages.ACCOUNT, "create")), 540, 200);
+
+        // tools icon sits over everything
+        content.add(new Image("/images/landing/dacontest_tools.png"), 130, 110);
+    }
+
+    /**
+     * Helper function for creating an invisible area of a given size with a given offsite link
+     * that will be opened in a new window.
+     */
+    protected Widget createInvisiLink (final String offsiteLinkPath, int width, int height)
+    {
+        return createInvisiLink(new ClickListener() {
             public void onClick (Widget sender) {
-                Window.open("http://wiki.whirled.com/", "_blank", null);
+                Window.open(offsiteLinkPath, "_blank", null);
             }
-        }, 70, 30), 110, 280);
+        }, width, height);
     }
 
     /**
@@ -94,7 +110,10 @@ public class DAContestPanel extends SimplePanel
     }
 
     /** Scenes to display under "check out these cool rooms!"; images are indexed by id */
-    protected static final int[] COOL_ROOM_IDS = { 1, 2, 3 };
+    protected static final int[] COOL_ROOM_IDS = { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1 };
     protected static final String[] COOL_ROOM_NAMES = { "OOO Tentacles", "Club Bella",
-        "Corpse Craft" };
+        "Corpse Craft", "OOO Tentacles", "Club Bella", "Corpse Craft", "OOO Tentacles",
+        "Club Bella", "Corpse Craft", "OOO Tentacles" };
+
+    protected static final LandingMessages _msgs = GWT.create(LandingMessages.class);
 }
