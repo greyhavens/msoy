f65219f5aa4d5ece5d8b1a54998f3b1ee0aff08c
Jamie Doornbos <jamie@threerings.net>
2008-10-23 05:58:47 +0000

    Provide the media descriptor for InProgressBadge

--- src/java/com/threerings/msoy/badge/data/all/InProgressBadge.java
+++ src/java/com/threerings/msoy/badge/data/all/InProgressBadge.java
@@ -4,6 +4,8 @@
 package com.threerings.msoy.badge.data.all;
 
 import com.threerings.msoy.data.all.DeploymentConfig;
+import com.threerings.msoy.data.all.MediaDesc;
+import com.threerings.msoy.data.all.StaticMediaDesc;
 
 public class InProgressBadge extends Badge
 {
@@ -27,12 +29,23 @@ public class InProgressBadge extends Badge
     @Override // from Badge
     public String imageUrl ()
     {
-        return DeploymentConfig.staticMediaURL + BADGE_IMAGE_DIR + Integer.toHexString(badgeCode) +
-            "_" + (level > 0 ? (level - 1) + "f" : level + "e") + BADGE_IMAGE_TYPE;
+        return DeploymentConfig.staticMediaURL + BADGE_IMAGE_DIR + getImageFilename() + 
+            BADGE_IMAGE_TYPE;
     }
-
+    
+    // TODO: can this become abstract in Badge and replace Badge.imageUrl?
+    public MediaDesc imageMedia ()
+    {
+        return new StaticMediaDesc(MediaDesc.IMAGE_PNG, "badge", getImageFilename());
+    }
+    
     public String toString ()
     {
         return "badgeCode=" + badgeCode + " nextLevel=" + level + " progress=" + progress;
+    }
+    
+    protected String getImageFilename()
+    {
+        return Integer.toHexString(badgeCode) + "_" + (level > 0 ? (level - 1) + "f" : level + "e");
     }
 }
