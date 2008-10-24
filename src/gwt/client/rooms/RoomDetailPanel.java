5957558004d9c516a5fe8abc404180276d94d3df
Michael Bayne <mdb@threerings.net>
2008-10-24 18:19:10 +0000

    Some modest arrangement improvements.

--- src/gwt/client/rooms/RoomDetailPanel.java
+++ src/gwt/client/rooms/RoomDetailPanel.java
@@ -5,12 +5,15 @@ package client.rooms;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasAlignment;
 
 import com.threerings.gwt.ui.InlineLabel;
 import com.threerings.gwt.ui.SmartTable;
+import com.threerings.gwt.ui.WidgetUtil;
 
 import com.threerings.msoy.comment.gwt.Comment;
 import com.threerings.msoy.data.all.GroupName;
+import com.threerings.msoy.data.all.MediaDesc;
 import com.threerings.msoy.data.all.MemberName;
 
 import com.threerings.msoy.room.gwt.RoomDetail;
@@ -21,6 +24,7 @@ import com.threerings.msoy.web.gwt.RatingResult;
 import client.comment.CommentsPanel;
 import client.room.SceneUtil;
 import client.shell.CShell;
+import client.ui.MsoyUI;
 import client.ui.Rating;
 import client.ui.StyledTabPanel;
 import client.util.Link;
@@ -35,7 +39,7 @@ public class RoomDetailPanel extends SmartTable
 {
     public RoomDetailPanel (int sceneId)
     {
-        super("roomDetailPanel", 0, 5);
+        super("roomDetailPanel", 0, 10);
 
         _worldsvc.loadRoomDetail(sceneId, new MsoyCallback<RoomDetail>() {
             public void onSuccess (RoomDetail detail) {
@@ -52,7 +56,12 @@ public class RoomDetailPanel extends SmartTable
         }
         CShell.frame.setTitle(detail.info.name);
 
+        setWidget(0, 0, SceneUtil.createSceneView(detail.info.sceneId, detail.snapshot));
+        final int snapWidth = MediaDesc.getWidth(MediaDesc.SNAPSHOT_FULL_SIZE);
+        getFlexCellFormatter().setWidth(0, 0, snapWidth + "px");
+
         FlowPanel obits = new FlowPanel();
+        obits.add(MsoyUI.createLabel(detail.info.name, "Title"));
         obits.add(new InlineLabel(_msgs.owner(), false, false, true));
         if (detail.owner instanceof MemberName) {
             MemberName name = (MemberName)detail.owner;
@@ -61,18 +70,20 @@ public class RoomDetailPanel extends SmartTable
             GroupName name = (GroupName)detail.owner;
             obits.add(Link.groupView(name.toString(), name.getGroupId()));
         }
-        obits.add(SceneUtil.createSceneView(detail.info.sceneId, detail.snapshot));
+        obits.add(WidgetUtil.makeShim(10, 15));
         obits.add(new Rating(detail.info.rating, detail.ratingCount, detail.memberRating, true) {
             @Override
             protected void handleRate (byte newRating , MsoyCallback<RatingResult> callback) {
                 _worldsvc.rateRoom(detail.info.sceneId, newRating, callback);
             }
         });
-        addWidget(obits, 1, null);
+        setWidget(0, 1, obits);
+
+        getFlexCellFormatter().setVerticalAlignment(0, 1, HasAlignment.ALIGN_TOP);
 
         StyledTabPanel tabs = new StyledTabPanel();
         tabs.add(new CommentsPanel(Comment.TYPE_ROOM, detail.info.sceneId), _msgs.tabComments());
-        addWidget(tabs, 1, null);
+        addWidget(tabs, 2, null);
         tabs.selectTab(0);
     }
 
