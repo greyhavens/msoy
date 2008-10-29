7254cb4e72e9d6e6ec18d37c2f6c4b6b514f8640
Michael Bayne <mdb@threerings.net>
2008-10-29 23:15:28 +0000

    Formatting.

--- src/gwt/client/util/NoopAsyncCallback.java
+++ src/gwt/client/util/NoopAsyncCallback.java
@@ -1,3 +1,6 @@
+//
+// $Id$
+
 package client.util;
 
 import client.shell.CShell;
@@ -5,14 +8,16 @@ import client.shell.CShell;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
- * Noop implementation of an AsyncCallback of type Void.
- * Does nothing on success, and logs a message on failure.
+ * Noop implementation of an AsyncCallback of type Void.  Does nothing on success, and logs a
+ * message on failure.
  */
-public class NoopAsyncCallback implements AsyncCallback<Void> {
+public class NoopAsyncCallback implements AsyncCallback<Void>
+{
     public void onSuccess (Void result)
     {
         // no op!
     }
+
     public void onFailure (Throwable caught)
     {
         CShell.log("Failed to communicate with the server: " + caught);
