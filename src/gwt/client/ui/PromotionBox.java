3067fafc36a43a4207316d6520b10790273d653e
Michael Bayne <mdb@threerings.net>
2008-11-26 21:49:32 +0000

    Replaced left and right arrows with a little circular arrow image that just moves forward.

--- src/gwt/client/ui/PromotionBox.java
+++ src/gwt/client/ui/PromotionBox.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.ui.ClickListener;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -52,30 +50,14 @@ public class PromotionBox extends RoundBox
         hbox.add(blurb);
         hbox.setCellWidth(blurb, "100%");
         if (_promos.size() > 1) {
-            hbox.add(WidgetUtil.makeShim(10, 10));
-            FlowPanel navi = new FlowPanel();
-            navi.add(createActionLink("&larr;", new ClickListener() {
+            hbox.add(WidgetUtil.makeShim(5, 5));
+            hbox.add(MsoyUI.createActionImage("/images/ui/promo_arrow.png", new ClickListener() {
                 public void onClick (Widget sender) {
                     showPromotion((index + _promos.size()-1) % _promos.size());
                 }
             }));
-            navi.add(WidgetUtil.makeShim(5, 5));
-            navi.add(createActionLink("&rarr;", new ClickListener() {
-                public void onClick (Widget sender) {
-                    showPromotion((index + 1) % _promos.size());
-                }
-            }));
-            hbox.add(navi);
         }
         add(hbox);
-    }
-
-    protected HTML createActionLink (String glyph, ClickListener listener)
-    {
-        HTML link = new HTML(glyph);
-        link.addClickListener(listener);
-        link.addStyleName("hoverActionLabel");
-        return link;
     }
 
     protected List<Promotion> _promos;
