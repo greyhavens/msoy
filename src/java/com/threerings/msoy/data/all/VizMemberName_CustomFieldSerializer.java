1b63ec801bc27d232c271c71fe180ef8a8dcd2ef
Jamie Doornbos <jamie@threerings.net>
2008-12-04 21:03:51 +0000

    Fix broken profile page. VizMemberName needs this gubbins too

---
+++ src/java/com/threerings/msoy/data/all/VizMemberName_CustomFieldSerializer.java
@@ -0,0 +1,26 @@
+package com.threerings.msoy.data.all;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.SerializationStreamReader;
+import com.google.gwt.user.client.rpc.SerializationStreamWriter;
+
+public class VizMemberName_CustomFieldSerializer
+{
+    public static void serialize (SerializationStreamWriter streamWriter, VizMemberName name)
+        throws SerializationException
+    {
+        MemberName_CustomFieldSerializer.serialize(streamWriter, name);
+        streamWriter.writeObject(name.getPhoto());
+    }
+    
+    public static VizMemberName instantiate (SerializationStreamReader streamReader)
+        throws SerializationException
+    {
+        return new VizMemberName(streamReader.readString(), streamReader.readInt(),
+            (MediaDesc)streamReader.readObject());
+    }
+    
+    public static void deserialize (SerializationStreamReader streamReader, VizMemberName instance)
+    {
+    }
+}
