422e0fd10c951292332758d9ded06a6f9e4854d0
Michael Bayne <mdb@threerings.net>
2008-12-15 19:32:47 +0000

    Wrapped long line. 100 columns please.

--- src/gwt/client/shop/BuyPanel.java
+++ src/gwt/client/shop/BuyPanel.java
@@ -107,7 +107,8 @@ public class BuyPanel extends FlowPanel
             FlowPanel charityPanel = new FlowPanel();
             charityPanel.setStyleName("Charity");
             charityPanel.add(new InlineLabel(_msgs.donatedToCharity(percentage)));
-            charityPanel.add(Link.create(result.charity.toString(), Pages.PEOPLE, ""+result.charity.getMemberId()));
+            charityPanel.add(Link.create(result.charity.toString(),
+                                         Pages.PEOPLE, ""+result.charity.getMemberId()));
             charityPanel.add(WidgetUtil.makeShim(10, 10));
             charityPanel.add(Link.create(_msgs.changeCharity(), Pages.ME, "account"));
             add(charityPanel);
