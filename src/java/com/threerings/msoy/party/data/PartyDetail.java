a57d822399d4a575c97d48eca4cfec727b3a3e03
Ray Greenwell <ray@threerings.net>
2008-12-19 21:34:57 +0000

    Ahfuck, I forgot a no-arg constructor, so these are not able to be instantiated when squirted from one node to another.

--- src/java/com/threerings/msoy/party/data/PartyDetail.java
+++ src/java/com/threerings/msoy/party/data/PartyDetail.java
@@ -11,6 +11,12 @@ public class PartyDetail extends PartyBoardInfo
     /** The people in this party. */
     public PartyPeep[] peeps;
 
+    /** Suitable for unserialization. */
+    public PartyDetail () {}
+
+    /**
+     * Construct a party detail.
+     */
     public PartyDetail (PartyInfo info, PartyPeep[] peeps)
     {
         super(info);
