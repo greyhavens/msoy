2a56251d931bbc1b2ca7ab0748b1a7fb3ef86a59
Robert Zubeck <robert@threerings.net>
2009-01-16 23:20:22 +0000

    Unused import patrol makes Eclipse happy.

--- src/java/com/threerings/msoy/room/data/AudioData.java
+++ src/java/com/threerings/msoy/room/data/AudioData.java
@@ -6,8 +6,6 @@ package com.threerings.msoy.room.data;
 import com.threerings.io.SimpleStreamableObject;
 
 import com.threerings.msoy.data.all.MediaDesc;
-import com.threerings.msoy.item.data.all.DefaultItemMediaDesc;
-import com.threerings.msoy.item.data.all.Item;
 
 /**
  * Contains immutable information on background audio in a scene.
