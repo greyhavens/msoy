c3a603858eb60424fa37ca19451f852897c88889
Matt Jensen <matt@threerings.net>
2009-02-17 04:07:40 +0000

    Pointing the "Buy Coins" link at a billing landing page that does not require a password. This will allow the user to peruse the different payment types and see a brief description of bars and a happy tofu with a top hat before we hit them with the scary billing login page.

--- src/gwt/client/util/BillingURLs.java
+++ src/gwt/client/util/BillingURLs.java
@@ -8,44 +8,46 @@ import com.threerings.msoy.web.gwt.WebCreds;
 
 /**
  * Contains methods to get URLs for various points in the billing system.
- * 
+ *
  * @author Kyle Sampson <kyle@threerings.net>
  */
 public final class BillingURLs
 {
     /**
      * Returns the entry point into the billing system.
-     * 
+     *
      * @param creds If not null, this should contain the account name to be filled in automatically
      * on the login page.
      */
     public static String getEntryPoint (WebCreds creds)
     {
         if (creds != null) {
-            return BASE + "?initUsername=" + creds.accountName;
+            return LANDING + "?initUsername=" + creds.accountName;
         }
-        return BASE;
+        return LANDING;
     }
-    
+
     /**
      * Returns a URL to the page showing transaction history for a particular user.  Must be
      * logged in as admin in billing to see this page.
-     * 
+     *
      * @param accountName Account name of the user (login email address)
      * @param permaName Permaname of the user, if chosen.
      */
     public static String getUserStatusPage (String accountName, String permaName)
     {
-        return BASE + "admin/user_status.wm?username=" + 
+        return BASE + "admin/user_status.wm?username=" +
             (permaName == null ? accountName : permaName);
     }
-    
+
     static
     {
         // Just to make sure this has an ending '/'.
         BASE = DeploymentConfig.billingURL + (DeploymentConfig.billingURL.endsWith("/") ?
             "" : "/");
     }
-    
-    private static final String BASE;
+
+    protected static final String BASE;
+
+    protected static final String LANDING = BASE + "whirled.wm";
 }
