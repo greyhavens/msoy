437d52ec0451046bc40fec7b97798782392704fe
Ray Greenwell <ray@threerings.net>
2009-02-24 01:42:28 +0000

    WRLD-586: the bland finale. Show a result message in the BuyPanel, linking to the user's new room. Wet paper bags; asses; both hands.

--- src/gwt/client/people/RoomBuyPanel.java
+++ src/gwt/client/people/RoomBuyPanel.java
@@ -5,6 +5,10 @@ package client.people;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+
+import com.threerings.msoy.web.gwt.Pages;
 
 import com.threerings.msoy.money.data.all.Currency;
 import com.threerings.msoy.money.data.all.PurchaseResult;
@@ -13,6 +17,7 @@ import com.threerings.msoy.room.gwt.RoomInfo;
 import com.threerings.msoy.room.gwt.WebRoomService;
 import com.threerings.msoy.room.gwt.WebRoomServiceAsync;
 
+import client.util.Link;
 import client.util.ServiceUtil;
 
 import client.money.BuyPanel;
@@ -30,6 +35,14 @@ public class RoomBuyPanel extends BuyPanel<RoomInfo>
         return true;
     }
 
+    @Override
+    protected void addPurchasedUI (RoomInfo room, FlowPanel boughtPanel)
+    {
+        boughtPanel.add(new Label(_msgs.boughtRoom()));
+        boughtPanel.add(Link.create(_msgs.boughtRoomGo(), Pages.WORLD, "s" + room.sceneId));
+    }
+
+    protected static final PeopleMessages _msgs = GWT.create(PeopleMessages.class);
     protected static final WebRoomServiceAsync _roomsvc = (WebRoomServiceAsync)
         ServiceUtil.bind(GWT.create(WebRoomService.class), WebRoomService.ENTRY_POINT);
 }
