eff08f0c7f544ac9d4b1ceae4b2a9e1060d76a7b
Michael Bayne <mdb@threerings.net>
2009-03-03 17:07:10 +0000

    Nix A/B test. The results were 9.23% converted, 1.51% retained for the combined page and 8.47% converted and 1.40% retained for the split page, so we go back to the combined page.
    Note to future A/B test implementors. Don't use a redirect. Just show the
    landing page in question at #landing. Redirecting fucks up Google Analytics and
    makes it impossible to tell where people go *after* landing, which is the only
    way we can get an idea of how to improve the landing page.
    
    Ideally Google Analytics wouldn't fuck this up, but they do, so we cope.

--- src/gwt/client/landing/LandingPage.java
+++ src/gwt/client/landing/LandingPage.java
@@ -4,19 +4,13 @@
 package client.landing;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
-import com.threerings.msoy.landing.gwt.LandingService;
-import com.threerings.msoy.landing.gwt.LandingServiceAsync;
 import com.threerings.msoy.web.gwt.Args;
 import com.threerings.msoy.web.gwt.Pages;
-import com.threerings.msoy.web.gwt.WebMemberService;
-import com.threerings.msoy.web.gwt.WebMemberServiceAsync;
 
 import client.shell.CShell;
 import client.shell.Page;
 import client.util.Link;
-import client.util.ServiceUtil;
 
 /**
  * The main entry point for the landing page(s).
@@ -29,8 +23,6 @@ public class LandingPage extends Page
     public static String GAME_CONTEST = "gamecontest";
     public static String DESIGN_CONTEST = "designcontest";
     public static String DEVELOPER_INTRO = "devintro";
-    public static String LANDING_COMBINED = "combined";
-    public static String LANDING_SPLIT = "split";
 
     @Override // from Page
     public void onHistoryChanged (Args args)
@@ -41,12 +33,8 @@ public class LandingPage extends Page
         if (action.equals(CREATORS)) {
             setContent(_msgs.titleCreators(), new CreatorsPanel());
 
-        // landing page for deviant art contest
-        } else if (action.equals(DEVIANT_CONTEST)) {
-            Link.go(Pages.LANDING, "dawinners");
-
         // landing page for deviant art contest winners
-        } else if (action.equals(DEVIANT_CONTEST_WINNERS)) {
+        } else if (action.equals(DEVIANT_CONTEST) || action.equals(DEVIANT_CONTEST_WINNERS)) {
             setContent(_msgs.titleDAContestWinners(), new DAContestWinnersPanel());
 
         // landing page for flash game developer contest
@@ -61,50 +49,8 @@ public class LandingPage extends Page
         } else if (action.equals(DEVELOPER_INTRO)) {
             setContent(_msgs.landingTitle(), new DeveloperIntroPanel());
 
-        // combined (old) landing page
-        } else if (action.equals(LANDING_COMBINED)) {
+        } else {
             setContent(_msgs.landingTitle(), new LandingPanel());
-
-        // split games/rooms combined (new) landing page
-        } else if (action.equals(LANDING_SPLIT)) {
-            setContent(_msgs.landingTitle(), new SplitLandingPanel());
-
-        // A/B test between old combination and a new split landing page
-        } else {
-            runABTest();
-        }
-    }
-
-    /**
-     * Runs AB test defined on the landing page.
-     */
-    protected void runABTest ()
-    {
-        // list of redirects, based on the user's test group.
-        final String[] testpages = new String[] {
-            // since groups are 1-indexed, we use "group 0" to mean the default value.
-            LANDING_COMBINED,
-            // our test groups
-            LANDING_COMBINED, LANDING_SPLIT };
-
-        _membersvc.getABTestGroup(CShell.visitor, "2009 01 landing split", true,
-            new AsyncCallback<Integer>() {
-                public void onSuccess (Integer group) {
-                    gotTestGroup(testpages, group);
-                }
-
-                public void onFailure (Throwable cause) {
-                    gotTestGroup(testpages, -1);
-                }
-            });
-    }
-
-    protected void gotTestGroup (String[] testpages, int group)
-    {
-        if (group > 0 && group < testpages.length) {
-            Link.go(Pages.LANDING, testpages[group]);
-        } else {
-            Link.go(Pages.LANDING, testpages[0]);
         }
     }
 
@@ -115,8 +61,4 @@ public class LandingPage extends Page
     }
 
     protected static final LandingMessages _msgs = GWT.create(LandingMessages.class);
-    protected static final WebMemberServiceAsync _membersvc = (WebMemberServiceAsync)
-        ServiceUtil.bind(GWT.create(WebMemberService.class), WebMemberService.ENTRY_POINT);
-    protected static final LandingServiceAsync _landingsvc = (LandingServiceAsync)
-        ServiceUtil.bind(GWT.create(LandingService.class), LandingService.ENTRY_POINT);
 }
