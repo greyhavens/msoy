d74b92260e8f5b0cdec8c9344028d4235a66132d
Jamie Doornbos <jamie@threerings.net>
2009-03-05 06:21:42 +0000

    Plain text feed emails. Next up: html. This kludges in the gwt properties files until we have a more comprehensive server-side localiztion solution such as storing each user's preferred locale and selecting an appropriate bundle.

--- src/java/com/threerings/msoy/spam/server/SpamLogic.java
+++ src/java/com/threerings/msoy/spam/server/SpamLogic.java
@@ -1,3 +1,6 @@
+//
+// $Id$
+
 package com.threerings.msoy.spam.server;
 
 import java.util.List;
@@ -7,39 +10,107 @@ import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.samskivert.util.IntSet;
-import com.threerings.msoy.badge.data.BadgeType;
 import com.threerings.msoy.data.all.MediaDesc;
-import com.threerings.msoy.item.data.all.Item;
 import com.threerings.msoy.person.gwt.FeedItemGenerator;
 import com.threerings.msoy.person.gwt.FeedItemGenerator.Builder;
 import com.threerings.msoy.person.gwt.FeedItemGenerator.Icon;
 import com.threerings.msoy.person.gwt.FeedItemGenerator.Media;
 import com.threerings.msoy.person.gwt.FeedItemGenerator.Messages;
 import com.threerings.msoy.person.gwt.FeedItemGenerator.Plural;
+import com.threerings.msoy.person.gwt.FeedMessageType.Category;
 import com.threerings.msoy.person.gwt.FeedMessage;
 import com.threerings.msoy.person.gwt.FeedMessageAggregator;
 import com.threerings.msoy.person.gwt.FeedMessageType;
 import com.threerings.msoy.person.gwt.MyWhirledData.FeedCategory;
 import com.threerings.msoy.person.server.FeedLogic;
 import com.threerings.msoy.server.ServerConfig;
+import com.threerings.msoy.server.ServerMessages;
 import com.threerings.msoy.server.persist.MemberRecord;
 import com.threerings.msoy.server.persist.MemberRepository;
 import com.threerings.msoy.server.util.MailSender;
 import com.threerings.msoy.server.util.MailSender.Parameters;
 import com.threerings.msoy.web.gwt.Pages;
 import com.threerings.presents.annotation.BlockingThread;
+import com.threerings.util.MessageBundle;
 
 @Singleton @BlockingThread
 public class SpamLogic
 {
+    /**
+     * News feed category for the purposes of filling in a velocity template. NOTE: this class must
+     * be public so velocity can reflect its methods and members.
+     */
+    public class EmailFeedCategory
+    {
+        /** The category. */
+        public Category category;
+
+        /** The items. */
+        public List<EmailFeedItem> items;
+
+        /**
+         * Gets the name of this feed category. NOTE: this is called using reflection by velocity.
+         */
+        public String getCategoryName ()
+        {
+            return _dmsgs.get("feedCategory" + category.ordinal());
+        }
+
+        protected EmailFeedCategory (Category category, List<EmailFeedItem> items)
+        {
+            this.category = category;
+            this.items = items;
+        }
+    }
+
+    /**
+     * News feed item for the purposes of filling in a velocity template. NOTE: this class must be
+     * public so velocity can reflect its methods and members.
+     */
+    public class EmailFeedItem
+    {
+        /**
+         * Gets the plain text representation of this feed item. NOTE: this is called using
+         * reflection by velocity.
+         */
+        public String getPlainText ()
+        {
+            if (_content == null) {
+                EmailItemBuilder builder = new EmailItemBuilder();
+                FeedItemGenerator gen = new FeedItemGenerator(_memberId, true, builder, _messages);
+                gen.addMessage(_message);
+                _content = builder.buffer.toString();
+            }
+            return _content;
+        }
+
+        protected EmailFeedItem (int memberId, FeedMessage message)
+        {
+            _memberId = memberId;
+            _message = message;
+        }
+
+        protected int _memberId;
+        protected FeedMessage _message;
+        protected String _content;
+    }
+
+    /**
+     * Sends a feed email to the given member id. Returns true unless there were no feed items in
+     * the user's feed.
+     */
     public boolean sendFeedEmail (int memberId)
     {
+        // lazy init message bundles for now
+        initMessageBundles();
+
+        // load up the data
         final MemberRecord mrec = _memberRepo.loadMember(memberId);
         IntSet friendIds = _memberRepo.loadFriendIds(mrec.memberId);
-
         List<FeedCategory> categories = _feedLogic.loadFeedCategories(
             mrec, friendIds, ITEMS_PER_CATEGORY, null);
 
+        // convert to our accessible versions
         List<EmailFeedCategory> ecats = Lists.newArrayList();
         int total = 0;
         for (FeedCategory category : categories) {
@@ -54,171 +125,233 @@ public class SpamLogic
                 continue;
             }
             EmailFeedCategory ecat = new EmailFeedCategory(
-                FeedMessageType.Category.values()[category.category], eitems);
+                Category.values()[category.category], eitems);
             total += eitems.size();
             ecats.add(ecat);
         }
 
+        // bail if we've got no items
         if (total == 0) {
             return false;
         }
 
-        Parameters mailParams = new Parameters();
-        mailParams.set("feed", ecats);
-        _mailSender.sendTemplateEmail(mrec.accountName, ServerConfig.getFromAddress(),
-            "feed", mailParams);
+        // fire off the email, the template will take care of looping over categories and items
+        // TODO: we'll need more parameters here 
+        Parameters params = new Parameters();
+        params.set("feed", ecats);
+        _mailSender.sendTemplateEmail(
+            mrec.accountName, ServerConfig.getFromAddress(), MAIL_TEMPLATE, params);
         return true;
     }
 
-    protected static class EmailFeedCategory
+    /**
+     * Sets up the message bundles if they are not already.
+     */
+    protected void initMessageBundles ()
     {
-        public FeedMessageType.Category category;
-        public List<EmailFeedItem> items;
-
-        protected EmailFeedCategory (FeedMessageType.Category category, List<EmailFeedItem> items)
-        {
-            this.category = category;
-            this.items = items;
+        if (_pmsgs == null) {
+            _pmsgs = _serverMsgs.getBundle("feed.PersonMessages");
+        }
+        if (_dmsgs == null) {
+            _dmsgs = _serverMsgs.getBundle("feed.DynamicMessages");
         }
     }
 
-    protected static class EmailFeedItem
-    {
-        protected EmailFeedItem (int memberId, FeedMessage message)
-        {
-            _memberId = memberId;
-            _message = message;
-        }
-
-        public String getTextOnly ()
-        {
-            if (_content == null) {
-                EmailItemBuilder builder = new EmailItemBuilder();
-                FeedItemGenerator gen = new FeedItemGenerator(_memberId, true, builder, _messages);
-                gen.addMessage(_message);
-                _content = builder.buffer.toString();
-            }
-            return _content;
-        }
-
-        public String toString ()
-        {
-            FeedItemGenerator gen = new FeedItemGenerator(_memberId, true, null, _messages);
-            gen.addMessage(_message);
-            return null;
-        }
-
-        protected int _memberId;
-        protected FeedMessage _message;
-        protected String _content;
-    }
-
+    /**
+     * Implements the generator components, just wraps a string.
+     */
     protected static class StringWrapper
         implements Media, Icon
     {
+        /** The string we wrap. */
         public String str;
 
+        /**
+         * Creates a new wrapper.
+         */
         public StringWrapper (String str)
         {
             this.str = str;
         }
 
+        // from Object
         public String toString ()
         {
             return str;
         }
     }
 
+    /**
+     * Implementation of an feed item builder that keeps a string buffer of the item so far.
+     * TODO: separate buffer for html.
+     */
     protected static class EmailItemBuilder
         implements Builder
     {
+        /** The buffer we append to for this item. */
         public StringBuilder buffer = new StringBuilder();
 
+        // from Builder
         public void addIcon (Icon icon) {
             buffer.append(icon);
         }
 
+        // from Builder
         public void addMedia (Media media, String message) {
             buffer.append(message);
         }
 
+        // from Builder
         public void addMedia (Media[] media, String message) {
             buffer.append(message);
         }
 
+        // from Builder
         public void addText (String text) {
             buffer.append(text);
         }
 
+        // from Builder
         public Icon createGainedLevelIcon (String text) {
             return new StringWrapper(text);
         }
 
+        // from Builder
         public String createLink (String label, Pages page, String args) {
             return label;
         }
 
+        // from Builder
         public Media createMedia (MediaDesc md, Pages page, String args) {
             return new StringWrapper("");
         }
     }
 
-    protected static final Messages _messages = new Messages () {
-        public String action (FeedMessageType type, String subject, String object, Plural plural) {
-            if (plural == Plural.SUBJECT) {
-                subject = subject + " all";
-            }
-            String suffix = " thing";
-            if (plural == Plural.OBJECT) {
-                suffix += "ies";
-            }
-            return subject + " did " + type + " on " + object + suffix;
-        }
-
-        public String andCombine (String list, String item) {
-            return list + " and " + item;
-        }
-
-        public String badgeName (int code, String levelName) {
-            return BadgeType.getType(code).name() + " " + levelName;
-        }
-
-        public String briefLevelGain (String subject, String level) {
-            return subject + ": " + level;
-        }
-
-        public String commaCombine (String list, String item) {
-            return list + ", " + item;
-        }
-
-        public String describeItem (String typeName, String itemName) {
-            return "the " + typeName + " " + itemName;
-        }
-
-        public String medal (String medal, String group) {
-            return "the " + medal + " medal from " + group;
-        }
-
-        public String noGroupForMedal (String medalLink) {
-            return "the " + medalLink + " medal";
-        }
-
+    /** Messages instance that delegates to the bundles in our parent class. */
+    protected Messages _messages = new Messages () {
+        // from Messages
         public String typeName (String itemType) {
-            return Item.getClassForType(Byte.parseByte(itemType)).getSimpleName();
+            return _dmsgs.get("itemType" + itemType);
         }
 
-        public String unknownMember () {
-            return "Someone";
-        }
-
+        // from Messages
         public String you () {
-            return "You";
+            return _pmsgs.get("feedProfileMemberYou");
+        }
+
+        // from Messages
+        public String describeItem (String typeName, String itemName) {
+            return _pmsgs.get("descCombine", typeName, itemName);
+        }
+
+        // from Messages
+        public String badgeName (int code, String levelName) {
+            String hexCode = Integer.toHexString(code);
+            return _dmsgs.get("badge_" + hexCode, levelName);
+        }
+
+        // from Messages
+        public String noGroupForMedal (String medalLink) {
+            return _pmsgs.get("medalNoGroup", medalLink);
+        }
+
+        // from Messages
+        public String medal (String medal, String group) {
+            return _pmsgs.get("medal", medal, group);
+        }
+
+        // from Messages
+        public String unknownMember () {
+            return _pmsgs.get("feedProfileMemberUnknown");
+        }
+
+        // from Messages
+        public String action (
+            FeedMessageType type, String subject, String object, Plural plural) {
+            switch (type) {
+            case GLOBAL_ANNOUNCEMENT:
+                return _pmsgs.get("globalAnnouncement", object);
+
+            case FRIEND_ADDED_FRIEND:
+                return _pmsgs.get("friendAddedFriend", subject, object);
+
+            case FRIEND_UPDATED_ROOM:
+                switch (plural) {
+                case NONE:
+                    return _pmsgs.get("friendUpdatedRoom", subject, object);
+                case SUBJECT:
+                    return _pmsgs.get("friendsUpdatedRoom", subject);
+                case OBJECT:
+                    return _pmsgs.get("friendUpdatedRooms", subject, object);
+                }
+
+            case FRIEND_WON_TROPHY:
+                return plural == Plural.OBJECT ?
+                    _pmsgs.get("friendWonTrophies", subject, object) :
+                    _pmsgs.get("friendWonTrophy", subject, object);
+
+            case FRIEND_LISTED_ITEM:
+                return _pmsgs.get("friendListedItem", subject, object);
+
+            case FRIEND_GAINED_LEVEL:
+                return plural == Plural.SUBJECT ?
+                    _pmsgs.get("friendsGainedLevel", subject) :
+                    _pmsgs.get("friendGainedLevel", subject, object);
+
+            case FRIEND_WON_BADGE:
+                return plural == Plural.OBJECT ?
+                    _pmsgs.get("friendWonBadges", subject, object) :
+                    _pmsgs.get("friendWonBadge", subject, object);
+
+            case FRIEND_WON_MEDAL:
+                return _pmsgs.get("friendWonMedal", subject, object);
+
+            case GROUP_ANNOUNCEMENT:
+                return _pmsgs.get("groupAnnouncement", subject, object);
+
+            case GROUP_UPDATED_ROOM:
+                return _pmsgs.get("friendUpdatedRoom", subject, object);
+
+            case SELF_ROOM_COMMENT:
+                return _pmsgs.get("selfRoomComment", subject, object);
+
+            case SELF_ITEM_COMMENT:
+                return _pmsgs.get("selfItemComment", subject, object);
+
+            case SELF_FORUM_REPLY:
+                return _pmsgs.get("selfForumReply", subject, object);
+
+            default:
+                return "Unknown message type: " + subject + " did something to " + object + ".";
+            }
+        }
+
+        // from Messages
+        public String andCombine (String list, String item) {
+            return _pmsgs.get("andCombine", list, item);
+        }
+
+        // from Messages
+        public String briefLevelGain (String subject, String level) {
+            return _pmsgs.get("colonCombine", subject, level);
+        }
+
+        // from Messages
+        public String commaCombine (String list, String item) {
+            return _pmsgs.get("commaCombine", list, item);
         }
     };
+
+    // Message bundles
+    // TODO: proper localization
+    protected MessageBundle _pmsgs;
+    protected MessageBundle _dmsgs;
 
     @Inject protected MemberRepository _memberRepo; 
     @Inject protected FeedLogic _feedLogic;
     @Inject protected MailSender _mailSender;
+    @Inject protected ServerMessages _serverMsgs;
 
     protected static final int ITEMS_PER_CATEGORY = 50;
+    protected static final String MAIL_TEMPLATE = "feed";
 }
