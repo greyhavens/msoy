9639be19a8ca8374e4d53875791a3a89060b7555
Jamie Doornbos <jamie@threerings.net>
2009-03-28 10:30:19 +0000

    Fixed check for tardy forwards to not NPE in AuthName.equals

--- src/java/com/threerings/msoy/peer/server/MsoyPeerManager.java
+++ src/java/com/threerings/msoy/peer/server/MsoyPeerManager.java
@@ -410,10 +410,12 @@ public class MsoyPeerManager extends CrowdPeerManager
         _mobjCache.put(memobj.username, new MemObjCacheEntry(memobj, locals));
 
         // TEMP
-        long dt = System.currentTimeMillis() - _lastRequestTime;
-        if (dt < 60000 && memobj.username.equals(_lastRequestName)) {
-            log.warning("Oh my lanta! We seem to be a little late for this stashing!",
-                "user", memobj.username, "miss milliseconds", dt, new Exception());
+        if (_lastRequestName != null) {
+            long dt = System.currentTimeMillis() - _lastRequestTime;
+            if (dt < 60000 && memobj.username.equals(_lastRequestName)) {
+                log.warning("Oh my lanta! We seem to be a little late for this stashing!",
+                    "user", memobj.username, "miss milliseconds", dt, new Exception());
+            }
         }
     }
 
