36b36696d93f29792a4708d535b8ba3335ff8391
Jamie Doornbos <jamie@threerings.net>
2009-04-21 05:20:27 +0000

    Enable the "Fresh blood only" flag for game invites using whirled mail in production. I have changed this code before but must have missed the error message on commit. Damn!

--- src/gwt/client/people/GameInvitePanel.java
+++ src/gwt/client/people/GameInvitePanel.java
@@ -21,7 +21,6 @@ import com.google.gwt.user.client.ui.Widget;
 
 import com.threerings.gwt.ui.SmartTable;
 
-import com.threerings.msoy.data.all.DeploymentConfig;
 import com.threerings.msoy.data.all.MediaDesc;
 import com.threerings.msoy.game.gwt.GameDetail;
 import com.threerings.msoy.game.gwt.GameService;
@@ -166,9 +165,6 @@ public class GameInvitePanel extends InvitePanel
      */
     protected static class WhirledFriendsPanel extends SmartTable
     {
-        // holding off on new features until after EC2 deployment
-        public static final boolean FRESH_BLOOD = DeploymentConfig.devDeployment;
-
         /**
          * Creates a new friends panel.
          */
@@ -184,8 +180,7 @@ public class GameInvitePanel extends InvitePanel
 
             // instructions
             setText(row++, 0, _msgs.gameInviteWhirledTitle(), 1, "Bold");
-            setText(row++, 0, FRESH_BLOOD ? _msgs.gameInviteWhirledDetail(gameName) :
-                _msgs.gameInviteWhirledDetail_Old());
+            setText(row++, 0, _msgs.gameInviteWhirledDetail(gameName));
 
             // buttons to select and deselect all friends
             SmartTable selectors = new SmartTable();
@@ -199,15 +194,13 @@ public class GameInvitePanel extends InvitePanel
                     selectAll(false);
                 }
             }));
-            if (FRESH_BLOOD) {
-                _showAll = new CheckBox(_msgs.gameInviteWhirledShowAll());
-                selectors.setWidget(0, 2, _showAll);
-                _showAll.addClickListener(new ClickListener() {
-                    public void onClick (Widget sender) {
-                        downloadFriends();
-                    }
-                });
-            }
+            _showAll = new CheckBox(_msgs.gameInviteWhirledShowAll());
+            selectors.setWidget(0, 2, _showAll);
+            _showAll.addClickListener(new ClickListener() {
+                public void onClick (Widget sender) {
+                    downloadFriends();
+                }
+            });
             setWidget(row++, 0, selectors);
 
             // friends grid
@@ -262,7 +255,7 @@ public class GameInvitePanel extends InvitePanel
         protected void downloadFriends ()
         {
             // pass a zero game id to include people who have already played
-            int gameId = FRESH_BLOOD ? (_showAll.isChecked() ? 0 : _gameId) : 0;
+            int gameId = _showAll.isChecked() ? 0 : _gameId;
 
             // get friend list from the server; fill in our grid
             _invitesvc.getFriends(gameId, ROWS * COLS, new InfoCallback<List<MemberCard>>() {
