cbba0aa4b8cc84e0698f70d2cebc0ca90244f863
Jamie Doornbos <jamie@threerings.net>
2009-05-15 23:18:11 +0000

    New columns for attribution.

--- src/java/com/threerings/msoy/item/server/persist/CatalogRecord.java
+++ src/java/com/threerings/msoy/item/server/persist/CatalogRecord.java
@@ -48,9 +48,11 @@ public abstract class CatalogRecord extends PersistentRecord
     public static final ColumnExp PURCHASES = colexp(_R, "purchases");
     public static final ColumnExp RETURNS = colexp(_R, "returns");
     public static final ColumnExp FAVORITE_COUNT = colexp(_R, "favoriteCount");
+    public static final ColumnExp BASIS_ID = colexp(_R, "basisId");
+    public static final ColumnExp DERIVATION_COUNT = colexp(_R, "derivationCount");
     // AUTO-GENERATED: FIELDS END
 
-    public static final int SCHEMA_VERSION = 13;
+    public static final int SCHEMA_VERSION = 14;
 
     /** Converts this record to a runtime record. See {@link #toListingCard} for caveats.  */
     public static final Function<CatalogRecord,ListingCard> TO_CARD =
@@ -102,6 +104,12 @@ public abstract class CatalogRecord extends PersistentRecord
     /** A reference to the listed item. This value is not persisted. */
     @Transient
     public ItemRecord item;
+
+    /** The id of the listing that is the basis of this item. */
+    public int basisId;
+
+    /** The number of items for which this listing is a base. */
+    public int derivationCount;
 
     /**
      * Creates a runtime record from this persistent record.
