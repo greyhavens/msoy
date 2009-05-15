26151cecaf0960306e08b6972997ecec80512022
Michael Bayne <mdb@threerings.net>
2009-05-15 19:22:42 +0000

    Apparently removeHandler() doesn't execute immediately or some crazy shit, so our handler is not gone by the time selectTab() turns around and tries to dispatch an event. Wacky.

--- src/gwt/client/ui/NaviTabPanel.java
+++ src/gwt/client/ui/NaviTabPanel.java
@@ -6,6 +6,8 @@ package client.ui;
 import com.google.gwt.event.logical.shared.BeforeSelectionEvent;
 import com.google.gwt.event.logical.shared.BeforeSelectionHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
 
 import com.threerings.msoy.web.gwt.Pages;
 
@@ -27,11 +29,15 @@ public abstract class NaviTabPanel extends StyledTabPanel
      * what a page should call to configure the selected tab based on information from its page
      * arguments.
      */
-    public void activateTab (int tabIdx)
+    public void activateTab (final int tabIdx)
     {
         _handreg.removeHandler();
-        selectTab(tabIdx);
-        _handreg = addBeforeSelectionHandler(_onSelect);
+        DeferredCommand.add(new Command() {
+            public void execute () {
+                selectTab(tabIdx);
+                _handreg = addBeforeSelectionHandler(_onSelect);
+            }
+        });
     }
 
     protected abstract String getTabArgs (int tabidx);
