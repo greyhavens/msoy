5f51bd57aa63ef85481c60c633c0ec47aa6bb5aa
Ray Greenwell <ray@threerings.net>
2009-05-18 20:08:44 +0000

    This fixes it.

--- src/gwt/client/util/FlashClients.java
+++ src/gwt/client/util/FlashClients.java
@@ -304,7 +304,7 @@ public class FlashClients
      */
     protected static String getClientHeight ()
     {
-        return _clientFullHeight ? "100%" : (""+CLIENT_HEIGHT);
+        return _clientFullHeight ? "100%" : (CLIENT_HEIGHT + "px");
     }
 
     /**
