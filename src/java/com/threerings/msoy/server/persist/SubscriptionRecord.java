dcb41aaa73f2c76567b40395574cf486b69b3ccb
Ray Greenwell <ray@threerings.net>
2009-05-27 19:02:24 +0000

    Proposed SubscriptionRecord.

---
+++ src/java/com/threerings/msoy/server/persist/SubscriptionRecord.java
@@ -0,0 +1,53 @@
+//
+// $Id$
+
+package com.threerings.msoy.server.persist;
+
+import java.sql.Timestamp;
+
+import com.samskivert.depot.Key;
+import com.samskivert.depot.PersistentRecord;
+import com.samskivert.depot.annotation.*; // for Depot annotations
+import com.samskivert.depot.expression.ColumnExp;
+
+/**
+ * Tracks data about subscribers and ex-subscribers.
+ */
+public class SubscriptionRecord extends PersistentRecord
+{
+    // AUTO-GENERATED: FIELDS START
+    public static final Class<SubscriptionRecord> _R = SubscriptionRecord.class;
+    public static final ColumnExp MEMBER_ID = colexp(_R, "memberId");
+    public static final ColumnExp END_DATE = colexp(_R, "endDate");
+    public static final ColumnExp LAST_BAR_DISTRIBUTION = colexp(_R, "lastBarDistribution");
+    // AUTO-GENERATED: FIELDS END
+
+    /** To be updated when the structure of this class changes. */
+    public static final int SCHEMA_VERSION = 1;
+
+    /** The memberId of the subscriber. */
+    @Id
+    public int memberId;
+
+    /** The time at which the subscription ends. This may be in the past if this
+     * represents an expired subscription. It is useful to us to see people that are
+     * ex-subscribers. */
+    public Timestamp endDate;
+
+    /** The last time bars have been distributed to this subscriber. */
+    public Timestamp lastBarDistribution;
+
+    // AUTO-GENERATED: METHODS START
+    /**
+     * Create and return a primary {@link Key} to identify a {@link SubscriptionRecord}
+     * with the supplied key values.
+     */
+    public static Key<SubscriptionRecord> getKey (int memberId)
+    {
+        return new Key<SubscriptionRecord>(
+                SubscriptionRecord.class,
+                new ColumnExp[] { MEMBER_ID },
+                new Comparable[] { memberId });
+    }
+    // AUTO-GENERATED: METHODS END
+}
