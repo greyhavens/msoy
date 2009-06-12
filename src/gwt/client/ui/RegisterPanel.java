5860ede4ee246c4bbca6ca2624188a9fce88beb3
Michael Bayne <mdb@threerings.net>
2009-06-12 00:42:46 +0000

    Consolidated a bunch of other register bits into RegisterPanel.

--- src/gwt/client/ui/RegisterPanel.java
+++ src/gwt/client/ui/RegisterPanel.java
@@ -3,6 +3,8 @@
 
 package client.ui;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -11,27 +13,31 @@ import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.ui.ButtonBase;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
 
 import com.threerings.gwt.ui.SmartTable;
 import com.threerings.gwt.ui.WidgetUtil;
 
 import com.threerings.msoy.data.all.DeploymentConfig;
 import com.threerings.msoy.data.all.MemberName;
+import com.threerings.msoy.web.gwt.AccountInfo;
 import com.threerings.msoy.web.gwt.CaptchaException;
+import com.threerings.msoy.web.gwt.DateUtil;
+import com.threerings.msoy.web.gwt.Invitation;
 import com.threerings.msoy.web.gwt.RegisterInfo;
 import com.threerings.msoy.web.gwt.WebUserService;
 import com.threerings.msoy.web.gwt.WebUserServiceAsync;
 
+import client.shell.CShell;
 import client.shell.ShellMessages;
 import client.ui.DateFields;
 import client.ui.MsoyUI;
 import client.util.ClickCallback;
-import client.util.ConversionTrackingUtil;
 import client.util.RecaptchaUtil;
-import client.util.RegisterUtil;
 import client.util.ServiceUtil;
 import client.util.StringUtil;
 
@@ -47,6 +53,12 @@ public class RegisterPanel extends FlowPanel
     {
         setStyleName("register");
         setStepOne();
+
+        // if we have an active invitation, use the invite email as our default
+        Invitation invite = CShell.frame.getActiveInvitation();
+        if (invite != null && invite.inviteeEmail.matches(MsoyUI.EMAIL_REGEX)) {
+            _newemail.setText(invite.inviteeEmail);
+        }
     }
 
     protected void setStepOne ()
@@ -75,7 +87,7 @@ public class RegisterPanel extends FlowPanel
                     MsoyUI.errorNear(_cmsgs.regiFillAll(), _newemail);
                 } else if (StringUtil.isBlank(_newpass.getText().trim())) {
                     MsoyUI.errorNear(_cmsgs.regiFillAll(), _newpass);
-                } else if (!RegisterUtil.checkIsThirteen(_birthday)) {
+                } else if (!checkIsThirteen(_birthday.getDate())) {
                     MsoyUI.errorNear(_cmsgs.regiNotThirteen(), _birthday);
                 } else {
                     setStepTwo();
@@ -123,9 +135,8 @@ public class RegisterPanel extends FlowPanel
                     MsoyUI.errorNear(_cmsgs.regiMustTOS(), tosBox);
                     return false;
                 }
-                RegisterInfo info = RegisterUtil.createRegInfo(_newemail, _newpass, _birthday);
                 // TODO: switch to regi step 3: display "creating account"
-                _usersvc.register(DeploymentConfig.version, info, true, this);
+                _usersvc.register(DeploymentConfig.version, createRegInfo(), true, this);
                 return true;
             }
 
@@ -151,13 +162,69 @@ public class RegisterPanel extends FlowPanel
         clear();
         addHeader(true);
         add(MsoyUI.createHTML(_cmsgs.regiEmailConfirm(session.creds.accountName), null));
-        add(ConversionTrackingUtil.createAdWordsTracker());
-        add(ConversionTrackingUtil.createBeacon(session.entryVector));
+
+        if (!DeploymentConfig.devDeployment) {
+            add(createAdWordsTracker());
+            add(createBeacon(session.entryVector));
+        }
     }
 
     protected void addHeader (boolean complete)
     {
         // nada
+    }
+
+    protected RegisterInfo createRegInfo ()
+    {
+        RegisterInfo info = new RegisterInfo();
+        info.email = _newemail.getText().trim();
+        info.password = CShell.frame.md5hex(_newpass.getText().trim());
+        // info.displayName = _name.getText().trim();
+        info.displayName = "???";
+        info.birthday = _birthday.getDate();
+        info.info = new AccountInfo();
+        // info.info.realName = _rname.getText().trim();
+        info.info.realName = "";
+        info.expireDays = 1; // TODO: unmagick?
+        Invitation invite = CShell.frame.getActiveInvitation();
+        info.inviteId = (invite == null) ? null : invite.inviteId;
+        info.permaguestId = CShell.isPermaguest() ? CShell.getMemberId() : 0;
+        info.visitor = CShell.frame.getVisitorInfo();
+        info.captchaChallenge = RecaptchaUtil.isEnabled() ? RecaptchaUtil.getChallenge() : null;
+        info.captchaResponse = RecaptchaUtil.isEnabled() ? RecaptchaUtil.getResponse() : null;
+        return info;
+    }
+
+    protected static boolean checkIsThirteen (int[] birthday)
+    {
+        String[] today = new Date().toString().split(" ");
+        String thirteenYearsAgo = "";
+        for (int ii = 0; ii < today.length; ii++) {
+            if (today[ii].matches("[0-9]{4}")) {
+                int year = Integer.valueOf(today[ii]).intValue();
+                today[ii] = "" + (year - 13);
+            }
+            thirteenYearsAgo += today[ii] + " ";
+        }
+        return DateUtil.newDate(thirteenYearsAgo).compareTo(DateUtil.toDate(birthday)) >= 0;
+    }
+
+    protected static Widget createAdWordsTracker ()
+    {
+        Frame tracker = new Frame("./googleconversion.html");
+        tracker.setStyleName("AdWordsFrame");
+        return tracker;
+    }
+
+    protected static Widget createBeacon (String vector)
+    {
+        for (String[] entry : BEACONS) {
+            final String vecpat = entry[0], url = entry[1];
+            if (vector != null && vector.startsWith(vecpat)) {
+                return MsoyUI.createImage(url, "Beacon");
+            }
+        }
+        return null;
     }
 
     protected TextBox _newemail = MsoyUI.createTextBox("", MemberName.MAX_EMAIL_LENGTH, -1);
@@ -167,4 +234,9 @@ public class RegisterPanel extends FlowPanel
     protected static final ShellMessages _cmsgs = GWT.create(ShellMessages.class);
     protected static final WebUserServiceAsync _usersvc = (WebUserServiceAsync)
         ServiceUtil.bind(GWT.create(WebUserService.class), WebUserService.ENTRY_POINT);
+
+    // post-registration beacon URLs: yay for partners!
+    protected static final String[][] BEACONS = new String[][] {
+        { "a.shizmoo", "http://server.cpmstar.com/action.aspx?advertiserid=275&gif=1" }
+    };
 }
