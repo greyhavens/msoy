7906c9a615a8a0addb3434f8c0bdebd00b4195f2
Ray Greenwell <ray@threerings.net>
2009-06-30 21:07:27 +0000

    We now prefer configurability (memories) to remixability (a whole new media file), but we don't know if something's configurable until we load it up and run it in our environment, so maybe we shouldn't add the little remixable icon to highlight listings that we don't want highlighted.

--- src/gwt/client/item/ItemBox.java
+++ src/gwt/client/item/ItemBox.java
@@ -3,11 +3,11 @@
 
 package client.item;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
+//import com.google.gwt.core.client.GWT;
+//import com.google.gwt.user.client.ui.HorizontalPanel;
+//import com.google.gwt.user.client.ui.Image;
 
-import com.threerings.gwt.ui.WidgetUtil;
+//import com.threerings.gwt.ui.WidgetUtil;
 
 import com.threerings.msoy.data.all.MediaDesc;
 import com.threerings.msoy.web.gwt.Pages;
@@ -25,19 +25,19 @@ public class ItemBox extends ClickBox
         super(media, name, page, args);
         addStyleName("itemBox");
 
-        if (remixable) {
-            // add a wee icon indicating remixable
-            Image remix = new Image("/images/item/remixable_icon.png");
-            remix.setTitle(_imsgs.remixTip());
-
-            // arrange it next to the thumbnail
-            HorizontalPanel hpan = new HorizontalPanel();
-            hpan.add(getWidget(0, 0));
-            hpan.add(WidgetUtil.makeShim(2, 2));
-            hpan.add(remix);
-            setWidget(0, 0, hpan);
-        }
+//        if (remixable) {
+//            // add a wee icon indicating remixable
+//            Image remix = new Image("/images/item/remixable_icon.png");
+//            remix.setTitle(_imsgs.remixTip());
+//
+//            // arrange it next to the thumbnail
+//            HorizontalPanel hpan = new HorizontalPanel();
+//            hpan.add(getWidget(0, 0));
+//            hpan.add(WidgetUtil.makeShim(2, 2));
+//            hpan.add(remix);
+//            setWidget(0, 0, hpan);
+//        }
     }
 
-    protected static final ItemMessages _imsgs = GWT.create(ItemMessages.class);
+//    protected static final ItemMessages _imsgs = GWT.create(ItemMessages.class);
 }
