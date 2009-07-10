bbcbfade840b9654c61c312da4733d788809140c
Jamie Doornbos <jamie@threerings.net>
2009-07-10 05:07:47 +0000

    Removed unused import. Added keywords.

--- src/gwt/client/games/FBGameHeaderPanel.java
+++ src/gwt/client/games/FBGameHeaderPanel.java
@@ -10,7 +10,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 
 import com.threerings.gwt.ui.InlineLabel;
