a8e0a8aee79c4eece32f8ee7f81975925f14bedd
Par Winzell <zell@threerings.net>
2009-07-13 19:30:41 +0000

    Prepare this label for the fact that creators may now be brands, not just players. I think we should probably show a little brand logo in here too, or at least a star or something to differentiate brand names from player names.

--- src/gwt/client/ui/CreatorLabel.java
+++ src/gwt/client/ui/CreatorLabel.java
@@ -6,6 +6,7 @@ package client.ui;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.FlowPanel;
 
+import com.threerings.msoy.data.all.GroupName;
 import com.threerings.msoy.data.all.MemberName;
 
 import com.threerings.gwt.ui.InlineLabel;
@@ -14,21 +15,24 @@ import client.shell.ShellMessages;
 import client.util.Link;
 
 /**
- * Displays a creator's name with "by Foozle" where Foozle is a link to the creator's profile page.
+ * Displays a creator's name with "by Foozle" where Foozle is a link to the creator's profile page
+ * if it's a player, or its group page if it's a brand.
  */
 public class CreatorLabel extends FlowPanel
 {
     public CreatorLabel ()
     {
-        this(null);
+        addStyleName("creator");
     }
 
     public CreatorLabel (MemberName name)
     {
-        addStyleName("creator");
-        if (name != null) {
-            setMember(name);
-        }
+        setMember(name);
+    }
+
+    public CreatorLabel (GroupName name)
+    {
+        setBrand(name);
     }
 
     public void setMember (MemberName name)
@@ -36,6 +40,13 @@ public class CreatorLabel extends FlowPanel
         clear();
         add(new InlineLabel(_cmsgs.creatorBy() + " "));
         add(Link.memberView(name.toString(), name.getMemberId()));
+    }
+
+    public void setBrand (GroupName name)
+    {
+        clear();
+        add(new InlineLabel(_cmsgs.creatorBy() + " "));
+        add(Link.groupView(name.toString(), name.getGroupId()));
     }
 
     protected static final ShellMessages _cmsgs = GWT.create(ShellMessages.class);
