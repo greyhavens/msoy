4efd012ff6ae4a8ede06f5a89ed7eaf32df9b333
Jamie Doornbos <jamie@threerings.net>
2009-07-21 20:55:11 +0000

    Some wacky methods for dealing with javascript dictionary objects without having to use a native method each time.

--- src/gwt/client/util/JavaScriptUtil.java
+++ src/gwt/client/util/JavaScriptUtil.java
@@ -3,6 +3,8 @@
 
 package client.util;
 
+import java.util.Map;
+
 import com.google.gwt.core.client.JavaScriptObject;
 
 /**
@@ -81,5 +83,68 @@ public class JavaScriptUtil
     public static native boolean setBooleanElement (
         JavaScriptObject array, int index, boolean value) /*-{
         array[index] = value;
+    }-*/;
+
+    /**
+     * Converts a java map into a javascript dictionary that can then be passed into a native
+     * method. Returns null in the event of an exception and logs the error to the console, if
+     * active.
+     */
+    public static native <K, V> JavaScriptObject createDictionaryFromMap (Map<K, V> map) /*-{
+        try {
+            var obj = {};
+            var entries = map.@java.util.Map::entrySet()();
+            var iter = entries.@java.util.Set::iterator()();
+            while (iter.@java.util.Iterator::hasNext()()) {
+                var entry = iter.@java.util.Iterator::next()();
+                var key = entry.@java.util.Map.Entry::getKey()();
+                var value = entry.@java.util.Map.Entry::getValue()();
+                obj[key] = value;
+            }
+
+        } catch (e) {
+            if ($wnd.console) {
+                $wnd.console.log("Error in createFromMap: " + e);
+            }
+            return null;
+        }
+        return obj;
+    }-*/;
+
+    /**
+     * Attempts to retrieve an entry from a java script dictionary. Returns null in the event of
+     * an exception and logs the error to the console, if active.
+     */
+    public static native <K, V> V getDictionaryEntry (JavaScriptObject map, K key) /*-{
+        try {
+            return "" + map[key];
+
+        } catch (e) {
+            if ($wnd.console) {
+                $wnd.console.log("Error in getMapEntry: " + e);
+            }
+            return null;
+        }
+    }-*/;
+
+    /**
+     * Converts a javascript dictionary to a string for debugging. Returns null if there is an
+     * exception and logs the exception to the console if available.
+     */
+    public static native String dictionaryToString (JavaScriptObject map) /*-{
+        try {
+            var s = "{";
+            for (var key in map) {
+                s += "\"" + key + "\": \"" + map[key] + "\", ";
+            }
+            s += "}";
+            return s;
+
+        } catch (e) {
+            if ($wnd.console) {
+                $wnd.console.log("Error in dictionaryToString: " + e);
+            }
+            return null;
+        }
     }-*/;
 }
