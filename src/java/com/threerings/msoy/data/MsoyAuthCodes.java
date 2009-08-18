0ba8adf4f6a5a6d1d3a709a0091f2c06611ecd93
Jamie Doornbos <jamie@threerings.net>
2009-08-18 23:22:49 +0000

    Simple "under load" auth code to be returned when the server is busy. Patterned after yohoho.

--- src/java/com/threerings/msoy/data/MsoyAuthCodes.java
+++ src/java/com/threerings/msoy/data/MsoyAuthCodes.java
@@ -15,4 +15,8 @@ public interface MsoyAuthCodes extends AuthCodes, GwtAuthCodes
     /** An error code we use on msoy when the username or password are invalid. We 
      * do not tell people if an email address is valid, that's a security risk. */
     public static final String INVALID_LOGON = "m.invalid_logon";
+
+    /** An error code sent when the server is experiencing a high load. The client should tell the
+     * user to try again later, or automatically retry after a certain amount of time. */
+    public static final String UNDER_LOAD = "m.under_load";
 }
