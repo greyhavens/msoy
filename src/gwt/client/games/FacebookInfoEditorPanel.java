e546383c3679c7c7bcb614cedf146795c03d578e
Jamie Doornbos <jamie@threerings.net>
2009-09-08 22:06:28 +0000

    Front end for editing additional game thumbnails. Shoved into the facebook info panel for now since that is the current intended use, but it may be moved out if these are ever useful elsewhere for anything else

--- src/gwt/client/games/FacebookInfoEditorPanel.java
+++ src/gwt/client/games/FacebookInfoEditorPanel.java
@@ -3,6 +3,9 @@
 
 package client.games;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Button;
@@ -10,12 +13,20 @@ import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
+import com.threerings.gwt.ui.SmartTable;
+import com.threerings.gwt.ui.WidgetUtil;
+import com.threerings.msoy.data.all.MediaDesc;
 import com.threerings.msoy.game.gwt.FacebookInfo;
 import com.threerings.msoy.game.gwt.GameService;
 import com.threerings.msoy.game.gwt.GameServiceAsync;
+import com.threerings.msoy.game.gwt.GameThumbnail;
+import com.threerings.msoy.item.data.all.Item;
 
+import client.games.EditorUtil.ConfigException;
+import client.games.EditorUtil.MediaBox;
 import client.ui.MsoyUI;
 import client.util.ClickCallback;
+import client.util.InfoCallback;
 import client.util.StringUtil;
 
 /**
@@ -25,6 +36,7 @@ public class FacebookInfoEditorPanel extends FlowPanel
 {
     public FacebookInfoEditorPanel (final FacebookInfo info)
     {
+        setStyleName("fie");
         add(_mainFields = new EditorTable());
         _mainFields.addWidget(MsoyUI.createHTML(_msgs.fieIntro(), null), 2);
 
@@ -82,6 +94,70 @@ public class FacebookInfoEditorPanel extends FlowPanel
                 return true;
             }
         };
+
+        _gamesvc.loadAdditionalThumbnails(info.gameId, new InfoCallback<List<GameThumbnail>>() {
+            @Override public void onSuccess (List<GameThumbnail> result) {
+                setThumbnails(result);
+            }
+        });
+
+        add(WidgetUtil.makeShim(1, 20));
+        add(MsoyUI.createLabel(_msgs.fieThumbnailsTitle(), "Title"));
+        add(MsoyUI.createLabel(_msgs.fieThumbnailsTip(), "Tip"));
+        add(_thumbsTable = new SmartTable("thumbnails", 0, 10));
+        add(_thumbsEditor = new EditorTable());
+        Button saveThumbs = _thumbsEditor.addSaveRow();
+        new ClickCallback<Void>(saveThumbs) {
+            protected boolean callService () {
+                try {
+                    _gamesvc.updateAdditionalThumbnails(info.gameId, getThumbnails(), this);
+                } catch (ConfigException e) {
+                    MsoyUI.error(e.getMessage());
+                }
+                return true;
+            }
+            protected boolean gotResult (Void result) {
+                MsoyUI.info(_msgs.fieThumbnailsUpdated());
+                return true;
+            }
+        };
+    }
+
+    protected List<GameThumbnail> getThumbnails ()
+    {
+        List<GameThumbnail> thumbs = new ArrayList<GameThumbnail>();
+        for (MediaBox box : _thumbBoxes) {
+            if (box.getMedia() != null) {
+                thumbs.add(new GameThumbnail(EditorUtil.checkImageMedia(
+                    _msgs.egShot(), box.getMedia())));
+            }
+        }
+        return thumbs;
+    }
+
+    protected void setThumbnails (List<GameThumbnail> thumbnails)
+    {
+        _thumbsTable.clear();
+        final int size = MediaDesc.GAME_SHOT_SIZE;
+        for (int ii = 0; ii < GameThumbnail.MAX_COUNT; ++ii) {
+            EditorUtil.MediaBox box = new EditorUtil.MediaBox(size, "thumb" + ii,
+                ii < thumbnails.size() ? thumbnails.get(ii).media : null) {
+                @Override public void mediaUploaded (String name, MediaDesc desc, int w, int h) {
+                    int targetW = MediaDesc.getWidth(size), targetH = MediaDesc.getHeight(size);
+                    if (w != targetW || h != targetH) {
+                        MsoyUI.error(_msgs.errInvalidShot(
+                            String.valueOf(targetW), String.valueOf(targetH)));
+                    } else {
+                        super.mediaUploaded(name, desc, w, h);
+                    }
+                }
+                @Override protected void mediaModified () {
+                    _thumbsEditor.mediaModified();
+                }
+            };
+            _thumbsTable.setWidget(ii / THUMB_COLS, ii % THUMB_COLS, box);
+            _thumbBoxes.add(box);
+        }
     }
 
     protected void updateAppLink (FacebookInfo info)
@@ -91,8 +167,12 @@ public class FacebookInfoEditorPanel extends FlowPanel
     }
 
     protected EditorTable _mainFields;
+    protected SmartTable _thumbsTable;
+    protected EditorTable _thumbsEditor; // just used for the "Save" button
+    protected List<MediaBox> _thumbBoxes = new ArrayList<MediaBox>();
     protected int _viewRow;
 
+    protected static final int THUMB_COLS = 2;
     protected static final GamesMessages _msgs = GWT.create(GamesMessages.class);
     protected static final GameServiceAsync _gamesvc = GWT.create(GameService.class);
 }
