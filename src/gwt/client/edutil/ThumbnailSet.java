ab6e838150c522e6f94fafe6efd87389e233a98b
Jamie Doornbos <jamie@threerings.net>
2009-09-28 18:49:03 +0000

    Added a few more collection-y type methods to ThumbnailSet for use in the upcoming shiny new editor.

--- src/gwt/client/edutil/ThumbnailSet.java
+++ src/gwt/client/edutil/ThumbnailSet.java
@@ -12,24 +12,62 @@ import java.util.Map;
 import com.threerings.msoy.facebook.gwt.FeedThumbnail;
 
 /**
- * Organizes a mixed list of game thumbnails and provides convenient access by type and
+ * Organizes a mixed list of feed thumbnails and provides convenient access by code and
  * variant.
  */
 public class ThumbnailSet
 {
     /**
-     * Creates a new thumbnail set.
+     * Creates a new empty set.
      */
-    public ThumbnailSet (List<FeedThumbnail> allThumbnails)
+    public ThumbnailSet ()
     {
-        for (FeedThumbnail thumb : allThumbnails) {
-            get(thumb.code, thumb.variant).add(thumb);
+    }
+
+    /**
+     * Creates a new thumbnail set with the given contents.
+     */
+    public ThumbnailSet (List<FeedThumbnail> thumbnails)
+    {
+        addAll(thumbnails);
+    }
+
+    /**
+     * Adds all of the given thumbnails to this set.
+     */
+    public void addAll (List<FeedThumbnail> thumbnails)
+    {
+        for (FeedThumbnail thumb : thumbnails) {
+            add(thumb);
         }
     }
 
     /**
-     * Gets a list of thumbnails for a given type and variant, creating the list if it does
-     * not exist. Modifications will take effect.
+     * Adds the given thumbnail to this set.
+     */
+    public void add (FeedThumbnail thumbnail)
+    {
+        get(thumbnail.code, thumbnail.variant).add(thumbnail);
+        _deepSize++;
+    }
+
+    /**
+     * Extracts the contents of this set as a list.
+     */
+    public List<FeedThumbnail> toList ()
+    {
+        List<FeedThumbnail> thumbnails = new ArrayList<FeedThumbnail>();
+        for (Map<String, List<FeedThumbnail>> vmap : _organized.values()) {
+            for (List<FeedThumbnail> thumbs : vmap.values()) {
+                thumbnails.addAll(thumbs);
+            }
+        }
+        return thumbnails;
+    }
+
+    /**
+     * Gets a list of thumbnails for a given code and variant, creating the list if it does
+     * not exist. The resulting list should not be modified.
      */
     public List<FeedThumbnail> get (String code, String variant)
     {
@@ -42,8 +80,8 @@ public class ThumbnailSet
     }
 
     /**
-     * Gets a map of variant to thumbnail list for a given type, creating the map if it does
-     * not exist. Modifications will take effect.
+     * Gets a map of variant to thumbnail list for a given code, creating the map if it does
+     * not exist. The resulting map should not be modified.
      */
     public Map<String, List<FeedThumbnail>> getVariantMap (String code)
     {
@@ -56,7 +94,18 @@ public class ThumbnailSet
     }
 
     /**
-     * Gets a sorted list of variants present in the set for the given type.
+     * Gets a sorted list of codes present in the set.
+     */
+    public List<String> getCodes ()
+    {
+        List<String> codes = new ArrayList<String>();
+        codes.addAll(_organized.keySet());
+        Collections.sort(codes);
+        return codes;
+    }
+
+    /**
+     * Gets a sorted list of variants present in the set for the given code.
      */
     public List<String> getVariants (String code)
     {
@@ -66,6 +115,35 @@ public class ThumbnailSet
         return variants;
     }
 
+    /**
+     * Removes all thumbnails with the given code and variant from this set.
+     */
+    public boolean remove (String code, String variant)
+    {
+        Map<String, List<FeedThumbnail>> vmap = _organized.get(code);
+        if (vmap != null) {
+            List<FeedThumbnail> thumbs = vmap.get(variant);
+            if (thumbs != null) {
+                vmap.remove(variant);
+                _deepSize -= thumbs.size();
+                if (vmap.size() == 0) {
+                    _organized.remove(code);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if this set is empty.
+     */
+    public boolean isEmpty ()
+    {
+        return _deepSize == 0;
+    }
+
+    protected int _deepSize;
     protected Map<String, Map<String, List<FeedThumbnail>>> _organized =
         new HashMap<String, Map<String, List<FeedThumbnail>>>();
 }
