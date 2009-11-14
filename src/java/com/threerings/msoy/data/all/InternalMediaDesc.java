dc5c04bc2b329151ed2c4aceb4e4760464511586
Par Winzell <zell@threerings.net>
2009-11-14 14:46:26 +0000

    A somewhat cheesy class that lets us point to an existing media asset at an arbitrary (relative to the serverURL) path. This is a bit cheesy, and should be used only for internal purposes -- don't stream this class -- but it drastically simplifies providing default values in e.g. the Theme class.

---
+++ src/java/com/threerings/msoy/data/all/InternalMediaDesc.java
@@ -0,0 +1,42 @@
+//
+// $Id: StaticMediaDesc.java 10116 2008-07-27 15:40:58Z mdb $
+
+package com.threerings.msoy.data.all;
+
+/**
+ * Provides a "faked" media descriptor for media served internally at arbitrary relative URLs.
+ * NOTE: Unless {@link MediaDesc} and {@link StaticMediaDesc}, this class does not exist in the
+ * AS3 client, and must not be streamed there.
+ */
+public class InternalMediaDesc extends MediaDesc
+{
+    /** Used for unserialization. */
+    public InternalMediaDesc ()
+    {
+    }
+
+    /**
+     * Creates a configured static media descriptor.
+     */
+    public InternalMediaDesc (String path, byte mimeType, String mediaType)
+    {
+        this(path, mimeType, mediaType, NOT_CONSTRAINED);
+    }
+
+    /**
+     * Creates a configured static media descriptor.
+     */
+    public InternalMediaDesc (String path, byte mimeType, String mediaType, byte constraint)
+    {
+        super((byte[]) null, mimeType, constraint);
+        _path = path;
+    }
+
+    @Override // from MediaDesc
+    public String getMediaPath ()
+    {
+        return DeploymentConfig.serverURL + _path + mimeTypeToSuffix(mimeType);
+    }
+
+    protected String _path;
+}
