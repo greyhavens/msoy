09b0f61ac3147b0a74135b88d84061137eb3385e
Par Winzell <zell@threerings.net>
2010-03-09 05:28:58 +0000

    A simple funnel graph.

--- projects/mcell/src/webapp/whirled.js
+++ projects/mcell/src/webapp/whirled.js
@@ -1,3 +1,7 @@
+if (!whirled) {
+    var whirled = {};
+}
+
 whirled.addCharts = function () {
     eval(bedrock.include({
             'bedrock.util': ['log'],
@@ -9,6 +13,39 @@ whirled.addCharts = function () {
         }));
 
     return function() {
+        addChart("economy", "exchange_rate", "Exchange Rate", function () {
+            var options = {
+                xaxis: { mode: "time", minTickSize: [1, "day"]}
+            };
+            return new SelfContainedEventChart(
+                "DailyExchangeRate", function (ev, collector) {
+                    collector.assume("Exchange Rate").add(
+                        [ev.timestamp, ev.rate]);
+                }, options);
+        });
+
+        addChart("funnel", "funnel_web", "Conversion and Retention", function () {
+            var sourceNames = new List([
+                ["lost", "Lost"],
+                ["played", "Played"],
+                ["converted", "Converted"],
+                ["returned", "Returned"],
+                ["retained", "Retained"]
+            ]);
+            var sources = new CheckBoxes("Sources", "sources", sourceNames);
+            var group = new RadioButtons("Group", "group", ["web", "embed" ]);
+            function valueExtractor (event, name) {
+                if (event.group == 'web' && name == 'lost') {
+                    alert("Foo: " + event.lost + ", bar: " + event.date);
+                }
+                return (group.value == event.group && sources.has(name)) ?
+                    (event[name] || 0) : 0;
+            }
+            var chart = new StackedBarChart(
+                "DailyVisitorFutures", sourceNames, valueExtractor, {controls:[sources, group]});
+            chart.extractKey = function (ev) { return ev.date; }
+            return chart;
+        });
     }
 }();
 
