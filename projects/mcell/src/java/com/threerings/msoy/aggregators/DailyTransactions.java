8229d2bfa2db18d850a8d7729b5c6d700998c8a1
Par Winzell <zell@threerings.net>
2010-04-06 20:52:37 +0000

    Further desperate attempts to track down this bizarre duplicate-key bug.

--- projects/mcell/src/java/com/threerings/msoy/aggregators/DailyTransactions.java
+++ projects/mcell/src/java/com/threerings/msoy/aggregators/DailyTransactions.java
@@ -21,7 +21,7 @@ import com.threerings.panopticon.common.event.EventData;
 import com.threerings.panopticon.common.event.EventDataBuilder;
 import com.threerings.panopticon.eventstore.EventWriter;
 
-@Aggregator(output=DailyTransactions.OUTPUT_EVENT_NAME/*, incremental="timestamp" */)
+@Aggregator(output=DailyTransactions.OUTPUT_EVENT_NAME /*, incremental="timestamp" */)
 public class DailyTransactions
     implements JavaAggregator<DailyTransactions.TransactionKey>,
                KeyFactory<DailyTransactions.TransactionKey>
@@ -93,10 +93,10 @@ public class DailyTransactions
                 keys.add(new TransactionKey(keyInitData.eventData, Currency.COINS));
             }
             if (keyInitData.eventData.getDefaultInt("deltaBars", 0) != 0) {
-                keys.add(new TransactionKey(keyInitData.eventData, Currency.BARS));
+//                keys.add(new TransactionKey(keyInitData.eventData, Currency.BARS));
             }
             if (keyInitData.eventData.getDefaultInt("deltaBling", 0) != 0) {
-                keys.add(new TransactionKey(keyInitData.eventData, Currency.BLING));
+//                keys.add(new TransactionKey(keyInitData.eventData, Currency.BLING));
             }
         }
         return keys;
