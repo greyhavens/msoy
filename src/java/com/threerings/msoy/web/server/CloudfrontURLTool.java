8062481ac0fc72d7f9f4b3ed4ab853dd912f21a1
Par Winzell <zell@threerings.net>
2010-11-02 23:43:18 +0000

    Untested but (I think) complete version of URL signer. Not much to it really.

--- src/java/com/threerings/msoy/web/server/CloudfrontURLTool.java
+++ src/java/com/threerings/msoy/web/server/CloudfrontURLTool.java
@@ -6,6 +6,11 @@ package com.threerings.msoy.web.server;
 import java.net.URL;
 import java.net.MalformedURLException;
 
+import java.security.KeyFactory;
+import java.security.Signature;
+import java.security.GeneralSecurityException;
+import java.security.spec.PKCS8EncodedKeySpec;
+
 import org.apache.commons.codec.binary.Base64;
 
 import static com.threerings.msoy.Log.log;
@@ -20,18 +25,18 @@ public class CloudfrontURLTool
     /**
      * This class must be instantiated with the private half of a CloudFront signature key pair.
      */
-    public CloudfrontURLTool (String signingKeyId, String signingPrivateKey)
+    public CloudfrontURLTool (String signingKeyId, String signingKey)
     {
-        this(signingKeyId, Base64.decodeBase64(signingPrivateKey.getBytes()));
+        this(signingKeyId, Base64.decodeBase64(signingKey.getBytes()));
     }
 
     /**
      * This class must be instantiated with the private half of a CloudFront signature key pair.
      */
-    public CloudfrontURLTool (String signingKeyId, byte[] privateKey)
+    public CloudfrontURLTool (String signingKeyId, byte[] signingKeyBytes)
     {
         _signingKeyId = signingKeyId;
-        _privateKey = privateKey;
+        _signingKeyBytes = signingKeyBytes;
     }
 
     /**
@@ -40,12 +45,12 @@ public class CloudfrontURLTool
      *
      * TODO: Remove restrictions, they are only motivated by laziness.
      */
-    public void signURL (String urlString, int expirationEpoch)
+    public String signURL (String nakedUrl, int expirationEpoch)
         throws CloudfrontException
     {
         URL url;
         try {
-            url = new URL(urlString);
+            url = new URL(nakedUrl);
         } catch (MalformedURLException e) {
             throw new CloudfrontException("Bad URL.", e);
         }
@@ -57,11 +62,31 @@ public class CloudfrontURLTool
             throw new CloudfrontException("Can't sign URLs with query bits.");
         }
 
-        // {"Statement":[{"Resource":"RESOURCE","Condition":{"DateLessThan":{"AWS:EpochTime":EXPIRES}}}]}
-        String policy = "{\"Statement\":[{\"Resource\":\"" + urlString +
+        // {"Statement":[{"Resource":"RSRC","Condition":{"DateLessThan":{"AWS:EpochTime":EXPR}}}]}
+        String policy = "{\"Statement\":[{\"Resource\":\"" + nakedUrl +
             "\",\"Condition\":{\"DateLessThan\":{\"AWS:EpochTime\":" + expirationEpoch + "}}}]}";
+
+        byte[] sigBytes;
+        try {
+            Signature sig = Signature.getInstance("SHA1withRSA");
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            sig.initSign(keyFactory.generatePrivate(new PKCS8EncodedKeySpec(_signingKeyBytes)));
+            sig.update(policy.getBytes());
+            sigBytes = sig.sign();
+
+        } catch (GeneralSecurityException e) {
+            throw new CloudfrontException("Cryptographic failure signing URL", e);
+        }
+
+        String encSig = new String(Base64.encodeBase64(sigBytes))
+            .replace("+", "-").replace("=", "_").replace("/", "~");
+
+        String signedUrl = nakedUrl + "?Expires=" + expirationEpoch + "&Key-Pair-Id=" +
+            _signingKeyId + "&Signature=" + encSig;
+
+        return signedUrl;
     }
 
     protected String _signingKeyId;
-    protected byte[] _privateKey;
+    protected byte[] _signingKeyBytes;
 }
