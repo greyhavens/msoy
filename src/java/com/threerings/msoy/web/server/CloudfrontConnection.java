8846e3c1f2097620af2a6823a0ff926b764d77f7
Par Winzell <zell@threerings.net>
2010-11-03 19:47:33 +0000

    Break all the complex types into a separate class, just to get the line count down a bit.

--- src/java/com/threerings/msoy/web/server/CloudfrontConnection.java
+++ src/java/com/threerings/msoy/web/server/CloudfrontConnection.java
@@ -21,7 +21,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
 import java.util.TimeZone;
 
 import javax.crypto.Mac;
@@ -30,11 +29,9 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.EndElement;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 
 import com.megginson.sax.XMLWriter;
 import org.xml.sax.SAXException;
@@ -58,6 +55,7 @@ import org.apache.commons.httpclient.protocol.Protocol;
 import com.samskivert.util.StringUtil;
 
 import com.threerings.msoy.server.ServerConfig;
+import com.threerings.msoy.web.server.CloudfrontTypes.*;
 
 import static com.threerings.msoy.Log.log;
 
@@ -163,363 +161,6 @@ public class CloudfrontConnection
         }
     }
 
-    public static abstract class ElementIterable
-    {
-        public void iterateOverElements (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            do {
-                if (!nextElement(reader)) {
-                    throw new XMLStreamException("Unexpected event: " + reader.peek());
-                }
-            } while (!(reader.peek() instanceof EndElement));
-        }
-
-        public abstract boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException;
-    }
-
-    public static abstract class ContainerElement extends ElementIterable
-    {
-        public void recurseInto (CloudfrontEventReader reader, String elementName)
-            throws XMLStreamException
-        {
-            reader.expectElementStart(elementName);
-            iterateOverElements(reader);
-            reader.expectElementEnd(elementName);
-        }
-    }
-
-    public static abstract class CloudFrontComplexType<T extends CloudFrontComplexType>
-        extends ContainerElement
-    {
-        public T initialize (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            recurseInto(reader, typeElement());
-            if (!isComplete()) {
-                throw new XMLStreamException("Got partial object: " + this);
-            }
-
-            @SuppressWarnings("unchecked")
-            T tThis = (T) this;
-            return tThis;
-        }
-
-        public abstract String typeElement ();
-        public abstract boolean isComplete ();
-
-        public String toString ()
-        {
-            return StringUtil.fieldsToString(this);
-        }
-    }
-
-    public static class OriginAccessIdentitySummary
-        extends CloudFrontComplexType<OriginAccessIdentitySummary>
-    {
-        public String id;
-        public String s3CanonicalUserId;
-        public String comment;
-
-        public boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            String str;
-            if (null != (str = reader.maybeString("Id"))) {
-                id = str;
-            } else if (null != (str = reader.maybeString("S3CanonicalUserId"))) {
-                s3CanonicalUserId = str;
-            } else if (null != (str = reader.maybeString("Comment"))) {
-                comment = str;
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        public String typeElement ()
-        {
-            return "CloudFrontOriginAccessIdentitySummary";
-        }
-
-        public boolean isComplete ()
-        {
-            return id != null && s3CanonicalUserId != null;
-        }
-    }
-
-    public static class DistributionSummary
-        extends CloudFrontComplexType<DistributionSummary>
-    {
-        public String id;
-        public String status;
-        public Date lastModifiedTime;
-        public String domainName;
-        public String origin;
-        public Set<String> cnames = Sets.newHashSet();
-        public String comment;
-        public Boolean enabled;
-        public boolean selfIsSigner;
-        public List<String> trustedAwsSigners = Lists.newArrayList();
-
-        public boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            String str; Date date; Boolean bool;
-
-            if (null != (str = reader.maybeString("Id"))) {
-                id = str;
-            } else if (null != (str = reader.maybeString("Status"))) {
-                status = str;
-            } else if (null != (date = reader.maybeDate("LastModifiedTime"))) {
-                lastModifiedTime = date;
-            } else if (null != (str = reader.maybeString("DomainName"))) {
-                domainName = str;
-            } else if (null != (str = reader.maybeString("CNAME"))) {
-                cnames.add(str);
-            } else if (null != (str = reader.maybeString("Origin"))) {
-                origin = str;
-            } else if (null != (str = reader.maybeString("Comment"))) {
-                comment = str;
-            } else if (null != (bool = reader.maybeBoolean("Enabled"))) {
-                enabled = bool;
-
-            } else if (reader.peekForElement("TrustedSigners")) {
-                new ContainerElement() {
-                    public boolean nextElement (CloudfrontEventReader reader) throws XMLStreamException {
-                        String str;
-                        if (null != (str = reader.maybeString("Self"))) {
-                            selfIsSigner = true;
-                        } else if (null != (str = reader.maybeString("AwsAccountNumber"))) {
-                            trustedAwsSigners.add(str);
-                        } else {
-                            return false;
-                        }
-                        return true;
-                    }
-                }.recurseInto(reader, "TrustedSigners");
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        public String typeElement ()
-        {
-            return "DistributionSummary";
-        }
-
-        public boolean isComplete ()
-        {
-            return id != null && status != null && lastModifiedTime != null && domainName != null
-                && origin != null && enabled != null;
-        }
-    }
-
-    public static class Signer
-        extends CloudFrontComplexType<Signer>
-    {
-        public boolean isSelf;
-        public String awsAccountNumber;
-        public Set<String> keyIds = Sets.newHashSet();
-
-        public boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            String str;
-            if (null != (str = reader.maybeString("Self"))) {
-                isSelf = true;
-            } else if (null != (str = reader.maybeString("AwsAccountNumber"))) {
-                awsAccountNumber = str;
-            } else if (null != (str = reader.maybeString("KeyPairId"))) {
-                keyIds.add(str);
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        public String typeElement ()
-        {
-            return "Signer";
-        }
-
-        public boolean isComplete ()
-        {
-            return isSelf || (awsAccountNumber != null);
-        }
-    }
-
-    public static class Distribution
-        extends CloudFrontComplexType<Distribution>
-    {
-        public String id;
-        public String status;
-        public Integer inProgressInvalidationBatches;
-        public Date lastModifiedTime;
-        public String domainName;
-        public List<Signer> activeTrustedSigners = Lists.newArrayList();
-        public DistributionConfig config;
-
-        public boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            String str; Date date; Integer n;
-
-            if (null != (str = reader.maybeString("Id"))) {
-                id = str;
-            } else if (null != (str = reader.maybeString("Status"))) {
-                status = str;
-            } else if (null != (n = reader.maybeInt("InProgressInvalidationBatches")))  {
-                inProgressInvalidationBatches = n;
-            } else if (null != (date = reader.maybeDate("LastModifiedTime"))) {
-                lastModifiedTime = date;
-            } else if (null != (str = reader.maybeString("DomainName"))) {
-                domainName = str;
-
-            } else if (reader.peekForElement("ActiveTrustedSigners")) {
-                new ContainerElement() {
-                    public boolean nextElement (CloudfrontEventReader reader) throws XMLStreamException {
-                        if (reader.peekForElement("Signer")) {
-                            activeTrustedSigners.add(new Signer().initialize(reader));
-                            return true;
-                        }
-                        return false;
-                    }
-                }.recurseInto(reader, "ActiveTrustedSigners");
-
-            } else if (reader.peekForElement("DistributionConfig")) {
-                config = new DistributionConfig().initialize(reader);
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        public String typeElement ()
-        {
-            return "Distribution";
-        }
-
-        public boolean isComplete ()
-        {
-            return id != null && status != null && lastModifiedTime != null && domainName != null
-                && config != null;
-        }
-    }
-
-    public static class Logging
-        extends CloudFrontComplexType<Logging>
-    {
-        public String bucket;
-        public String prefix;
-
-        public boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            String str;
-            if (null != (str = reader.maybeString("Bucket"))) {
-                bucket = str;
-            } else if (null != (str = reader.maybeString("Prefix"))) {
-                prefix = str;
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        public String typeElement ()
-        {
-            return "Logging";
-        }
-
-        public boolean isComplete ()
-        {
-            return bucket != null;
-        }
-    }
-
-    public static class DistributionConfig
-        extends CloudFrontComplexType<DistributionConfig>
-    {
-        public String origin;
-        public String callerReference;
-        public String cname;
-        public String comment;
-        public Boolean enabled;
-        public String defaultRootObject;
-        public Logging logging;
-        public String originAccessIdentity;
-        public boolean selfIsSigner;
-        public List<String> trustedAwsSigners = Lists.newArrayList();
-        public List<String> requiredProtocols = Lists.newArrayList();
-
-        public boolean nextElement (CloudfrontEventReader reader)
-            throws XMLStreamException
-        {
-            String str; Boolean bool;
-
-            if (null != (str = reader.maybeString("Origin"))) {
-                origin = str;
-            } else if (null != (str = reader.maybeString("CallerReference"))) {
-                callerReference = str;
-            } else if (null != (str = reader.maybeString("CNAME"))) {
-                cname = str;
-            } else if (null != (str = reader.maybeString("Comment"))) {
-                comment = str;
-            } else if (null != (bool = reader.maybeBoolean("Enabled"))) {
-                enabled = bool;
-            } else if (null != (str = reader.maybeString("DefaultRootObject"))) {
-                defaultRootObject = str;
-            } else if (null != (str = reader.maybeString("OriginAccessIdentity"))) {
-                originAccessIdentity = str;
-            } else if (reader.peekForElement("Logging")) {
-                logging = new Logging().initialize(reader);
-
-            } else if (reader.peekForElement("TrustedSigners")) {
-                new ContainerElement() {
-                    public boolean nextElement (CloudfrontEventReader reader) throws XMLStreamException {
-                        String str;
-                        if (null != (str = reader.maybeString("Self"))) {
-                            selfIsSigner = true;
-                        } else if (null != (str = reader.maybeString("AwsAccountNumber"))) {
-                            trustedAwsSigners.add(str);
-                        } else {
-                            return false;
-                        }
-                        return true;
-                    }
-                }.recurseInto(reader, "TrustedSigners");
-
-            } else if (reader.peekForElement("RequiredProtocols")) {
-                new ContainerElement() {
-                    public boolean nextElement (CloudfrontEventReader reader) throws XMLStreamException {
-                        String str;
-                        if (null != (str = reader.maybeString("Protocol"))) {
-                            requiredProtocols.add(str);
-                            return true;
-                        }
-                        return false;
-                    }
-                }.recurseInto(reader, "RequiredProtocols");
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        public String typeElement ()
-        {
-            return "DistributionConfig";
-        }
-
-        public boolean isComplete ()
-        {
-            return origin != null && callerReference != null && enabled != null;
-        }
-    }
-
     public List<OriginAccessIdentitySummary> getOriginAccessIdentities ()
         throws CloudfrontException
     {
@@ -605,12 +246,16 @@ public class CloudfrontConnection
         });
     }
 
-    public String getDistributionConfig (String distribution)
+    public DistributionConfig getDistributionConfig (String distribution)
         throws CloudfrontException
     {
         // GET /2010-08-01/distribution/DistID/config
         GetMethod method = new GetMethod(API.DISTRIBUTION.build(distribution, "config"));
-        return executeAndReturn(method, null);
+        return executeAndReturn(method, new ReturnBodyParser<DistributionConfig>() {
+            public DistributionConfig parseBody (CloudfrontEventReader reader) throws XMLStreamException {
+                return new DistributionConfig().initialize(reader);
+            }
+        });
     }
 
     public String getInvalidations (String distribution)
@@ -732,7 +377,7 @@ public class CloudfrontConnection
         try {
             InputStream stream = execute(method);
             if (parser != null) {
-                CloudfrontEventReader reader = 
+                CloudfrontEventReader reader =
                     new CloudfrontEventReader(_xmlFactory.createXMLEventReader(stream));
                 reader.expectType(XMLStreamConstants.START_DOCUMENT);
                 T val = parser.parseBody(reader);
