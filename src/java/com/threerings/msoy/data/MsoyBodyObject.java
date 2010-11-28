48202a807ed51a6343410e146e271d0b3314e06d
Par Winzell <zell@threerings.net>
2010-11-28 22:50:53 +0000

    Didn't mean to lose the documentation.

--- src/java/com/threerings/msoy/data/MsoyBodyObject.java
+++ src/java/com/threerings/msoy/data/MsoyBodyObject.java
@@ -10,11 +10,22 @@ import com.threerings.crowd.data.BodyObject;
  */
 public interface MsoyBodyObject
 {
+    /**
+     * Return the implementor itself as a BodyObject. Please use this method rather than
+     * brutally casting this type to a BodyObject.
+     */
     BodyObject self ();
 
+    /**
+     * Returns true if this body has an actor in the scene and as a consequence uses an
+     * OccupantInfo that is instanceof ActorInfo. Currently, MOBs are not actors, and
+     * MemberObjects representing anonymous viewers aren't either.
+     */
     boolean isActor ();
 
+    /** Get the current state of the body's actor, or null if unset/unknown/default. */
     String getActorState ();
 
+    /** Set the current state of the body's actor. */
     public void setActorState (String value);
 }
