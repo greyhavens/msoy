18491871a26f28e43eee84e919b8348af63152b3
Par Winzell <zell@threerings.net>
2010-12-01 17:03:37 +0000

    Substantial improvements. Support now bypasses the login queue. A few bugs are ironed out. Better queue size estimation. We still try really hard to avoid creating an additional on-and-off invoker bounce.

--- src/java/com/threerings/msoy/server/MsoyClientResolver.java
+++ src/java/com/threerings/msoy/server/MsoyClientResolver.java
@@ -85,7 +85,7 @@ public class MsoyClientResolver extends CrowdClientResolver
     @Override
     public ClientLocal createLocalAttribute ()
     {
-        return null;
+        return new MemberClientLocal();
     }
 
     @Override
@@ -98,9 +98,9 @@ public class MsoyClientResolver extends CrowdClientResolver
         // see if we have a member object forwarded from our peer
         _fwddata = _peerMan.getForwardedMemberObject(_username);
         if (_fwddata != null) {
-            MemberObject memobj = _fwddata.left;
-            _mcobj.memobj = memobj;
-            _mcobj.bodyOid = memobj.getOid();
+            _memobj = _fwddata.left;
+            _mcobj.memobj = _memobj;
+            _mcobj.bodyOid = _memobj.getOid();
             for (Streamable local : _fwddata.right) {
                 @SuppressWarnings("unchecked") Class<Streamable> lclass =
                     (Class<Streamable>)local.getClass();
@@ -109,39 +109,29 @@ public class MsoyClientResolver extends CrowdClientResolver
             }
         }
 
-        // now we let the ClientResolutionListeners know (this is usually handled by the
-        // ClientResolver subclass, but we override it)
-        for (int ii = 0, ll = _listeners.size(); ii < ll; ii++) {
-            ClientResolutionListener crl = _listeners.get(ii);
-            try {
-                // add a reference for each listener
-                _clobj.reference();
-                crl.clientResolved(_username, _clobj);
-
-            } catch (Exception e) {
-                Log.log.warning("Client resolution listener choked in clientResolved() " + crl, e);
-            }
-        }
-
-        if (_mcobj.memobj != null) {
+        if (_mcobj.bodyOid != 0) {
             // we're done
-            log.debug("Resolved forwarded session", "clobj", _clobj.who());
+            log.info("Resolved forwarded session", "clobj", _clobj.who());
+
+            reportSuccess();
             return;
         }
 
         // otherwise we're creating a new MemberObject
-        final MemberObject memobj = new MemberObject();
+        _memobj = new MemberObject();
+
         // register it with the presents system
-        _omgr.registerObject(memobj);
+        _omgr.registerObject(_memobj);
+
         // hook the client object up with the body
-        _mcobj.memobj = memobj;
+        _mcobj.memobj = _memobj;
 
         // give the MemberObject the same (auth) username as we gave MemberClientObject
-        memobj.username = _username;
+        _memobj.username = _username;
 
         // and put the local into place. this completes what's normally done in clientmanager.
         MemberLocal local = new MemberLocal();
-        memobj.setLocal(ClientLocal.class, local);
+        _memobj.setLocal(ClientLocal.class, local);
 
         // create a deferred notifications array so that we can track any notifications dispatched
         // to this client until they're ready to read them; we'd have MsoyNotificationManager do
@@ -152,173 +142,214 @@ public class MsoyClientResolver extends CrowdClientResolver
 
         // do some stats-related hackery
         if (local.stats instanceof ServerStatSet) {
-            ((ServerStatSet)local.stats).init(_badgeMan, memobj);
+            ((ServerStatSet)local.stats).init(_badgeMan, _memobj);
         }
 
         // guests have MemberName as an auth username, members have Name
         if (_username instanceof MemberName) {
             // our auth username has our assigned name and member id, so use those
             final MemberName aname = (MemberName)_username;
-            memobj.memberName = new VizMemberName(
+            _memobj.memberName = new VizMemberName(
                 aname.toString(), aname.getId(), MemberCard.DEFAULT_PHOTO);
             local.stats = new StatSet();
             local.badges = new EarnedBadgeSet();
             local.friendIds = new StreamableArrayIntSet(0);
             local.inProgressBadges = new InProgressBadgeSet();
 
-        } else if (_username instanceof LurkerName) {
+            log.info("Resolved guest session", "guest", _memobj);
+            reportSuccess();
+            announce();
+            return;
+        }
+
+        if (_username instanceof LurkerName) {
             // we are lurker, we have no visible name to speak of
-            memobj.memberName = new VizMemberName("", 0, MemberCard.DEFAULT_PHOTO);
+            _memobj.memberName = new VizMemberName("", 0, MemberCard.DEFAULT_PHOTO);
             local.stats = new StatSet();
             local.badges = new EarnedBadgeSet();
             local.friendIds = new StreamableArrayIntSet(0);
             local.inProgressBadges = new InProgressBadgeSet();
 
-        } else {
-            Task task = new Task() {
-                @Override public void resolve () throws Exception {
-                    resolveMember(memobj);
-                }
-                @Override public void handle () throws Exception {
-                    // resolve this user's party info
-                    memobj.setParty(_peerMan.getPartySummary(memobj.getMemberId()));
-                }
-            };
-            Listener listener = new Listener() {
-                @Override public void progress (int position) {
-                    _mcobj.setPosition(position);
-                }
-                @Override public void done (Task task) {
-                    announce(memobj);
-                }
-                @Override public void failed (Task task, Exception e) {
-                    // destroy the dangling user object
-                    _omgr.destroyObject(_clobj.getOid());
-                    // let our listeners know that we're hosed
-                    reportFailure(e);
-                }
-            };
-
-            // Task fake = new Task() {
-            //     @Override public void resolve () throws Exception {
-            //         Thread.sleep(2000);
-            //     }
-            //     @Override public void handle () throws Exception {
-            //     }
-            // };
-
-            // _queue.queueTask(fake, null);
-            // _queue.queueTask(fake, null);
-            // _queue.queueTask(fake, null);
-            // _queue.queueTask(fake, null);
-            // _queue.queueTask(fake, null);
-            // _queue.queueTask(fake, null);
-
-            _queue.queueTask(task, listener);
-            
-            log.debug("Resolved unforwarded session", "clobj", _clobj.who());
+            log.info("Resolved lurker session", "guest", _memobj);
+            reportSuccess();
+            announce();
             return;
         }
-        log.debug("Resolved simple session", "clobj", _clobj.who());
 
-        announce(memobj);
+        // to finish creating MemberClientObject, we have to read MemberRecord on the invoker
+        // Note: this will automatically call reportSuccess(), but not announce()
+        _invoker.postUnit(this);
+    }
 
+    @Override // from ClientResolver
+    protected void resolveClientData (ClientObject clobj)
+        throws Exception
+    {
+        // load their record
+        _mrec = _memberRepo.loadMember(_username.toString());
+        if (_mrec == null) {
+            throw new Exception("Missing member record for authenticated member? " +
+                                "[username=" + _username + "]");
+        }
+
+        // and their mutelist
+        int[] muted = _memberRepo.loadMutelist(_mrec.memberId);
+        if (muted.length > 0) {
+            MemberClientLocal local = _mcobj.getLocal(MemberClientLocal.class);
+            local.mutedMemberIds = muted;
+        }
+
+        // support get resolved immediately, others will need to queue
+        if (_mrec.isSupport()) {
+            resolveMember();
+        }
+    }
+
+    @Override // from ClientResolver
+    protected void finishResolution (ClientObject clobj)
+    {
+        // if we're support, we already resolved, just finish up here   
+        if (_mrec.isSupport()) {        
+            _memobj.setParty(_peerMan.getPartySummary(_memobj.getMemberId()));
+
+            // reportSuccess() will be called automatically by our superclass, but we
+            // still want the availability of the MemberObject announced aftwards
+            _needAnnounce = true;
+            return;
+        }
+
+        // otherwise we queue up
+
+        Task task = new Task() {
+            @Override public void resolve () throws Exception {
+                resolveMember();
+            }
+            @Override public void handle () throws Exception {
+                _memobj.setParty(_peerMan.getPartySummary(_memobj.getMemberId()));
+            }
+        };
+        Listener listener = new Listener() {
+            @Override public void progress (int position) {
+                _mcobj.setPosition(position);
+            }
+            @Override public void done (Task task) {
+                // note: reportSuccess() has already been called by our superclass
+                announce();
+            }
+            @Override public void failed (Task task, Exception e) {
+                // destroy the dangling user object
+                _omgr.destroyObject(_clobj.getOid());
+                // let our listeners know that we're hosed
+                reportFailure(e);
+            }
+        };
+
+        // DEBUGGING
+        if (true) {
+            Task fake = new Task() {
+                @Override public void resolve () throws Exception {
+                    Thread.sleep(2000);
+                }
+                @Override public void handle () throws Exception {
+                }
+            };
+
+            _queue.queueTask(fake, null);
+            _queue.queueTask(fake, null);
+            _queue.queueTask(fake, null);
+            _queue.queueTask(fake, null);
+            _queue.queueTask(fake, null);
+            _queue.queueTask(fake, null);
+        }
+
+        _queue.queueTask(task, listener);
+
+        // update the queue size, things might have happened since the object was created
+        _mcobj.position = _queue.getQueueSize();
+
+        log.info("Resolved unforwarded session", "clobj", _clobj.who());
     }
 
     /**
      * Resolve a msoy member. This is called on the invoker thread.
      */
-    protected void resolveMember (final MemberObject memobj)
+    protected void resolveMember ()
         throws Exception
     {
+        int memberId = _mrec.memberId;
+
         // start keeping track of what we're doing
         ResolutionProfiler profiler = new ResolutionProfiler();
 
         // load up their member information using on their authentication (account) name
-        final MemberRecord member = _memberRepo.loadMember(_username.toString());
-        if (member == null) {
-            throw new Exception("Missing member record for authenticated member? " +
-                                "[username=" + _username + "]");
-        }
         profiler.complete(Step.MEMBER);
 
-        final MemberMoney money = _moneyLogic.getMoneyFor(member.memberId);
+        final MemberMoney money = _moneyLogic.getMoneyFor(memberId);
         profiler.complete(Step.MONEY);
 
         // NOTE: we avoid using the dobject setters here because we know the object is not out in
         // the wild and there's no point in generating a crapload of events during user
         // initialization when we know that no one is listening
-        final ProfileRecord precord = _profileRepo.loadProfile(member.memberId);
+        final ProfileRecord precord = _profileRepo.loadProfile(memberId);
         profiler.complete(Step.PROFILE);
 
-        memobj.memberName = new VizMemberName(
-            member.name, member.memberId,
+        _memobj.memberName = new VizMemberName(_mrec.name, memberId,
             (precord == null) ? MemberCard.DEFAULT_PHOTO : precord.getPhoto());
         if (precord != null) {
-            memobj.headline = precord.headline;
+            _memobj.headline = precord.headline;
         }
 
         // the place they call home
-        memobj.homeSceneId = member.homeSceneId;
+        _memobj.homeSceneId = _mrec.homeSceneId;
 
         // runtime money
-        memobj.coins = money.coins;
-        memobj.bars = money.bars;
+        _memobj.coins = money.coins;
+        _memobj.bars = money.bars;
 
         // assign the level, giving them a chance to level up too; note that we do this even though
         // the code tries to always keep the level in sync with accCoins. this is necessary because
         //   1. once upon a time the level was not kept in sync and we did not do a big migration
         //   2. there could have been bugs or errors with previous accCoins changes that inhibited
         //      the level up
-        memobj.level = _memberLogic.synchMemberLevel(
-            member.memberId, member.level, money.accCoins);
+        _memobj.level = _memberLogic.synchMemberLevel(memberId, _mrec.level, money.accCoins);
 
         // load up this member's persistent stats
-        MemberLocal local = memobj.getLocal(MemberLocal.class);
-        final List<Stat> stats = _statRepo.loadStats(member.memberId);
-        local.stats = new ServerStatSet(stats.iterator(), _badgeMan, memobj);
+        MemberLocal local = _memobj.getLocal(MemberLocal.class);
+        List<Stat> stats = _statRepo.loadStats(memberId);
+        local.stats = new ServerStatSet(stats.iterator(), _badgeMan, _memobj);
         profiler.complete(Step.STATS);
 
-        // and their mutelist
-        int[] muted = _memberRepo.loadMutelist(member.memberId);
-        if (muted.length > 0) {
-            local.mutedMemberIds = muted;
-        }
-        profiler.complete(Step.MUTED);
-
         // and their badges
-        local.badgesVersion = member.badgesVersion;
-        local.badges = new EarnedBadgeSet(
-            Iterables.transform(_badgeRepo.loadEarnedBadges(member.memberId),
-                                EarnedBadgeRecord.TO_BADGE));
-        local.inProgressBadges = new InProgressBadgeSet(
-            Iterables.transform(_badgeRepo.loadInProgressBadges(member.memberId),
-                                InProgressBadgeRecord.TO_BADGE));
+        local.badgesVersion = _mrec.badgesVersion;
+        local.badges = new EarnedBadgeSet(Iterables.transform(
+                _badgeRepo.loadEarnedBadges(memberId), EarnedBadgeRecord.TO_BADGE));
+        local.inProgressBadges = new InProgressBadgeSet(Iterables.transform(
+                _badgeRepo.loadInProgressBadges(memberId), InProgressBadgeRecord.TO_BADGE));
         profiler.complete(Step.BADGES);
 
 //        // load up any item lists they may have
-//        List<ItemListInfo> itemLists = _itemMan.getItemLists(member.memberId);
-//        memobj.lists = new DSet<ItemListInfo>(itemLists);
+//        List<ItemListInfo> itemLists = _itemMan.getItemLists(memberId);
+//        _memobj.lists = new DSet<ItemListInfo>(itemLists);
 
         // fill in this member's raw friends list; the friend manager will update it later
-        local.friendIds = _memberRepo.loadFriendIds(member.memberId);
+        local.friendIds = _memberRepo.loadFriendIds(memberId);
         profiler.complete(Step.FRIENDS);
 
         // load up this member's group memberships
-        memobj.groups = new DSet<GroupMembership>(
+        _memobj.groups = new DSet<GroupMembership>(
             // we don't pass in member name here because we don't need it on the client
-            _groupRepo.resolveGroupMemberships(member.memberId, null).iterator());
+            _groupRepo.resolveGroupMemberships(memberId, null).iterator());
         profiler.complete(Step.GROUPS);
 
         // load up this member's current new mail count
-        memobj.newMailCount = _mailLogic.getUnreadConvoCount(member.memberId);
+        _memobj.newMailCount = _mailLogic.getUnreadConvoCount(memberId);
         profiler.complete(Step.MAIL);
 
         // load up their selected avatar, we'll configure it later
-        if (member.avatarId != 0) {
-            final AvatarRecord avatar = _itemLogic.getAvatarRepository().loadItem(member.avatarId);
+        if (_mrec.avatarId != 0) {
+            AvatarRecord avatar = _itemLogic.getAvatarRepository().loadItem(_mrec.avatarId);
             if (avatar != null) {
-                memobj.avatar = (Avatar)avatar.toItem();
+                _memobj.avatar = (Avatar)avatar.toItem();
                 MemoriesRecord memrec = _memoryRepo.loadMemory(avatar.getType(), avatar.itemId);
                 local.memories = (memrec == null) ? null : memrec.toEntry();
             }
@@ -326,30 +357,40 @@ public class MsoyClientResolver extends CrowdClientResolver
         }
 
         // resolve their persisted theme
-        if (member.themeGroupId != 0) {
-            memobj.theme = _groupRepo.loadGroupName(member.themeGroupId);
+        if (_mrec.themeGroupId != 0) {
+            _memobj.theme = _groupRepo.loadGroupName(_mrec.themeGroupId);
             profiler.complete(Step.THEME);
         }
 
         // for players, resolve this here from the database.
         // guests will get resolution later on, in MsoySession.sessionWillStart()
-        memobj.visitorInfo = new VisitorInfo(member.visitorId, true);
+        _memobj.visitorInfo = new VisitorInfo(_mrec.visitorId, true);
 
         // Load up the member's experiences
-        //memobj.experiences = new DSet<MemberExperience>(
-        //        _memberLogic.getExperiences(member.memberId));
+        //_memobj.experiences = new DSet<MemberExperience>(
+        //        _memberLogic.getExperiences(memberId));
         profiler.complete();
     }
 
-    protected void announce (MemberObject obj)
+    protected void announce ()
     {
         // setting the oid should complete the client's two-phase loading process
-        _mcobj.setBodyOid(obj.getOid());
+        _mcobj.setBodyOid(_memobj.getOid());
+    }
+
+    @Override
+    protected void reportSuccess ()
+    {
+        super.reportSuccess();
+
+        /** If finishResolution() flagged the need for an announce here, do it */
+        if (_needAnnounce) {
+            announce();
+        }
     }
 
     protected enum Step {
-        MEMBER, MONEY, PROFILE, STATS, MUTED, BADGES, FRIENDS, GROUPS, MAIL,
-        AVATAR, THEME,
+        MEMBER, MONEY, PROFILE, STATS, BADGES, FRIENDS, GROUPS, MAIL, AVATAR, THEME,
     }
 
     /**
@@ -414,7 +455,17 @@ public class MsoyClientResolver extends CrowdClientResolver
     /** Info on our member object forwarded from another server. */
     protected Tuple<MemberObject,Streamable[]> _fwddata;
 
+    /** Our actual client object, constructed very early in the process. */
     protected MemberClientObject _mcobj;
+
+    /** Our actual member object, which needs to be explicitly announced. */
+    protected MemberObject _memobj;
+
+    /** Our MemberRecord, which needs to be passed this way from invoker to dobj thread. */
+    protected MemberRecord _mrec;
+
+    /** Whether or not we need to automatically announce() after reportSuccess() finishes. */
+    protected boolean _needAnnounce;
 
     @Inject protected BadgeManager _badgeMan;
     @Inject protected BadgeRepository _badgeRepo;
