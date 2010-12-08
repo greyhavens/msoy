35b06be8717ffa8bb7da0fc31f36a9151340ea85
Par Winzell <zell@threerings.net>
2010-12-08 04:41:13 +0000

    This is pretty cool. While trying to figure out an obscure pet following problems, I realized we were never destroying MemberObjects when players were logging out. Oops? So, here's some further complexity to add to our two-phase system. Seems to work though.

--- src/java/com/threerings/msoy/server/MsoyClientManager.java
+++ src/java/com/threerings/msoy/server/MsoyClientManager.java
@@ -8,14 +8,21 @@ import com.google.inject.Singleton;
 import com.samskivert.util.Lifecycle;
 import com.samskivert.util.ObserverList;
 
+import com.threerings.util.Name;
+
 import com.threerings.presents.data.ClientObject;
 import com.threerings.presents.dobj.AttributeChangedEvent;
 import com.threerings.presents.dobj.AttributeChangeListener;
+import com.threerings.presents.dobj.ObjectDeathListener;
+import com.threerings.presents.dobj.ObjectDestroyedEvent;
 import com.threerings.presents.server.ClientManager;
 import com.threerings.presents.server.PresentsSession;
 import com.threerings.presents.server.ReportManager;
 
 import com.threerings.msoy.data.MemberClientObject;
+import com.threerings.msoy.data.MemberObject;
+
+import static com.threerings.msoy.Log.log;
 
 /**
  */
@@ -37,7 +44,7 @@ public class MsoyClientManager extends ClientManager
         if (clobj instanceof MemberClientObject) {
             if (((MemberClientObject) clobj).bodyOid == 0) {
                 // listen for bodyOid to be set
-                session.getClientObject().addListener(new AttributeChangeListener() {
+                clobj.addListener(new AttributeChangeListener() {
                     public void attributeChanged (AttributeChangedEvent event) {
                         if (MemberClientObject.BODY_OID.equals(event.getName())) {
                             clientSessionReallyDidStart(session);
@@ -48,11 +55,22 @@ public class MsoyClientManager extends ClientManager
             }
         }
         // else go right now
-        clientSessionReallyDidStart (session);
+        clientSessionReallyDidStart(session);
     }
 
     protected void clientSessionReallyDidStart (final PresentsSession session)
     {
+        ClientObject clobj = session.getClientObject();
+        if (clobj instanceof MemberClientObject) {
+            MemberClientObject mcobj = (MemberClientObject) clobj;
+            if (mcobj.memobj != null) {
+                mcobj.addListener(new ClientObjectDeathListener(mcobj.memobj));
+            } else {
+                log.warning("MemberClientObject session started without MemberObject",
+                    "who", mcobj.who());
+            }
+        }
+
         // let the observers know
         _clobservers.apply(new ObserverList.ObserverOp<ClientObserver>() {
             public boolean apply (ClientObserver observer) {
@@ -60,5 +78,30 @@ public class MsoyClientManager extends ClientManager
                 return true;
             }
         });
+    }
+
+    protected static class ClientObjectDeathListener implements ObjectDeathListener {
+        public void objectDestroyed (ObjectDestroyedEvent event) {
+            // sanity checks
+            if (!_memobj.isActive()) {
+                log.warning("What the... almost destroyed already-destroyed MemberObject",
+                    "member", _memobj.who());
+                return;
+            }
+            if (_memobj.location != null) {
+                log.warning("Erk, destroying MemberObject in a location", "member",
+                    _memobj.who(), "location", _memobj.location);
+                // but we'll accept it
+            }
+            // proceed with the destruction
+            _memobj.destroy();
+        }
+
+        protected ClientObjectDeathListener (MemberObject memobj)
+        {
+            _memobj = memobj;
+        }
+
+        protected MemberObject _memobj;
     }
 }
