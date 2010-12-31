6d21d0b81ecc83cac29aeeb8e73645653cff86d9
Michael Bayne <mdb@threerings.net>
2010-12-31 23:07:41 +0000

    ToyBoxGameConfig no longer used here (all this Java client stuff should probably just be nixed at some point to avoid future annoyance).

--- src/java/com/threerings/msoy/game/data/ParlorGameConfig.java
+++ src/java/com/threerings/msoy/game/data/ParlorGameConfig.java
@@ -4,9 +4,9 @@
 package com.threerings.msoy.game.data;
 
 import com.threerings.util.ActionScript;
-import com.threerings.toybox.data.ToyBoxGameConfig;
 
 import com.whirled.game.data.GameDefinition;
+import com.whirled.game.data.WhirledGameConfig;
 
 import com.threerings.orth.data.MediaDesc;
 
@@ -14,7 +14,7 @@ import com.threerings.orth.data.MediaDesc;
  * Configuration for a Parlor game. Parlor games take over the entire display and don't leverage
  * any of the Whirled virtual world stuff.
  */
-public class ParlorGameConfig extends ToyBoxGameConfig
+public class ParlorGameConfig extends WhirledGameConfig
     implements MsoyGameConfig
 {
     /** Info on the game being played. */
