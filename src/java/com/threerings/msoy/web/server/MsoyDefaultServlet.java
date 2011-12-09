a831333303d9d7b99dee388b705a0297ee303540
Bruno Garcia <bruno@threerings.net>
2011-12-09 03:26:40 +0000

    Revert debug logging from yesterday's experiments.

--- src/java/com/threerings/msoy/web/server/MsoyDefaultServlet.java
+++ src/java/com/threerings/msoy/web/server/MsoyDefaultServlet.java
@@ -116,8 +116,6 @@ public class MsoyDefaultServlet extends DefaultServlet
                 log.info("Sending landing cookie", "value", cookie);
             }
         }
-
-        log.info("Hello there index.html");
     }
 
     // dependencies
