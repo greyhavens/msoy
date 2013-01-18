d2c0bd91b77ec399ec4503491538767858fb30fb
Bruno Garcia <bruno@threerings.net>
2013-01-18 21:43:12 +0000

    Oh come on GWT.

---
+++ src/java/com/google/gwt/core/client/GWTBridge.java
@@ -0,0 +1,14 @@
+//
+// $Id$
+
+package com.google.gwt.core.client;
+
+/**
+ * Fix for GWT 2.5. This can probably be removed when the fix is released in 2.5.1.
+ *
+ * http://code.google.com/p/google-web-toolkit/issues/detail?id=7527
+ * https://groups.google.com/forum/?fromgroups=#!topic/google-web-toolkit/H0Blv3C_fns
+ */
+public abstract class GWTBridge extends com.google.gwt.core.shared.GWTBridge
+{
+}
